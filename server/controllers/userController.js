a880e3a88b3e13960b1b1f3408f297955bb6293d
jardenna <hbjornum@gmail.com>
2025-03-03 17:09:33 +0100

    user api

---
+++ server/controllers/userController.js
@@ -0,0 +1,8 @@
+// import User from '../models/userModel.js'
+import asyncHandler from '../middleware/asyncHandler.js';
+
+const createUser = asyncHandler(async (req, res) => {
+  res.send('hello');
+});
+
+export { createUser };
