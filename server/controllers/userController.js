6d4f975d03529b6b0350f5e31635083e0ce61c3b
jardenna <hbjornum@gmail.com>
2025-03-04 14:54:37 +0100

    login and logout user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import createToken from '../utils/createToken.js';
 import { validatePassword } from '../utils/passwordValidator.js';
 import { emailRegex } from '../utils/regex.js';
 import { t } from '../utils/translator.js';
@@ -21,7 +22,7 @@ const createUser = asyncHandler(async (req, res) => {
 
   // Validate email
   if (!emailRegex.test(email)) {
-    return res.status(400).json({
+    return res.status(422).json({
       success: false,
       message: t('invalidEmail', req.lang),
     });
@@ -53,7 +54,92 @@ const createUser = asyncHandler(async (req, res) => {
     password: hashPassword,
   });
 
-  await newUser.save();
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error(t('invalidUserData', req.lang));
+  }
 });
 
-export { createUser };
+// @desc    Login user
+// @route   POST /api/users/auth
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password,
+    );
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: t('invalidPassword', req.lang),
+      });
+    }
+  }
+
+  if (!email) {
+    return res.status(401).json({
+      message: 'Email must be provided',
+    });
+  }
+
+  if (!password) {
+    return res.status(401).json({
+      message: 'Password must be provided',
+    });
+  }
+
+  if (!existingUser) {
+    return res.status(400).json({
+      success: false,
+      message: t('noUser', req.lang),
+    });
+  }
+
+  try {
+    createToken(res, existingUser._id);
+    res.status(201).json({
+      _id: existingUser._id,
+      username: existingUser.username,
+      email: existingUser.email,
+      isAdmin: existingUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `${error.message} - ${t('loginFailed', req.lang)}`,
+    });
+  }
+});
+
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie('jwt', '', {
+    httyOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({
+    success: true,
+    message: t('loggedOut', req.lang),
+  });
+});
+
+export { createUser, loginUser, logoutCurrentUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import languageMiddleware from '../middleware/languageMiddleware.js';
 
-import { createUser } from '../controllers/userController.js';
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/register', languageMiddleware, createUser);
+router.post('/auth', languageMiddleware, loginUser);
+router.post('/logout', languageMiddleware, logoutCurrentUser);
 
 export default router;
