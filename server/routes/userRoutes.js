84045cafa92650185e239ee1572d93e6ca7f89e4
jardenna <hbjornum@gmail.com>
2025-03-05 10:27:56 +0100

    userroutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,10 @@
 import express from 'express';
+import { getAllUsers } from '../controllers/userController.js';
 import { authenticate, authorizeAdmin } from '../middleware/authMiddleware.js';
 import languageMiddleware from '../middleware/languageMiddleware.js';
 
-import { getAllUsers } from '../controllers/userController.js';
-
 const router = express.Router();
 
-// router.post('/auth', languageMiddleware, loginUser);
-// router.post('/logout', languageMiddleware, logoutCurrentUser);
 router.get('/', languageMiddleware, authenticate, authorizeAdmin, getAllUsers);
 
 export default router;
