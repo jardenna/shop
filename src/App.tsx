9f3b567084ea1ee655f01c1d97eeec480c591a42
jardenna <hbjornum@gmail.com>
2025-03-06 13:44:08 +0100

    images

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { FC } from 'react';
-import Table from './components/table/Table';
 
 const App: FC = () => (
   <div className="main-container">
     <header aria-label="primary">Header</header>
-    <main>
-      <Table />
-    </main>
+    <main>main</main>
     <footer aria-label="primary">Footer</footer>
   </div>
 );
