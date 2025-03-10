ecf5538424339da74ccb2bdf9b749844bacca199
jardenna <hbjornum@gmail.com>
2025-03-10 10:43:32 +0100

    dropdown btn

--- src/components/dropdownBtn/DropdownBtn.tsx
+++ src/components/dropdownBtn/DropdownBtn.tsx
@@ -1,70 +1,72 @@
-import { FC } from 'react';
-import { useNavigate } from 'react-router';
-import { useLogoutMutation } from '../../features/auth/authApiSlice';
+import { FC, useRef, useState } from 'react';
 import useLanguage from '../../features/language/useLanguage';
-import { MainPath } from '../../layout/nav/enums';
-import { BtnVariant } from '../../types/enums';
+import useClickOutside from '../../hooks/useClickOutside';
+import useKeyPress from '../../hooks/useKeyPress';
+import { BtnVariant, KeyCode } from '../../types/enums';
 import Button from '../Button';
 import IconContent from '../IconContent';
 import { IconName } from '../icons/Icon';
+import './_dropdown-btn.scss';
+
+export interface DropdownListProps {
+  id: number;
+  label: string;
+  onClick: () => void;
+}
 
 interface DropdownBtnProps {
+  dropdownList: DropdownListProps[];
   username: string;
   btnVariant?: BtnVariant;
 }
 
 const DropdownBtn: FC<DropdownBtnProps> = ({
   username,
+  dropdownList,
   btnVariant = BtnVariant.Ghost,
 }) => {
-  const navigate = useNavigate();
   const { language } = useLanguage();
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
+  useKeyPress(() => {
+    setDropdownIsOpen(false);
+  }, [KeyCode.Esc]);
+  useClickOutside(dropdownRef, () => {
+    setDropdownIsOpen(false);
+  });
 
-  const [logout] = useLogoutMutation();
-
-  const handleLogout = () => {
-    logout();
-    navigate(`/${MainPath.Login}`);
-  };
-
-  const dropdownList = [
-    {
-      label: language.myAccount,
-      id: 1,
-      onClick: () => {
-        navigate(`/${MainPath.MyAccount}`);
-      },
-    },
-    {
-      label: language.myOrders,
-      id: 2,
-      onClick: () => {
-        navigate(`/${MainPath.Orders}`);
-      },
-    },
-  ];
   return (
     <section>
-      <Button variant={btnVariant}>
+      <Button
+        variant={btnVariant}
+        onClick={() => {
+          setDropdownIsOpen(!dropdownIsOpen);
+        }}
+        ariaExpanded={dropdownIsOpen}
+        ariaHasPopup
+      >
         <IconContent
           ariaLabel={language.myAccount}
           iconName={IconName.User}
           title={language.user}
         />
       </Button>
-      <span>
-        {language.welcome} {username}
-      </span>
-      <div>
-        <Button onClick={handleLogout}>{language.logout}</Button>
-        <ul>
-          {dropdownList.map(({ id, label, onClick }) => (
-            <li key={id}>
-              <Button onClick={onClick}>{label}</Button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {dropdownIsOpen && (
+        <div className="dropdown-list" ref={dropdownRef}>
+          <span>
+            {language.welcome} {username}
+          </span>
+          <ul>
+            {dropdownList.map(({ id, label, onClick }) => (
+              <li key={id}>
+                <Button variant={BtnVariant.Ghost} onClick={onClick}>
+                  {label}
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </section>
   );
 };
