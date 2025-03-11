9c9ad2145c93878ce97131c9b7e01ef1de705ab1
jardenna <hbjornum@gmail.com>
2025-03-11 11:38:03 +0100

    fix skeleton height

--- src/components/skeleton/SkeletonGrid.tsx
+++ src/components/skeleton/SkeletonGrid.tsx
@@ -6,14 +6,14 @@ const SkeletonGrid: FC = () => (
   <div className="grid three-col">
     <span className="flex column">
       <Skeleton count={6} />
-      <SkeletonList count={2} width={14} variant="secondary" />
+      <SkeletonList count={2} width="14" variant="secondary" />
     </span>
     <span className="flex column">
       <Skeleton count={2} />
-      <Skeleton height={7.6} count={2} />
+      <Skeleton height="7.6" count={2} />
     </span>
     <div className="flex column">
-      <Skeleton variant="img" height={24} />
+      <Skeleton variant="img" height="24" />
     </div>
   </div>
 );
