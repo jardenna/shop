672003904dcb48a258615c7ca8696977a7b36e57
jardenna <hbjornum@gmail.com>
2025-03-13 15:18:11 +0100

    select for CurrencySwitcher

--- src/features/currency/components/CurrencySwitcher.tsx
+++ src/features/currency/components/CurrencySwitcher.tsx
@@ -1,27 +1,35 @@
+import Select from 'react-select';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { OptionType } from '../../../components/selectBox/SelectBox';
+
 import { selectCurrency, setCurrency } from '../currencySlice';
 
 const CurrencySwitcher = () => {
   const dispatch = useAppDispatch();
   const { selectedCurrency, rates } = useAppSelector(selectCurrency);
 
-  const availableCurrencies = Object.keys(rates);
+  // Convert rates into SelectBox options
+  const currencyOptions: OptionType[] = Object.keys(rates).map((currency) => ({
+    label: currency,
+    value: currency,
+  }));
+
+  const handleChange = (selected: OptionType | null) => {
+    if (selected) {
+      dispatch(setCurrency(selected.value));
+    }
+  };
 
   return (
-    <div>
-      <label htmlFor="currency">Currency: </label>
-      <select
-        id="currency"
-        value={selectedCurrency}
-        onChange={(e) => dispatch(setCurrency(e.target.value))}
-      >
-        {availableCurrencies.map((currency) => (
-          <option key={currency} value={currency}>
-            {currency}
-          </option>
-        ))}
-      </select>
-    </div>
+    <Select
+      id="currency"
+      name="currency"
+      options={currencyOptions}
+      value={
+        currencyOptions.find((opt) => opt.value === selectedCurrency) || null
+      }
+      onChange={handleChange}
+    />
   );
 };
 
