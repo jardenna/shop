edeaf931602294fdca5b647ef03c1ebe04d0eb8a
jardenna <hbjornum@gmail.com>
2025-03-14 11:40:07 +0100

    renamed selectTest

---
+++ src/components/selectBox/MultiSelectBox.tsx
@@ -0,0 +1,35 @@
+import { FC, useState } from 'react';
+import Selectbox, { OptionType } from './SelectBox';
+
+const MultiSelectBox: FC = () => {
+  const options = [
+    { label: 'apple', value: 1 },
+    { label: 'orange', value: 2 },
+    { label: 'kiwi', value: 3 },
+  ];
+
+  const [items, setItems] = useState<OptionType[] | OptionType>();
+
+  const handleOption = (selections: any) => {
+    setItems(selections);
+  };
+  console.log(items);
+
+  return (
+    <section>
+      <Selectbox
+        isMulti
+        name="fruits"
+        options={options}
+        onChange={(selections) => {
+          handleOption(selections);
+        }}
+        defaultValue={{ label: 'kiwi', value: 3 }}
+        id="fruits"
+        labelText="Select Fruits"
+      />
+    </section>
+  );
+};
+
+export default MultiSelectBox;
