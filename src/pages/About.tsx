95124e4a5ef5a6b763bd477c948eaf7d35c9a093
jardenna <hbjornum@gmail.com>
2025-03-15 08:34:19 +0100

    deleted drag

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,33 +1,8 @@
-import { FC, useState } from 'react';
-import Drag from '../drag/Drag';
+import { FC } from 'react';
 import ProductPrice from '../features/currency/components/ProductPrice';
-const About: FC = () => {
-  const [translate, setTranslate] = useState({
-    x: 0,
-    y: 0,
-  });
-
-  const handleDragMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    setTranslate({
-      x: translate.x + e.movementX,
-      y: translate.y + e.movementY,
-    });
-  };
-
-  return (
-    <section>
-      <ProductPrice price={10200} />
-
-      <Drag onDragMove={handleDragMove}>
-        <div
-          style={{
-            transform: `translateX(${translate.x}px) translateY(${translate.y}px)`,
-          }}
-        >
-          <div className="test">hello</div>
-        </div>
-      </Drag>
-    </section>
-  );
-};
+const About: FC = () => (
+  <section>
+    <ProductPrice price={10200} />
+  </section>
+);
 export default About;
