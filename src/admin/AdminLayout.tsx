e1849cb9093202cd25c5e6f5a2c47ca40521e1cb
jardenna <hbjornum@gmail.com>
2025-03-17 09:22:42 +0100

    useMediaqueries

--- src/admin/AdminLayout.tsx
+++ src/admin/AdminLayout.tsx
@@ -1,20 +1,44 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Outlet } from 'react-router';
+import IconBtn from '../components/IconBtn';
 import SkipLink from '../components/skipLinks/SkipLinks';
 import useLanguage from '../features/language/useLanguage';
+import useMediaQuery from '../hooks/useMediaQuery ';
 import Nav from '../layout/nav/Nav';
 import { adminNavList } from '../layout/nav/navList';
+import { IconName } from '../types/enums';
 import AdminHeader from './AdminHeader';
 
 const AdminLayout: FC = () => {
   const { language } = useLanguage();
+  const [collapsedMenu, setCollapsedMenu] = useState(false);
+
+  const isTabletSize = useMediaQuery('(max-width: 768px)');
+
+  useEffect(() => {
+    setCollapsedMenu(isTabletSize); // Auto-collapse when screen is small
+  }, [isTabletSize]);
+
+  const handleCollapseMenu = () => {
+    setCollapsedMenu(!collapsedMenu);
+  };
+
   return (
     <div className="main-container admin-page">
       <SkipLink />
       <AdminHeader ariaLabel={language.main} />
       <main id="main" className="main">
         <aside className="aside">
-          <Nav navList={adminNavList} className="admin-nav" />
+          <IconBtn
+            onClick={handleCollapseMenu}
+            ariaLabel="Collapse menu"
+            iconName={IconName.Account}
+            title="collapse menu"
+          />
+          <Nav
+            navList={adminNavList}
+            className={`admin-nav ${collapsedMenu ? 'collapsed' : ''}`}
+          />
         </aside>
         <div className="container">
           <Outlet />
