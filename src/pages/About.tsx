7126162870f6dd4ba59ad6a5c8bb4c092c9e41df
jardenna <hbjornum@gmail.com>
2025-03-20 06:36:27 +0100

    about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 import ProductPrice from '../features/currency/components/ProductPrice';
+
 const About: FC = () => (
   <section>
     <ProductPrice price={10200} />
   </section>
 );
+
 export default About;
