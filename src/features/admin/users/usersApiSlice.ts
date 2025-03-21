97110565aa4a93e86a0e51ab0b764f522fa597e2
jardenna <hbjornum@gmail.com>
2025-03-21 09:49:57 +0100

    delete user api

--- src/features/admin/users/usersApiSlice.ts
+++ src/features/admin/users/usersApiSlice.ts
@@ -1,20 +1,34 @@
 import apiSlice from '../../../app/api/apiSlice';
-import { UpdateUserRoleRequest, UserResponse } from '../../../app/api/apiTypes';
+import {
+  OmittedAuthResponse,
+  UpdateUserRoleRequest,
+  UserResponse,
+} from '../../../app/api/apiTypes';
 import { userEndpoints } from '../../../app/endpoints';
 import { TagTypesEnum } from '../../../types/enums';
+
+const endpoint = userEndpoints.users;
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getAllUsers: builder.query<UserResponse[], void>({
-      query: () => userEndpoints.users,
+      query: () => endpoint,
       providesTags: [TagTypesEnum.Auth],
     }),
     getUserByd: builder.query<UserResponse, number>({
-      query: (id) => `${userEndpoints.users}/${id}`,
+      query: (id) => `${endpoint}/${id}`,
+    }),
+    deleteUser: builder.mutation<OmittedAuthResponse, string>({
+      query: (id) => ({
+        url: `${endpoint}/${id}`,
+        method: 'DELETE',
+        body: id,
+      }),
+      invalidatesTags: [TagTypesEnum.Auth],
     }),
     updateUserRole: builder.mutation<UserResponse, UpdateUserRoleRequest>({
       query: (data) => ({
-        url: `${userEndpoints.users}/${data.userId}`,
+        url: `${endpoint}/${data.userId}`,
         method: 'PUT',
         body: data.role,
       }),
@@ -26,4 +40,5 @@ export const {
   useGetAllUsersQuery,
   useGetUserBydQuery,
   useUpdateUserRoleMutation,
+  useDeleteUserMutation,
 } = usersApiSlice;
