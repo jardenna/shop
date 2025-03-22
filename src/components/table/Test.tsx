40d3d405e3c1a92b90e4ee49516d474215d1ef45
jardenna <hbjornum@gmail.com>
2025-03-22 08:17:07 +0100

    test sort

---
+++ src/components/table/Test.tsx
@@ -0,0 +1,60 @@
+import useSorting from './useSorting';
+
+type Item = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+function Test() {
+  const initialItems: Item[] = [
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Bob', age: 30 },
+    { id: 3, name: 'Charlie', age: 20 },
+  ];
+
+  const { sortedItems, sortFunction, sortClassName, onClearAll } =
+    useSorting(initialItems);
+
+  const handleClear = () => {
+    onClearAll();
+  };
+
+  return (
+    <div>
+      <button onClick={handleClear} type="button">
+        clear
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th
+              onClick={() => {
+                sortFunction('name');
+              }}
+            >
+              Name {sortClassName('name')}
+            </th>
+            <th
+              onClick={() => {
+                sortFunction('age');
+              }}
+            >
+              Age {sortClassName('age')}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedItems.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Test;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 import ProductPrice from '../features/currency/components/ProductPrice';
+import Test from '../components/table/Test';
 
 const About: FC = () => (
   <section>
+    <Test />
     <ProductPrice price={10200} />
   </section>
 );
