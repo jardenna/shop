06c617c73b415696b497824879d2e38aa5f901da
jardenna <hbjornum@gmail.com>
2025-03-22 16:52:10 +0100

    new table

--- src/pages/Collections.tsx
+++ src/pages/Collections.tsx
@@ -1,5 +1,11 @@
 import { FC } from 'react';
+import Table from '../components/tabl/tabl';
 
-const Collections: FC = () => <section>Collections</section>;
+const Collections: FC = () => (
+  <section>
+    Collections
+    <Table />
+  </section>
+);
 
 export default Collections;
