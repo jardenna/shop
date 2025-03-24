45655e7cc8bd0fedd69bdf50fb3472b0582ccd53
jardenna <hbjornum@gmail.com>
2025-03-24 16:42:59 +0100

    deleted unused tables

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,10 @@
 import { FC } from 'react';
-import UserTable from '../components/table/UserTable';
-import { useGetAllUsersQuery } from '../features/admin/users/usersApiSlice';
 import ProductPrice from '../features/currency/components/ProductPrice';
 
-const About: FC = () => {
-  const tableHeaders = ['username', 'email', 'role', ''];
-  const { data: allUsers } = useGetAllUsersQuery();
-  return (
-    <section>
-      {allUsers && (
-        <UserTable tableHeaders={tableHeaders} tableData={allUsers} />
-      )}
-      <ProductPrice price={10200} />
-    </section>
-  );
-};
+const About: FC = () => (
+  <section>
+    <ProductPrice price={10200} />
+  </section>
+);
 
 export default About;
