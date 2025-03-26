3b480f2b9f56468d3b1fee1f25ee6408d046ade1
jardenna <hbjornum@gmail.com>
2025-03-26 15:48:19 +0100

    sortTable dashboard

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,5 +1,10 @@
 import { FC } from 'react';
+import SortTable from '../../components/sortTable/SortTable';
 
-const Dashboard: FC = () => <section>Dashboard</section>;
+const Dashboard: FC = () => (
+  <section>
+    <SortTable />
+  </section>
+);
 
 export default Dashboard;
