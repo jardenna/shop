f483368424a0cc5e115c7c8ead9a6d974c3063a9
jardenna <hbjornum@gmail.com>
2025-03-29 17:31:31 +0100

    update category

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -44,4 +44,37 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
-export { createCategory };
+// @desc    Create category
+// @route   /api/category/id
+// @method  Put
+// @access  Private for admin and employee
+const updateCategory = asyncHandler(async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { categoryId } = req.params;
+    const category = await Category.findOne({ id: categoryId });
+
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: t('categorieNotFound', req.lang),
+      });
+    }
+
+    category.name = name;
+
+    const updatedCategory = await category.save();
+    res.status(201).json({
+      success: true,
+      message: 'Category updated',
+      updatedCategory,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+});
+
+export { createCategory, updateCategory };
--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,11 +1,22 @@
 import express from 'express';
-import { createCategory } from '../controllers/categoryController.js';
+import {
+  createCategory,
+  updateCategory,
+} from '../controllers/categoryController.js';
 import { authenticate, authorizeAdmin } from '../middleware/authMiddleware.js';
 import languageMiddleware from '../middleware/languageMiddleware.js';
 const router = express.Router();
 
+router.post(
+  '/',
+  languageMiddleware,
+  authenticate,
+  authorizeAdmin,
+  createCategory,
+);
+
 router
-  .route('/')
-  .post(languageMiddleware, authenticate, authorizeAdmin, createCategory);
+  .route('/:id')
+  .put(languageMiddleware, authenticate, authorizeAdmin, updateCategory);
 
 export default router;
