27b420e3c056bf9865e28388aa5dff14c2791b81
jardenna <hbjornum@gmail.com>
2025-03-30 13:13:47 +0200

    create category

--- src/pages/admin/CategoryPage.tsx
+++ src/pages/admin/CategoryPage.tsx
@@ -1,15 +1,36 @@
 import Form from '../../components/formElements/form/Form';
 import Input from '../../components/formElements/Input';
+import useMessagePopup from '../../components/messagePopup/useMessagePopup';
+import { useCreateCategoryMutation } from '../../features/categories/categoriyApiSlice';
 import useLanguage from '../../features/language/useLanguage';
 import useFormValidation from '../../hooks/useFormValidation';
 
+const initialState = {
+  name: '',
+};
+
 const CategoryPage = () => {
   const { language } = useLanguage();
+  const { onAddMessagePopup } = useMessagePopup();
+  const [createCategory] = useCreateCategoryMutation();
+
   const { onChange, values, onSubmit, errors } = useFormValidation({
-    initialState: {
-      categoryName: '',
-    },
+    initialState,
+    callback: handleSubmitCategory,
   });
+
+  async function handleSubmitCategory() {
+    try {
+      const result = await createCategory(values);
+      console.log(result);
+    } catch (error: any) {
+      onAddMessagePopup({
+        messagePopupType: 'error',
+        message: error.data.message,
+        componentType: 'notification',
+      });
+    }
+  }
 
   return (
     <section>
@@ -17,14 +38,15 @@ const CategoryPage = () => {
       <Form onSubmit={onSubmit} submitBtnLabel={language.save}>
         <Input
           onChange={onChange}
-          value={values.categoryName}
-          id="categoryName"
-          name="categoryName"
+          value={values.name}
+          id="name"
+          name="name"
           labelText={language.addCategory}
-          placeholder={language.categoryName}
-          errorText={errors.categoryName}
+          placeholder={language.name}
+          errorText={errors.name}
         />
       </Form>
+      <div>category list</div>
     </section>
   );
 };
