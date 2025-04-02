ccdb2a5adbe292145e5c9b788eb074b636aa2a80
jardenna <hbjornum@gmail.com>
2025-04-02 09:20:19 +0200

    validate products

---
+++ server/utils/validateProduct .js
@@ -0,0 +1,18 @@
+const validateProduct = (product) => {
+  switch (true) {
+    case !product.productName:
+      return 'Product name is required';
+    case !product.description:
+      return 'Description is required';
+    case !product.price:
+      return 'Price is required';
+    case !product.category:
+      return 'Category is required';
+    case !product.quantity:
+      return 'Quantity is required';
+    case !product.brand:
+      return 'Brand is required';
+    default:
+      return null; // No errors
+  }
+};
