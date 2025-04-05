0cca8f6202aa67d7320651096435fe4e4bef70da
jardenna <hbjornum@gmail.com>
2025-04-05 08:49:10 +0200

    productPage table

--- src/pages/admin/ProductPage.tsx
+++ src/pages/admin/ProductPage.tsx
@@ -1,10 +1,67 @@
+import { ProductResponse } from '../../app/api/apiTypes';
+import Figure from '../../components/figure/Figure';
+import Table from '../../components/sortTable/Table';
+import ProductPrice from '../../features/currency/components/ProductPrice';
+import useLanguage from '../../features/language/useLanguage';
 import { useGetAllProductsQuery } from '../../features/products/productApiSlice';
+import dateToLocaleMap from '../../utils/dates';
 
 const ProductPage = () => {
-  const { data: allProducts } = useGetAllProductsQuery();
-  console.log(allProducts);
+  const { selectedLanguage } = useLanguage();
+  const tableHeaders: {
+    key: keyof ProductResponse;
+    label: string;
+  }[] = [
+    { key: 'image', label: 'image' },
+    { key: 'productName', label: 'productName' },
+    { key: 'description', label: 'description' },
+    { key: 'price', label: 'price' },
+    { key: 'updatedAt', label: 'updatedAt' },
+    { key: 'id', label: '' },
+  ];
 
-  return <section>Products</section>;
+  const { data: allProducts } = useGetAllProductsQuery();
+
+  return (
+    <section className="product-page">
+      {allProducts && (
+        <Table
+          data={allProducts.products}
+          columns={tableHeaders}
+          tableCaption="products"
+        >
+          {(data) =>
+            data.map(
+              ({ id, productName, image, description, price, updatedAt }) => (
+                <tr key={id}>
+                  <td>
+                    <Figure src={`/images${image}`} alt="s" />
+                  </td>
+                  <td>{productName}</td>
+                  <td>{description}</td>
+                  <td>
+                    <ProductPrice price={price} />
+                  </td>
+                  <td>
+                    <span>
+                      {new Intl.DateTimeFormat(
+                        dateToLocaleMap[selectedLanguage],
+                        {
+                          day: '2-digit',
+                          month: '2-digit',
+                          year: '2-digit',
+                        },
+                      ).format(new Date(updatedAt))}
+                    </span>
+                  </td>
+                </tr>
+              ),
+            )
+          }
+        </Table>
+      )}
+    </section>
+  );
 };
 
 export default ProductPage;
