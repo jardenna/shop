f76f4ed8e121dc2fe1c32029a9fe1566307de902
jardenna <hbjornum@gmail.com>
2025-04-06 15:57:27 +0200

    Automatically update "Scheduled" categories to "Published" if the date has passed

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -10,9 +10,29 @@ import validateScheduledDate from '../utils/validateCategory.js';
 // @access  Public
 const getAllCategories = asyncHandler(async (req, res) => {
   const allCategories = await Category.find({}).lean();
-  const formattedCategories = formatMongoData(allCategories);
 
-  if (!allCategories?.length) {
+  // Automatically update "Scheduled" categories to "Published" if the date has passed
+  const now = new Date();
+  const updatedCategories = await Promise.all(
+    allCategories.map(async (category) => {
+      if (
+        category.categoryStatus === 'Scheduled' &&
+        category.scheduledDate <= now
+      ) {
+        await Category.findByIdAndUpdate(category._id, {
+          categoryStatus: 'Published',
+          scheduledDate: undefined, // Clear the scheduledDate
+        });
+        category.categoryStatus = 'Published';
+        category.scheduledDate = undefined;
+      }
+      return category;
+    }),
+  );
+
+  const formattedCategories = formatMongoData(updatedCategories);
+
+  if (!updatedCategories?.length) {
     return res.status(404).json({ message: t('noData', req.lang) });
   }
 
