a82cda268e98ca43f95e05a61adfb77cd7e5d219
jardenna <hbjornum@gmail.com>
2025-04-07 07:02:20 +0200

    moved reviews to own controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -206,55 +206,6 @@ const getNewProducts = asyncHandler(async (req, res) => {
   });
 });
 
-// @desc    Create Product Reviews
-// @route   /api/products/:id/reviews
-// @method  Post
-// @access  Public for logged-in users
-const createProductReviews = asyncHandler(async (req, res) => {
-  const { rating, comment } = req.body;
-  const product = await Product.findById(req.params.id);
-
-  if (!product) {
-    return res.status(404).json(errorResponse);
-  }
-
-  const alreadyReviewed = product.reviews.find(
-    (review) => review.user.toString() === req.user._id.toString(),
-  );
-
-  if (alreadyReviewed) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'Product already reviewed' });
-  }
-
-  const review = {
-    name: req.user.username,
-    id: req.user._id,
-    rating: Number(rating),
-    comment,
-    user: req.user._id,
-  };
-
-  product.reviews.push(review);
-  product.numReviews = product.reviews.length;
-  product.rating =
-    product.reviews.reduce((acc, item) => item.rating + acc, 0) /
-    product.reviews.length;
-
-  await Product.findByIdAndUpdate(
-    req.params.id,
-    {
-      reviews: product.reviews,
-      numReviews: product.numReviews,
-      rating: product.rating,
-    },
-    { new: true },
-  );
-
-  res.status(201).json({ message: 'Review added' });
-});
-
 // @desc    Get Product By ID
 // @route   /api/products/:id
 // @method  Get
@@ -272,7 +223,6 @@ const getProductById = asyncHandler(async (req, res) => {
 
 export {
   createProduct,
-  createProductReviews,
   deleteProduct,
   getNewProducts,
   getProductById,
