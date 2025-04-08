4389929b096d13cb315aef5fb9040ddc30220748
jardenna <hbjornum@gmail.com>
2025-04-08 10:08:09 +0200

    formlabel for datepicker

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -2,28 +2,49 @@ import { DayPicker, OnSelectHandler } from 'react-day-picker';
 import { da, enGB } from 'react-day-picker/locale';
 import 'react-day-picker/style.css';
 import useLanguage from '../../features/language/useLanguage';
+import FormLabel from '../formElements/FormLabel';
 
 type DatePickerProps = {
+  id: string;
+  labelText: string;
   onSelectDate: OnSelectHandler<Date>;
   selectedDate: Date;
+  inputHasNoLabel?: boolean;
+  required?: boolean;
 };
 
-function DatePicker({ onSelectDate, selectedDate }: DatePickerProps) {
+function DatePicker({
+  onSelectDate,
+  selectedDate,
+  required,
+  labelText,
+  id,
+  inputHasNoLabel,
+}: DatePickerProps) {
   const { selectedLanguage } = useLanguage();
 
   const locales = selectedLanguage === 'en' ? enGB : da;
 
   return (
-    <DayPicker
-      fixedWeeks
-      required
-      startMonth={new Date()}
-      mode="single"
-      selected={selectedDate}
-      onSelect={onSelectDate}
-      today={new Date()}
-      locale={locales}
-    />
+    <div className="input-container">
+      <FormLabel
+        required={required}
+        inputLabel={labelText}
+        id={id}
+        inputHasNoLabel={inputHasNoLabel}
+      />
+      <DayPicker
+        fixedWeeks
+        id={id}
+        required
+        startMonth={new Date()}
+        mode="single"
+        selected={selectedDate}
+        onSelect={onSelectDate}
+        today={new Date()}
+        locale={locales}
+      />
+    </div>
   );
 }
 
