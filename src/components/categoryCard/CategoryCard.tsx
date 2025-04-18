615b9a45953cb31fbf231ca47e36e94da3977ee6
jardenna <hbjornum@gmail.com>
2025-04-18 15:20:21 +0200

    card content

--- src/components/categoryCard/CategoryCard.tsx
+++ src/components/categoryCard/CategoryCard.tsx
@@ -7,6 +7,7 @@ import DateDisplay from '../datePicker/DateDisplay';
 import { SecondaryActionBtnProps } from '../modal/Modal';
 import ModalContainer from '../modal/ModalContainer';
 import './_category-card.scss';
+import CategoryCardContent from './CategoryCardContent';
 
 type CategoryCardProps = {
   categoryId: string;
@@ -46,85 +47,80 @@ const CategoryCard = ({
 
   return (
     <div className="category-card-container">
-      <section className="category-card card-left">
-        <span className="card-top-line" />
-        <div className="category-card-content">
-          <div className="position-relative">
-            <Badge
-              badgeText={language[status.toLocaleLowerCase()]}
-              className={status.toLowerCase()}
-            />
-            <h2 className="category-card-title">{subCategoryName}</h2>
+      <CategoryCardContent className="left">
+        <div className="position-relative">
+          <Badge
+            badgeText={language[status.toLocaleLowerCase()]}
+            className={status.toLowerCase()}
+          />
+          <h2 className="category-card-title">{subCategoryName}</h2>
 
-            <span>
-              {language.totalProducts}: {totalProducts}
-            </span>
+          <span>
+            {language.totalProducts}: {totalProducts}
+          </span>
+        </div>
+
+        <div>
+          {scheduledDate && (
+            <div className="category-date">
+              <span>{subCategoryName} er planlagt til udgivelse den </span>
+              <DateDisplay
+                date={scheduledDate}
+                hour="2-digit"
+                minute="2-digit"
+              />
+            </div>
+          )}
+          <div className="footer-buttons">
+            <ModalContainer
+              triggerModalBtnContent={language.delete}
+              triggerModalBtnVariant={BtnVariant.Danger}
+              id={categoryId}
+              primaryActionBtn={primaryActionBtn}
+              secondaryActionBtn={secondaryActionBtn}
+              modalSize={SizeVariant.Md}
+              modalHeaderText={language.deleteCategory}
+            >
+              {language.sureToDelete} {subCategoryName}
+            </ModalContainer>
+
+            <Link
+              className="btn btn-primary"
+              to={`/admin/${MainPath.AdminSubCategoryUpdate}/${categoryId}`}
+            >
+              {language.edit}
+            </Link>
           </div>
+        </div>
+      </CategoryCardContent>
 
+      <CategoryCardContent className="right">
+        <h2 className="category-card-title">{mainCategoryName}</h2>
+
+        {showStatusMessage && (
           <div>
-            {scheduledDate && (
-              <div className="category-date">
-                <span>{subCategoryName} er planlagt til udgivelse den </span>
-                <DateDisplay
-                  date={scheduledDate}
-                  hour="2-digit"
-                  minute="2-digit"
-                />
-              </div>
-            )}
-            <div className="footer-buttons">
-              <ModalContainer
-                triggerModalBtnContent={language.delete}
-                triggerModalBtnVariant={BtnVariant.Danger}
-                id={categoryId}
-                primaryActionBtn={primaryActionBtn}
-                secondaryActionBtn={secondaryActionBtn}
-                modalSize={SizeVariant.Md}
-                modalHeaderText={language.deleteCategory}
-              >
-                {language.sureToDelete} {subCategoryName}
-              </ModalContainer>
+            <div className="category-card-text">
+              <h3>
+                {language.parentCategoryIs} {language[statusMessage]}
+              </h3>
+              {subCategoryName} {language.notVisibleInShop}.
+            </div>
 
+            <div>
               <Link
                 className="btn btn-primary"
-                to={`/admin/${MainPath.AdminSubCategoryUpdate}/${categoryId}`}
+                to={`/admin/${MainPath.AdminCategories}`}
               >
-                {language.edit}
+                {language.publish}
               </Link>
             </div>
           </div>
-        </div>
-      </section>
-      <section className="category-card card-right">
-        <span className="card-top-line" />
-        <div className="category-card-content">
-          <h2 className="category-card-title">{mainCategoryName}</h2>
+        )}
 
-          {showStatusMessage && (
-            <div>
-              <div className="category-card-text">
-                <h3>
-                  {language.parentCategoryIs} {language[statusMessage]}
-                </h3>
-                {subCategoryName} {language.notVisibleInShop}.
-              </div>
-
-              <div>
-                <Link
-                  className="btn btn-primary"
-                  to={`/admin/${MainPath.AdminCategories}`}
-                >
-                  {language.publish}
-                </Link>
-              </div>
-            </div>
-          )}
-
-          <span className="category-date">
-            {language.createdAt} <DateDisplay date={createdAt} />
-          </span>
-        </div>
-      </section>
+        <span className="category-date">
+          {language.createdAt} <DateDisplay date={createdAt} />
+        </span>
+      </CategoryCardContent>
     </div>
   );
 };
