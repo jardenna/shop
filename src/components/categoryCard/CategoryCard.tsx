9f223bd8eec4b27ea64ffd78779c0e9c09122cbf
jardenna <hbjornum@gmail.com>
2025-04-18 15:06:29 +0200

    category card

--- src/components/categoryCard/CategoryCard.tsx
+++ src/components/categoryCard/CategoryCard.tsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router';
 import useLanguage from '../../features/language/useLanguage';
+import { MainPath } from '../../layout/nav/enums';
+import { BtnVariant, SizeVariant } from '../../types/enums';
 import Badge from '../badge/Badge';
-import Button from '../Button';
 import DateDisplay from '../datePicker/DateDisplay';
+import { SecondaryActionBtnProps } from '../modal/Modal';
+import ModalContainer from '../modal/ModalContainer';
 import './_category-card.scss';
 
 type CategoryCardProps = {
+  categoryId: string;
   createdAt: Date;
   mainCategoryName: string;
   scheduledDate: Date | null;
@@ -13,6 +18,7 @@ type CategoryCardProps = {
   statusMessage: string;
   subCategoryName: string;
   totalProducts: number;
+  onDeleteSubCategory: () => void;
 };
 
 const CategoryCard = ({
@@ -24,53 +30,97 @@ const CategoryCard = ({
   scheduledDate,
   statusMessage,
   status,
+  categoryId,
+  onDeleteSubCategory,
 }: CategoryCardProps) => {
   const { language } = useLanguage();
+  const primaryActionBtn = {
+    onClick: onDeleteSubCategory,
+    label: language.delete,
+    variant: BtnVariant.Danger,
+  };
+
+  const secondaryActionBtn: SecondaryActionBtnProps = {
+    label: language.cancel,
+  };
 
   return (
     <div className="category-card-container">
       <section className="category-card card-left">
         <span className="card-top-line" />
         <div className="category-card-content">
-          <h2>{subCategoryName}</h2>
-          <Badge
-            badgeText={language[status.toLocaleLowerCase()]}
-            className={status.toLowerCase()}
-          />
-          <span>{language.totalProducts}:</span>
-          <span>{totalProducts}</span>
-          {scheduledDate && (
-            <div>
-              <span>{subCategoryName} er planlagt til udgivelse den </span>
-              <DateDisplay
-                date={scheduledDate}
-                hour="2-digit"
-                minute="2-digit"
-              />
+          <div className="position-relative">
+            <Badge
+              badgeText={language[status.toLocaleLowerCase()]}
+              className={status.toLowerCase()}
+            />
+            <h2 className="category-card-title">{subCategoryName}</h2>
+
+            <span>
+              {language.totalProducts}: {totalProducts}
+            </span>
+          </div>
+
+          <div>
+            {scheduledDate && (
+              <div className="category-date">
+                <span>{subCategoryName} er planlagt til udgivelse den </span>
+                <DateDisplay
+                  date={scheduledDate}
+                  hour="2-digit"
+                  minute="2-digit"
+                />
+              </div>
+            )}
+            <div className="footer-buttons">
+              <ModalContainer
+                triggerModalBtnContent={language.delete}
+                triggerModalBtnVariant={BtnVariant.Danger}
+                id={categoryId}
+                primaryActionBtn={primaryActionBtn}
+                secondaryActionBtn={secondaryActionBtn}
+                modalSize={SizeVariant.Md}
+                modalHeaderText={language.deleteCategory}
+              >
+                {language.sureToDelete} {subCategoryName}
+              </ModalContainer>
+
+              <Link
+                className="btn btn-primary"
+                to={`/admin/${MainPath.AdminSubCategoryUpdate}/${categoryId}`}
+              >
+                {language.edit}
+              </Link>
             </div>
-          )}
+          </div>
         </div>
       </section>
       <section className="category-card card-right">
         <span className="card-top-line" />
         <div className="category-card-content">
-          <h2>{mainCategoryName}</h2>
+          <h2 className="category-card-title">{mainCategoryName}</h2>
 
           {showStatusMessage && (
             <div>
-              <p>
-                <strong>
-                  <span>{language.parentCategoryIs}</span>
-                  <span> {language[statusMessage]}</span>
-                </strong>
-              </p>
-              <span>
+              <div className="category-card-text">
+                <h3>
+                  {language.parentCategoryIs} {language[statusMessage]}
+                </h3>
                 {subCategoryName} {language.notVisibleInShop}.
-              </span>
+              </div>
+
+              <div>
+                <Link
+                  className="btn btn-primary"
+                  to={`/admin/${MainPath.AdminCategories}`}
+                >
+                  {language.publish}
+                </Link>
+              </div>
             </div>
           )}
-          <Button>{language.edit}</Button>
-          <span>
+
+          <span className="category-date">
             {language.createdAt} <DateDisplay date={createdAt} />
           </span>
         </div>
--- src/pages/subCategory/ViewSubCategoryPage.tsx
+++ src/pages/subCategory/ViewSubCategoryPage.tsx
@@ -1,18 +1,15 @@
 import { ErrorBoundary } from 'react-error-boundary';
-import { Link, useNavigate, useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import ErrorBoundaryFallback from '../../components/ErrorBoundaryFallback';
 import PageHeader from '../../components/PageHeader';
 import CategoryCard from '../../components/categoryCard/CategoryCard';
 import useMessagePopup from '../../components/messagePopup/useMessagePopup';
-import { SecondaryActionBtnProps } from '../../components/modal/Modal';
-import ModalContainer from '../../components/modal/ModalContainer';
 import useLanguage from '../../features/language/useLanguage';
 import {
   useDeleteSubCategoryMutation,
   useGetSubCategoryByIdQuery,
 } from '../../features/subCategories/subCategoryApiSlice';
 import { MainPath } from '../../layout/nav/enums';
-import { BtnVariant, SizeVariant } from '../../types/enums';
 
 const ViewSubCategoryPage = () => {
   const { language } = useLanguage();
@@ -56,16 +53,6 @@ const ViewSubCategoryPage = () => {
     }
   };
 
-  const primaryActionBtn = {
-    onClick: handleDeleteSubCategory,
-    label: language.delete,
-    variant: BtnVariant.Danger,
-  };
-
-  const secondaryActionBtn: SecondaryActionBtnProps = {
-    label: language.cancel,
-  };
-
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -83,41 +70,20 @@ const ViewSubCategoryPage = () => {
       >
         <div className="page-card">
           {!isError && category ? (
-            <>
-              <CategoryCard
-                createdAt={category.createdAt}
-                subCategoryName={category.subCategoryName}
-                totalProducts={category.productCount}
-                mainCategoryName={category.mainCategory.categoryName}
-                showStatusMessage={
-                  category.mainCategory.categoryStatus !== 'Published'
-                }
-                scheduledDate={category.scheduledDate || null}
-                statusMessage={category.mainCategory.categoryStatus.toLocaleLowerCase()}
-                status={category.categoryStatus}
-              />
-
-              <div className="footer-buttons">
-                <ModalContainer
-                  triggerModalBtnContent={language.delete}
-                  triggerModalBtnVariant={BtnVariant.Danger}
-                  id={category.id}
-                  primaryActionBtn={primaryActionBtn}
-                  secondaryActionBtn={secondaryActionBtn}
-                  modalSize={SizeVariant.Md}
-                  modalHeaderText={language.deleteCategory}
-                >
-                  {language.sureToDelete} {category.subCategoryName}
-                </ModalContainer>
-
-                <Link
-                  className="btn btn-primary"
-                  to={`/admin/${MainPath.AdminSubCategoryUpdate}/${category.id}`}
-                >
-                  {language.edit}
-                </Link>
-              </div>
-            </>
+            <CategoryCard
+              onDeleteSubCategory={handleDeleteSubCategory}
+              categoryId={category.id}
+              createdAt={category.createdAt}
+              subCategoryName={category.subCategoryName}
+              totalProducts={category.productCount}
+              mainCategoryName={category.mainCategory.categoryName}
+              showStatusMessage={
+                category.mainCategory.categoryStatus !== 'Published'
+              }
+              scheduledDate={category.scheduledDate || null}
+              statusMessage={category.mainCategory.categoryStatus.toLocaleLowerCase()}
+              status={category.categoryStatus}
+            />
           ) : (
             <span>error..</span>
           )}
