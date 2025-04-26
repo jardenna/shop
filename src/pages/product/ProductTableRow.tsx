ffe52b0442c633801aec68ac9d8f1fda6d8e50cc
jardenna <hbjornum@gmail.com>
2025-04-26 10:12:59 +0200

    ProductTableRow

---
+++ src/pages/product/ProductTableRow.tsx
@@ -0,0 +1,65 @@
+import { Status } from '../../app/api/apiTypes';
+import CardBadge from '../../components/card/CardBadge';
+import Img from '../../components/Img';
+import MoreLink from '../../components/MoreLink';
+import ProductPrice from '../../features/currency/components/ProductPrice';
+import useLanguage from '../../features/language/useLanguage';
+import { MainPath } from '../../layout/nav/enums';
+
+type SubCategoryTableRowProps = {
+  categoryName: string;
+  countInStock: number;
+  id: string;
+  imageUrl: string;
+  price: number;
+  productName: string;
+  productStatus: Status;
+  scheduledDate: Date | null;
+  subCategoryName: string;
+};
+
+const ProductTableRow = ({
+  id,
+  productName,
+  imageUrl,
+  price,
+  subCategoryName,
+  categoryName,
+  countInStock,
+  productStatus,
+  scheduledDate,
+}: SubCategoryTableRowProps) => {
+  const { language } = useLanguage();
+  return (
+    <tr>
+      <td>
+        <div className="product-overview-cell">
+          <Img src={imageUrl} alt="" />
+          <span className="product-name">{productName}</span>
+        </div>
+      </td>
+      <td>
+        {subCategoryName} / {categoryName}
+      </td>
+      <td>
+        <ProductPrice price={price} />
+      </td>
+      <td>{countInStock}</td>
+      <td>
+        <CardBadge
+          badgeClassName={productStatus.toLowerCase()}
+          badgeText={language[productStatus.toLocaleLowerCase()]}
+          scheduledDate={scheduledDate || null}
+        />
+      </td>
+      <td>
+        <MoreLink
+          linkText={language.viewProduct}
+          linkTo={`/admin/${MainPath.AdminProductView}/${id}`}
+        />
+      </td>
+    </tr>
+  );
+};
+
+export default ProductTableRow;
