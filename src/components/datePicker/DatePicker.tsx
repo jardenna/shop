d0d40770e11bb1409edfae4bf3bc39c2b9b64800
jardenna <hbjornum@gmail.com>
2025-05-04 17:50:03 +0200

    label for datepicker

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -2,18 +2,27 @@ import { DayPicker, OnSelectHandler } from 'react-day-picker';
 import { da, enGB } from 'react-day-picker/locale';
 import 'react-day-picker/style.css';
 import useLanguage from '../../features/language/useLanguage';
+import FormLabel from '../formElements/FormLabel';
 import './_date-picker.scss';
 
 type DatePickerProps = {
   labelText: string;
   onSelectDate: OnSelectHandler<Date>;
   selectedDate: Date;
+  errorText?: string;
+  id?: string;
+  inputHasNoLabel?: boolean;
+  required?: boolean;
 };
 
 function DatePicker({
   onSelectDate,
   selectedDate,
   labelText,
+  id,
+  required,
+  inputHasNoLabel,
+  errorText,
 }: DatePickerProps) {
   const { selectedLanguage } = useLanguage();
 
@@ -21,7 +30,13 @@ function DatePicker({
 
   return (
     <div className="input-container date-picker-container">
-      <span>{labelText}</span>
+      <FormLabel
+        required={required}
+        labelText={labelText}
+        id={id || ''}
+        inputHasNoLabel={inputHasNoLabel}
+        errorText={errorText}
+      />
       <DayPicker
         fixedWeeks
         required
