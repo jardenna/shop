02a0b88f05464a5d0a26012cd3a8d68dbd81d0cc
jardenna <hbjornum@gmail.com>
2025-05-09 14:40:56 +0200

    skeleton items

---
+++ src/components/skeleton/SkeletonItems.tsx
@@ -0,0 +1,178 @@
+import './_skeleton-items.scss';
+
+const SkeletonItems = () => (
+  <section>
+    <div className="search-ssc-lg ssc">
+      <div className="ssc-card ssc-wrapper">
+        <div className="flex mbs">
+          <div className="tag ssc-square" />
+          <div className="tag ssc-square" />
+          <div className="tag ssc-square" />
+          <div className="tag ssc-square" />
+          <div className="tag ssc-square" />
+        </div>
+      </div>
+      <br />
+      <div className="ssc-hr" />
+      <br />
+      <div className="ssc-card ssc-wrapper">
+        <div className="ssc-head-line w-50" />
+        <br />
+        <div className="flex align-start justify-between">
+          <div className="ssc-card ssc-wrapper w-30">
+            <div className="ssc-head-line mb" />
+            <div className="ssc-line w-80 mbs" />
+            <div className="ssc-line w-40 mbs" />
+            <div className="ssc-line w-60 mbs" />
+          </div>
+          <div className="ssc-card ssc-wrapper w-30">
+            <div className="ssc-head-line mb" />
+            <div className="ssc-line w-70 mbs" />
+            <div className="ssc-line w-30 mbs" />
+            <div className="ssc-line w-90 mbs" />
+          </div>
+          <div className="ssc-card ssc-wrapper w-30">
+            <div className="ssc-head-line mb" />
+            <div className="ssc-line w-60 mbs" />
+            <div className="ssc-line w-90 mbs" />
+            <div className="ssc-line w-30 mbs" />
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="ssc-hr" />
+      <br />
+      <div className="flex align-start">
+        <div className="ssc-card w-30 mr">
+          <div className="ssc-wrapper flex justify-between">
+            <div className="w-60 mr">
+              <div className="ssc-line mb w-50" />
+              <div className="ssc-line mb w-70" />
+              <div className="ssc-line w-90" />
+            </div>
+            <div className="ssc-circle" style={{ height: 70, width: 70 }} />
+          </div>
+          <div className="ssc-hr" />
+          <div className="ssc-wrapper">
+            <div className="flex align-center justify-between">
+              <div className="ssc-head-line w-60 mr" />
+              <div className="ssc-line w-30" />
+            </div>
+          </div>
+          <div className="ssc-hr" />
+          <div className="ssc-wrapper">
+            <div className="ssc-line w-100 mb" />
+            <div className="ssc-line w-70 mb" />
+            <div className="ssc-line w-30 mb" />
+            <div className="ssc-line w-80 mb" />
+            <div className="ssc-line w-60 mb" />
+          </div>
+          <div className="ssc-hr" />
+          <div className="ssc-wrapper">
+            <div className="ssc-line w-100 mb" />
+            <div className="ssc-line w-70 mb" />
+            <div className="ssc-line w-30 mb" />
+            <div className="ssc-line w-80 mb" />
+            <div className="ssc-line w-60 mb" />
+          </div>
+          <div className="ssc-hr" />
+          <div className="ssc-wrapper">
+            <div className="ssc-line w-100 mb" />
+            <div className="ssc-line w-70 mb" />
+            <div className="ssc-line w-30 mb" />
+            <div className="ssc-line w-80 mb" />
+            <div className="ssc-line w-60 mb" />
+          </div>
+        </div>
+        <div className="w-100">
+          <div className="ssc-card ssc-wrapper">
+            <div className="ssc-head-line" />
+            <br />
+            <div className="ssc-square" />
+          </div>
+          <br />
+          <div className="ssc-card ssc-wrapper">
+            <div className="ssc-head-line" />
+            <br />
+            <div className="ssc-square" />
+          </div>
+          <br />
+          <div className="ssc-card ssc-wrapper">
+            <div className="ssc-head-line" />
+            <br />
+            <div className="ssc-square" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="search-ssc-sm ssc">
+      <div className="ssc-card ssc-wrapper">
+        <div className="ssc-line w-70" />
+      </div>
+      <br />
+      <div className="ssc-card ssc-wrapper flex justify-between">
+        <div className="w-70">
+          <div className="ssc-line mb w-50" />
+          <div className="ssc-line mb w-70" />
+          <div className="ssc-line w-90" />
+        </div>
+        <div className="ssc-circle" style={{ height: 70, width: 70 }} />
+      </div>
+      <br />
+      <div className="ssc-hr" />
+      <br />
+      <div className="ssc-card ssc-wrapper">
+        <div className="ssc-head-line mb" />
+        <div className="ssc-square mb" />
+        <div className="ssc-line mb w-50" />
+        <div className="ssc-line mb w-80" />
+        <div className="ssc-line mb w-30" />
+      </div>
+      <br />
+      <div className="ssc-card">
+        <div className="ssc-wrapper">
+          <div className="flex align-center justify-between">
+            <div className="ssc-head-line w-60 mr" />
+            <div className="ssc-line w-30" />
+          </div>
+        </div>
+        <div className="ssc-hr" />
+        <div className="ssc-wrapper">
+          <div className="ssc-line w-100 mb" />
+          <div className="ssc-line w-70 mb" />
+          <div className="ssc-line w-30 mb" />
+          <div className="ssc-line w-80 mb" />
+          <div className="ssc-line w-60 mb" />
+        </div>
+        <div className="ssc-hr" />
+        <div className="ssc-wrapper">
+          <div className="ssc-line w-100 mb" />
+          <div className="ssc-line w-70 mb" />
+          <div className="ssc-line w-30 mb" />
+          <div className="ssc-line w-80 mb" />
+          <div className="ssc-line w-60 mb" />
+        </div>
+        <div className="ssc-hr" />
+        <div className="ssc-wrapper">
+          <div className="ssc-line w-100 mb" />
+          <div className="ssc-line w-70 mb" />
+          <div className="ssc-line w-30 mb" />
+          <div className="ssc-line w-80 mb" />
+          <div className="ssc-line w-60 mb" />
+        </div>
+      </div>
+      <br />
+      <div className="ssc-card ssc-wrapper">
+        <div className="search-ssc-sm__tags flex" style={{ flexWrap: 'wrap' }}>
+          <div className="tag ssc-square w-30" />
+          <div className="tag ssc-square w-70" />
+          <div className="tag ssc-square w-50" />
+          <div className="tag ssc-square w-20" />
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default SkeletonItems;
