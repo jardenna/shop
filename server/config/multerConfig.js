27ddb230e155c8331803a4c9a63a3f80fa6445b6
jardenna <hbjornum@gmail.com>
2025-05-10 17:36:36 +0200

    moved multer logic to config

---
+++ server/config/multerConfig.js
@@ -0,0 +1,38 @@
+import multer from 'multer';
+import path from 'path';
+import { t } from '../utils/translator.js';
+
+const storage = multer.diskStorage({
+  destination: './public/images/uploads',
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + '-' + Date.now() + path.extname(file.originalname),
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpeg|jpg|png|webp|gif/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+  const extname = path.extname(file.originalname).toLowerCase();
+  const mimetype = file.mimetype;
+
+  const fileName = file.originalname;
+  const splitFileName = fileName.split('.')[1];
+
+  if (filetypes.test(extname) && mimetypes.test(mimetype)) {
+    cb(null, true);
+  } else {
+    return cb(
+      new Error(
+        `${splitFileName} ${t('unsupportedFile', req.lang)} . 
+        ${t('allowedFormats', req.lang)}: JPEG, JPG, PNG, WEBP, GIF.`,
+      ),
+    );
+  }
+};
+
+const fileSize = 1 * 1000 * 1000; // 1 MB file size limit
+
+export const upload = multer({ storage, fileFilter, limits: { fileSize } });
