751b7763738ef0e1818af8059690858b2e10ace0
jardenna <hbjornum@gmail.com>
2025-05-11 09:53:07 +0200

    pagelist

--- src/components/skeleton/SkeletonPageList.tsx
+++ src/components/skeleton/SkeletonPageList.tsx
@@ -2,13 +2,15 @@ const SkeletonPageList = () => (
   <div className="page-card">
     <span className="card-top-line" />
     <div className="flex">
-      <div className="page-card skeleton-card">
+      <div className="page-card flex-1">
         <div>img</div>
         <div>text badge</div>
         <div>footer</div>
       </div>
-      <div className="page-card skeleton-card">Center</div>
-      <div className="page-card skeleton-card">Right</div>
+      <div className="page-card flex-1">Center</div>
+      <div className="page-card flex-1" style={{ maxWidth: '25rem' }}>
+        Right
+      </div>
     </div>
   </div>
 );
