d1f2722aaf527840c168cb9ce1b5e82f4a5c5ba5
jardenna <hbjornum@gmail.com>
2025-05-14 09:01:52 +0200

    removed dropdown

--- src/components/formElements/fileInput/ProductImgList.tsx
+++ src/components/formElements/fileInput/ProductImgList.tsx
@@ -1,16 +1,14 @@
 import { ReactNode } from 'react';
-import useLanguage from '../../../features/language/useLanguage';
-import { BtnVariant, IconName } from '../../../types/enums';
-import Dropdown from '../../dropdown/Dropdown';
-import Icon from '../../icons/Icon';
+import { IconName } from '../../../types/enums';
+import IconBtn from '../../IconBtn';
 import Img from '../../Img';
 
 type ProductImgListProps = {
   ariaLabel: string;
+  children: ReactNode;
   img: string;
-  onClick: any;
   title: string;
-  children?: ReactNode;
+  onClick: () => void;
 };
 
 const ProductImgList = ({
@@ -19,26 +17,17 @@ const ProductImgList = ({
   children,
   ariaLabel,
   title,
-}: ProductImgListProps) => {
-  const { language } = useLanguage();
-  return (
-    <li className="preview-item">
-      <Img className="preview-img" src={img} alt="" />
-      {children && children}
-      <Dropdown
-        ariaControls="delete-image"
-        text={`${language.sureToDelete}?`}
-        triggerBtnVariant={BtnVariant.Ghost}
-        triggerBtnClassName="danger"
-        onPrimaryClick={onClick}
-        primaryBtnLabel={language.delete}
-        primaryBtnVariant={BtnVariant.Danger}
-        ariaLabel={ariaLabel}
-      >
-        <Icon iconName={IconName.Trash} title={title} ariaLabel={ariaLabel} />
-      </Dropdown>
-    </li>
-  );
-};
+}: ProductImgListProps) => (
+  <li className="preview-item">
+    <Img className="preview-img" src={img} alt="" />
+    {children}
+    <IconBtn
+      onClick={onClick}
+      iconName={IconName.Trash}
+      title={title}
+      ariaLabel={ariaLabel}
+    />
+  </li>
+);
 
 export default ProductImgList;
