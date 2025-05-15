0e52448c0aa13412a66dc59b8174bde373412f00
jardenna <hbjornum@gmail.com>
2025-05-15 09:27:28 +0200

    className ProductPrice

--- src/features/currency/components/ProductPrice.tsx
+++ src/features/currency/components/ProductPrice.tsx
@@ -1,9 +1,14 @@
 import useCurrency from '../useCurrency';
 
-const ProductPrice = ({ price }: { price: number }) => {
+type ProductPriceProps = {
+  price: number;
+  className?: string;
+};
+
+const ProductPrice = ({ price, className = '' }: ProductPriceProps) => {
   const { convertedPrice } = useCurrency(price);
 
-  return <span>{convertedPrice}</span>;
+  return <span className={className}>{convertedPrice}</span>;
 };
 
 export default ProductPrice;
