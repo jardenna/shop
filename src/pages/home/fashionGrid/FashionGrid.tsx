f8e9003ce224ac5da830511a8944dd2ea3585833
jardenna <hbjornum@gmail.com>
2025-06-02 12:17:49 +0200

    fashion grid

--- src/pages/home/fashionGrid/FashionGrid.tsx
+++ src/pages/home/fashionGrid/FashionGrid.tsx
@@ -1,6 +1,6 @@
-import Img from '../../../components/Img';
 import useLanguage from '../../../features/language/useLanguage';
 import './_fashion-grid.scss';
+import FashionGridItem from './FashionGridItem';
 
 const FashionGrid = () => {
   const { language } = useLanguage();
@@ -8,54 +8,41 @@ const FashionGrid = () => {
   return (
     <section>
       <article className="fashion-grid">
-        <section className="grid-item-1">
-          <Img src="/images/home/topbanner1.png" alt="" />
-          <div className="caption-text ">
-            <h2>{language.shopNewHeading}</h2>
+        <FashionGridItem
+          className="grid-item-1"
+          heading={language.shopNewHeading}
+          linkTo="/"
+          linkText={language.shopNewLink}
+          subHading="NewArrivals"
+        >
+          <p>{language.shopNewText}</p>
+        </FashionGridItem>
+        <FashionGridItem
+          className="grid-item-2"
+          heading={language.shopMenHeading}
+          linkTo="/"
+          linkText={language.shopMenLink}
+        >
+          <p>{language.shopMenText}</p>
+        </FashionGridItem>
 
-            <h3>NewArrivals</h3>
-
-            <p className="banner_text">{language.shopNewText}</p>
-
-            <a href="/collections/toddler-girl">{language.shopNewLink}</a>
-          </div>
-        </section>
-
-        <section className="grid-item-2">
-          <div className="caption-text ">
-            <h2>{language.shopMenHeading}</h2>
-
-            <h3>NewArrivals</h3>
-
-            <p className="banner_text">{language.shopMenText}</p>
-
-            <a href="/collections/toddler-girl">{language.shopMenLink}</a>
-          </div>
-        </section>
         <div className="grid-flex">
-          <section className="grid-item-3">
-            <div className="caption-text ">
-              <h2>{language.shopWomanHeading}</h2>
-
-              <h3>NewArrivals</h3>
-
-              <p className="banner_text">{language.shopWomanText}</p>
-
-              <a href="/collections/toddler-girl">{language.shopWomanLink}</a>
-            </div>
-          </section>
-
-          <section className="grid-item-4">
-            <div className="caption-text ">
-              <h2>{language.shopKidsHeading}</h2>
-
-              <h3>NewArrivals</h3>
-
-              <p className="banner_text">{language.shopKidsText}</p>
-
-              <a href="/collections/toddler-girl">{language.shopKidsLink}</a>
-            </div>
-          </section>
+          <FashionGridItem
+            className="grid-item-3"
+            heading={language.shopWomanHeading}
+            linkTo="/"
+            linkText={language.shopWomanLink}
+          >
+            <p>{language.shopWomanText}</p>
+          </FashionGridItem>
+          <FashionGridItem
+            className="grid-item-4"
+            heading={language.shopKidsHeading}
+            linkTo="/"
+            linkText={language.shopKidsLink}
+          >
+            <p>{language.shopWomanText}</p>
+          </FashionGridItem>
         </div>
       </article>
     </section>
