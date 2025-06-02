0d28f08810dd20f2e2523fdb6c17ce9adb4b5163
jardenna <hbjornum@gmail.com>
2025-06-02 16:52:37 +0200

    fashion grid

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,6 +4,40 @@ import Hero from './Hero';
 const HomePage = () => (
   <section className="container">
     <Hero />
+    {/* <ul>
+      <li>
+        <div className="service_content">
+          <div className="service_title1">Easy free delivery</div>
+          <div className="service_title2">
+            Online shopping from a great selection at free delivery
+          </div>
+        </div>
+      </li>
+      <li>
+        <div className="service_content">
+          <div className="service_title1">Easy free delivery</div>
+          <div className="service_title2">
+            Online shopping from a great selection at free delivery
+          </div>
+        </div>
+      </li>
+      <li>
+        <div className="service_content">
+          <div className="service_title1">Easy free delivery</div>
+          <div className="service_title2">
+            Online shopping from a great selection at free delivery
+          </div>
+        </div>
+      </li>
+      <li>
+        <div className="service_content">
+          <div className="service_title1">Easy free delivery</div>
+          <div className="service_title2">
+            Online shopping from a great selection at free delivery
+          </div>
+        </div>
+      </li>
+    </ul> */}
     <FashionGrid />
   </section>
 );
