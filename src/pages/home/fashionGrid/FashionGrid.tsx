2921fbfa91ac7f15420d95bbe28c356238857148
jardenna <hbjornum@gmail.com>
2025-06-02 09:08:59 +0200

    home text

--- src/pages/home/fashionGrid/FashionGrid.tsx
+++ src/pages/home/fashionGrid/FashionGrid.tsx
@@ -1,67 +1,65 @@
 import Img from '../../../components/Img';
+import useLanguage from '../../../features/language/useLanguage';
 import './_fashion-grid.scss';
 
-const FashionGrid = () => (
-  <section>
-    <article className="fashion-grid">
-      <section className="grid-item-1">
-        <Img src="/images/home/topbanner1.png" alt="" />
-        <div className="caption-text ">
-          <h2>Celebrating life</h2>
+const FashionGrid = () => {
+  const { language } = useLanguage();
 
-          <h3>NewArrivals</h3>
-
-          <p className="banner_text">
-            Discover style made to move with your passions and celebrate what
-            makes you unique.
-          </p>
-
-          <a href="/collections/toddler-girl">Discover Collection</a>
-        </div>
-      </section>
-
-      <section className="grid-item-2">
-        <div className="caption-text">
-          <h2>Newborn baby clothes</h2>
-
-          <p className="banner_text">Limited time only!</p>
-
-          <a href="/collections/kid-boy">Shop now!</a>
-        </div>
-      </section>
-      <div className="grid-flex">
-        <section className="grid-item-3">
+  return (
+    <section>
+      <article className="fashion-grid">
+        <section className="grid-item-1">
+          <Img src="/images/home/topbanner1.png" alt="" />
           <div className="caption-text ">
-            <h4>SPRiNG TIME COLLECTION</h4>
+            <h2>{language.shopNewHeading}</h2>
 
-            <h2>NewArrivals</h2>
+            <h3>NewArrivals</h3>
 
-            <p className="banner_text">
-              Season Sale!
-              <span>Save 50%</span> on all Items
-            </p>
+            <p className="banner_text">{language.shopNewText}</p>
 
-            <a href="/collections/toddler-girl">Shop now!</a>
+            <a href="/collections/toddler-girl">{language.shopNewLink}</a>
           </div>
         </section>
 
-        <section className="grid-item-4">
+        <section className="grid-item-2">
           <div className="caption-text ">
-            <h4>SPRiNG TIME COLLECTION</h4>
+            <h2>{language.shopMenHeading}</h2>
 
-            <h2>NewArrivals</h2>
+            <h3>NewArrivals</h3>
 
-            <p className="banner_text">
-              Season Sale!
-              <span>Save 50%</span> on all Items
-            </p>
+            <p className="banner_text">{language.shopMenText}</p>
 
-            <a href="/collections/toddler-girl">Shop now!</a>
+            <a href="/collections/toddler-girl">{language.shopMenLink}</a>
           </div>
         </section>
-      </div>
-    </article>
-  </section>
-);
+        <div className="grid-flex">
+          <section className="grid-item-3">
+            <div className="caption-text ">
+              <h2>{language.shopWomanHeading}</h2>
+
+              <h3>NewArrivals</h3>
+
+              <p className="banner_text">{language.shopWomanText}</p>
+
+              <a href="/collections/toddler-girl">{language.shopWomanLink}</a>
+            </div>
+          </section>
+
+          <section className="grid-item-4">
+            <div className="caption-text ">
+              <h2>{language.shopKidsHeading}</h2>
+
+              <h3>NewArrivals</h3>
+
+              <p className="banner_text">{language.shopKidsText}</p>
+
+              <a href="/collections/toddler-girl">{language.shopKidsLink}</a>
+            </div>
+          </section>
+        </div>
+      </article>
+    </section>
+  );
+};
 
 export default FashionGrid;
