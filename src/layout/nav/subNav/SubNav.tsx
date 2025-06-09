c088b8f9f5f5aff3021470e3d1ba24a57654022c
jardenna <hbjornum@gmail.com>
2025-06-09 17:50:16 +0200

    sub menu

--- src/layout/nav/subNav/SubNav.tsx
+++ src/layout/nav/subNav/SubNav.tsx
@@ -1,23 +1,53 @@
+import { NavLink } from 'react-router';
+import Button from '../../../components/Button';
 import Img from '../../../components/Img';
+import useLanguage from '../../../features/language/useLanguage';
+import { LinkText, MainPath } from '../enums';
 import './_sub-menu.scss';
 
-const SubNav = () => (
-  <div className="sub-menu-container">
-    <article className="sub-menu">
-      <section className="menu-ad">
-        <div>
-          <Img src="/images/ad.png" alt="man and woman" />
-        </div>
-        <div>
-          <h2>Discover latest</h2>
-          <p>og noget text</p>
-        </div>
-      </section>
-      <section>2</section>
-      <section>3</section>
-      <section>4</section>
-    </article>
-  </div>
-);
+const subNav = [
+  {
+    path: MainPath.Root,
+    linkText: LinkText.Women,
+    infoText: 'Opdag stilfulde nyheder, must-haves og sæsonens trends.',
+  },
+  {
+    path: MainPath.Collection,
+    linkText: LinkText.Men,
+    infoText: 'Fra hverdagslook til formelt, alt til den moderne mand.',
+  },
+  {
+    path: MainPath.Collection,
+    linkText: LinkText.Kids,
+    infoText: 'Komfortabelt og sjovt tøj til leg, skole og eventyr.',
+  },
+];
 
+const SubNav = () => {
+  const { language } = useLanguage();
+
+  return (
+    <div className="sub-menu-container">
+      <article className="sub-menu">
+        <div className="grid-line" />
+        <section className="menu-item img-item">
+          <Img src="/images/ad.png" alt="man and woman" />
+          <div>
+            <h2>Discover latest trends</h2>
+          </div>
+        </section>
+        {subNav.map(({ linkText, path, infoText }) => (
+          <NavLink to={path} className="menu-item" key={linkText}>
+            <h2 className="link-heading">{language[linkText]}</h2>
+            <p className="link-text">{infoText}</p>
+
+            <div className="nav-link">
+              <Button>Shop nu</Button>
+            </div>
+          </NavLink>
+        ))}
+      </article>
+    </div>
+  );
+};
 export default SubNav;
