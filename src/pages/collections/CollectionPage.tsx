7678682ac31f517120c12bdfccf48ea3adb63471
jardenna <hbjornum@gmail.com>
2025-06-18 18:04:26 +0200

    data

--- src/pages/collections/CollectionPage.tsx
+++ src/pages/collections/CollectionPage.tsx
@@ -1,12 +1,18 @@
 import { useParams } from 'react-router';
 import useLanguage from '../../features/language/useLanguage';
+import { useGetShopProductsQuery } from '../../features/products/productApiSlice';
 import MainPageContainer from '../pageContainer/MainPageContainer';
 import './_collection-page.scss';
 
 const CollectionPage = () => {
   const { language } = useLanguage();
   const { category } = useParams();
-  console.log(category);
+
+  const { data: products } = useGetShopProductsQuery({
+    pageSize: '100',
+    mainCategory: category,
+  });
+  console.log(products);
 
   return (
     <MainPageContainer heading={language.collection}>
