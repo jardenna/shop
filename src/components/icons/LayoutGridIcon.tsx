6bdd19bae033ce4f4ce2fbecadb735cb8ee22b34
jardenna <hbjornum@gmail.com>
2025-06-22 09:39:15 +0200

    layout icon

--- src/components/icons/LayoutGridIcon.tsx
+++ src/components/icons/LayoutGridIcon.tsx
@@ -13,17 +13,20 @@ const LayoutGridIcon = ({
     height={size}
     className={className}
     viewBox="0 0 15 15"
-    fill="currentcolor"
+    fill="currentColor"
     stroke="none"
     aria-hidden={ariaHidden}
   >
     <title>{title}</title>
-    <rect x="6.09106" y="1.36353" width="9.54545" height="1.36364" />
-    <rect x="6.09106" y="6.81812" width="9.54545" height="1.36364" />
-    <rect x="6.09106" y="12.2727" width="9.54545" height="1.36364" />
-    <rect x="0.636475" width="4.09091" height="4.09091" />
-    <rect x="0.636475" y="5.45459" width="4.09091" height="4.09091" />
-    <rect x="0.636475" y="10.9092" width="4.09091" height="4.09091" />
+    <rect x="10.9091" width="4.09091" height="4.09091" />
+    <rect x="10.9091" y="5.45459" width="4.09091" height="4.09091" />
+    <rect x="10.9091" y="10.9092" width="4.09091" height="4.09091" />
+    <rect x="5.45459" width="4.09091" height="4.09091" />
+    <rect x="5.45459" y="5.45459" width="4.09091" height="4.09091" />
+    <rect x="5.45459" y="10.9092" width="4.09091" height="4.09091" />
+    <rect width="4.09091" height="4.09091" />
+    <rect y="5.45459" width="4.09091" height="4.09091" />
+    <rect y="10.9092" width="4.09091" height="4.09091" />
   </svg>
 );
 
