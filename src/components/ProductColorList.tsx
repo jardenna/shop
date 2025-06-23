fb9b803f697632347f67dfbb4a0a048ded67adc8
jardenna <hbjornum@gmail.com>
2025-06-23 10:34:14 +0200

    a11y for color and sizes

--- src/components/ProductColorList.tsx
+++ src/components/ProductColorList.tsx
@@ -1,4 +1,6 @@
+import useLanguage from '../features/language/useLanguage';
 import variables from '../scss/variables.module.scss';
+import VisuallyHidden from './VisuallyHidden';
 
 type OptionSize = 'small' | '';
 
@@ -14,9 +16,13 @@ const ProductColorList = ({
   optionSize = '',
 }: ProductColorListProps) => {
   console.log(colours.length, count);
+  const { language } = useLanguage();
 
   return (
-    <ul className="color-list">
+    <ul
+      className={`color-list ${optionSize}`}
+      aria-label={language.availableColors}
+    >
       {colours.map((colour) => (
         <li
           key={colour}
@@ -24,8 +30,10 @@ const ProductColorList = ({
             backgroundColor: colour,
             borderColor: colour === 'white' ? variables.colorIconBorder : '',
           }}
-          className={`option-box ${optionSize}`}
-        />
+          className="option-box"
+        >
+          <VisuallyHidden>{colour}</VisuallyHidden>
+        </li>
       ))}
     </ul>
   );
