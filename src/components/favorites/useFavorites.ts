014d469ba3d468b8565cf91e15d9b57958395163
jardenna <hbjornum@gmail.com>
2025-06-28 09:41:43 +0200

    naming

--- src/components/favorites/useFavorites.ts
+++ src/components/favorites/useFavorites.ts
@@ -13,37 +13,38 @@ export const useFavorites = ({ id }: { id?: string }) => {
 
   const isFavorite = (productId: string) =>
     favorites.some((product) => product.id === productId);
+
   const [animate, setAnimate] = useState(isFavorite(id || ''));
 
-  const [optimisticPosts, addOptimisticPost] = useOptimistic(
+  const [optimisticFavorites, setOptimisticFavorites] = useOptimistic(
     favorites,
-    (state: Favorites[], postId: string) =>
-      state.map((post) => {
-        if (post.id === String(postId)) {
+    (favorites: Favorites[], productId: string) =>
+      favorites.map((favorite) => {
+        if (favorite.id === productId) {
           return {
-            ...post,
-            isLiked: !post.id,
+            ...favorite,
+            isLiked: !favorite.id,
           };
         }
-        return post;
+        return favorite;
       }),
   );
 
   const handleToggle = (productId: string) => {
     try {
       startTransition(async () => {
-        addOptimisticPost(productId);
+        setOptimisticFavorites(productId);
         await toggleFavorite(productId).unwrap();
       });
 
       setAnimate(!animate);
     } catch (error) {
-      console.error('Failed to toggle favorite:', error);
+      console.error(error);
     }
   };
 
   return {
-    favorites: optimisticPosts,
+    favorites: optimisticFavorites,
     isFavorite,
     toggleFavorite: handleToggle,
     isLoading,
