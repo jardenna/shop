3676e1bf979e18294e00439fdcd92a903071824e
jardenna <hbjornum@gmail.com>
2025-07-03 18:01:47 +0200

    moved files

--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -1,27 +1,22 @@
 import { ErrorBoundary } from 'react-error-boundary';
-import { Link, useParams } from 'react-router';
+import { useParams } from 'react-router';
 import Breadcrumbs from '../components/breadcrumbs/Breadcrumbs';
 import { routeBreadcrumbs } from '../components/breadcrumbs/breadcrumbsRoutes';
 import ErrorBoundaryFallback from '../components/ErrorBoundaryFallback';
-import Favorites from '../components/favorites/Favorites';
 import Img from '../components/Img';
-import ProductColorList from '../components/ProductColorList';
-import ProductSizeList from '../components/productSizeList/ProductSizeList';
 import Skeleton from '../components/skeleton/Skeleton';
 import useLanguage from '../features/language/useLanguage';
 import CollectionAside from '../features/shop/components/CollectionAside';
 import FilterPanel from '../features/shop/components/FilterPanel';
+import ProductCard from '../features/shop/components/ProductCard';
 import ProductViews from '../features/shop/components/ProductViews';
-import SizeOverlay from '../features/shop/components/SizeOverlay';
 import {
   useGetProductsQuery,
   useGetShopMenuQuery,
 } from '../features/shop/shopApiSlice';
 import useLocalStorage, { localStorageKeys } from '../hooks/useLocalStorage';
-import { ShopPath } from '../layout/nav/enums';
 import MetaTags from '../layout/nav/MetaTags';
 import './_collection-page.scss';
-import ProductDiscountPrice from './product/ProductDiscountPrice';
 
 const CollectionPage = () => {
   const { language } = useLanguage();
@@ -96,50 +91,11 @@ const CollectionPage = () => {
                     className={`product-card-list ${displayList ? 'list' : ''}`}
                   >
                     {products?.products.map((product) => (
-                      <section className="product-card" key={product.id}>
-                        <div className="img-container">
-                          <Favorites id={product.id} />
-                          {product.discount > 0 && (
-                            <span className="product-badge">
-                              - {product.discount} %
-                            </span>
-                          )}
-                          <Link to={`/${ShopPath.Product}/${product.id}`}>
-                            <Img
-                              alt=""
-                              src={product.images[0]}
-                              className="product-card-img"
-                            />
-                          </Link>
-                          {!displayList && (
-                            <div className="product-overlay-items">
-                              <SizeOverlay sizes={product.sizes} />
-                            </div>
-                          )}
-                        </div>
-                        <div className="product-card-content">
-                          <h2 className="product-card-title">
-                            {product.productName}
-                          </h2>
-
-                          {displayList && <p>{product.description}</p>}
-                          <ProductDiscountPrice
-                            price={product.price}
-                            discount={product.discount || null}
-                          />
-                          {displayList && (
-                            <ProductSizeList
-                              sizes={product.sizes}
-                              variant="shop-product"
-                            />
-                          )}
-                          <ProductColorList
-                            colours={product.colors}
-                            count={3}
-                            optionSize={displayList ? '' : 'small'}
-                          />
-                        </div>
-                      </section>
+                      <ProductCard
+                        product={product}
+                        key={product.id}
+                        displayList={productView === 'list'}
+                      />
                     ))}
                   </div>
                 </div>
