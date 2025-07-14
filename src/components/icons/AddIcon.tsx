aec5703bf9654c7623167830c620d3932c518240
jardenna <hbjornum@gmail.com>
2025-07-14 11:05:17 +0200

    add icon

--- src/components/icons/AddIcon.tsx
+++ src/components/icons/AddIcon.tsx
@@ -7,18 +7,17 @@ const AddIcon = ({ size, title, className, ariaHidden }: IconDefaultProps) => (
     width={size}
     height={size}
     className={className}
-    fill="currentcolor"
-    viewBox="0 0 18 19"
-    fillRule="evenodd"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
     strokeLinejoin="round"
-    strokeMiterlimit="2"
     aria-hidden={ariaHidden}
   >
     <title>{title}</title>
-    <path
-      d="M0 10.908v-3.6h7.092V0h3.744v7.308h7.128v3.6h-7.128v7.308H7.092v-7.308H0z"
-      fillRule="nonzero"
-    />
+    <path d="M5 12h14" />
+    <path d="M12 5v14" />
   </svg>
 );
 
