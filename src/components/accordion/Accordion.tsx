f6d31c88d696f0a8135b9e5ac241f64a6c14a608
jardenna <hbjornum@gmail.com>
2025-07-14 14:31:00 +0200

    moved items

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,61 +1,28 @@
-import useLanguage from '../../features/language/useLanguage';
+import { ReactNode } from 'react';
 import './_accordion.scss';
 import AccordionItem from './AccordionItem';
 
+type AccordionItem = {
+  content: ReactNode;
+  title: string;
+};
+
 type AccordionProps = {
-  description: string;
-  material: string;
+  accordionItems: AccordionItem[];
 };
 
-const Accordion = ({ description, material }: AccordionProps) => {
-  const { language } = useLanguage();
-
-  const accordionItems = [
-    { title: language.description, content: <p>{description}</p> },
-    {
-      title: language.materialAndCare,
-      content: (
-        <>
-          <p>
-            <strong>{language.material}:</strong> {material}
-          </p>
-          <strong>{language.care}</strong>
-          <ul className="product-care-info">
-            <li>{language.doNotBleach}</li>
-            <li>{language.noTumbleDry}</li>
-            <li>{language.machineWash}</li>
-          </ul>
-        </>
-      ),
-    },
-    {
-      title: language.paymentAndDelivery,
-      content: (
-        <>
-          <p>
-            <strong>{language.delivery}:</strong> {language.deliveryText}
-          </p>
-          <p>
-            <strong>{language.payment}:</strong> {language.paymentText}
-          </p>
-        </>
-      ),
-    },
-  ];
-
-  return (
-    <section className="accordion-container">
-      {accordionItems.map((item) => (
-        <AccordionItem
-          key={item.title}
-          title={item.title}
-          detailsName="product-info"
-        >
-          {item.content}
-        </AccordionItem>
-      ))}
-    </section>
-  );
-};
+const Accordion = ({ accordionItems }: AccordionProps) => (
+  <section className="accordion-container">
+    {accordionItems.map((item) => (
+      <AccordionItem
+        key={item.title}
+        title={item.title}
+        detailsName="product-info"
+      >
+        {item.content}
+      </AccordionItem>
+    ))}
+  </section>
+);
 
 export default Accordion;
--- src/pages/SingleProductPage.tsx
+++ src/pages/SingleProductPage.tsx
@@ -5,6 +5,7 @@ import { breadcrumbsList } from '../components/breadcrumbs/breadcrumbsLists';
 import Img from '../components/Img';
 import ProductColorList from '../components/ProductColorList';
 import ProductSizeList from '../components/productSizeList/ProductSizeList';
+import useLanguage from '../features/language/useLanguage';
 import ProductDiscountPrice from '../features/products/components/ProductDiscountPrice';
 import useSubMenu from '../features/shop/hooks/useSubMenu';
 import { useGetSingleProductQuery } from '../features/shop/shopApiSlice';
@@ -15,6 +16,40 @@ const SingleProductPage = () => {
   const { id, category } = useParams();
   const { data: product } = useGetSingleProductQuery(id ?? '');
   const { subMenu } = useSubMenu({ category });
+  const { language } = useLanguage();
+
+  const accordionItems = [
+    { title: language.description, content: <p>{product?.description}</p> },
+    {
+      title: language.materialAndCare,
+      content: (
+        <>
+          <p>
+            <strong>{language.material}:</strong> {product?.material}
+          </p>
+          <strong>{language.care}</strong>
+          <ul className="product-care-info">
+            <li>{language.doNotBleach}</li>
+            <li>{language.noTumbleDry}</li>
+            <li>{language.machineWash}</li>
+          </ul>
+        </>
+      ),
+    },
+    {
+      title: language.paymentAndDelivery,
+      content: (
+        <>
+          <p>
+            <strong>{language.delivery}:</strong> {language.deliveryText}
+          </p>
+          <p>
+            <strong>{language.payment}:</strong> {language.paymentText}
+          </p>
+        </>
+      ),
+    },
+  ];
 
   return (
     <div className="container">
@@ -42,10 +77,7 @@ const SingleProductPage = () => {
             />
             <ProductColorList colours={product.colors} />
             <ProductSizeList sizes={product.sizes} variant="shop-product" />
-            <Accordion
-              description={product.description}
-              material={product.material}
-            />
+            <Accordion accordionItems={accordionItems} />
           </section>
         )}
       </article>
