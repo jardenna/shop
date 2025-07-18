ee7792e9dab8ae8150971bfe51f26f3dd03fc127
jardenna <hbjornum@gmail.com>
2025-07-18 14:15:27 +0200

    update size

--- src/components/sizeSelector/SizeSelector.tsx
+++ src/components/sizeSelector/SizeSelector.tsx
@@ -7,37 +7,33 @@ export default function SizeSelector() {
   const [selectedSize, setSelectedSize] = useState<string | null>(null);
 
   return (
-    <fieldset className="size-selector">
-      <legend>Vælg størrelse</legend>
-      <ul className="size-options">
-        {sizes.map((size) => {
-          const id = `size-${size}`;
-          return (
-            <li key={size} className="size-option-wrapper">
-              <label
-                htmlFor={id}
-                className={`size-label ${
-                  selectedSize === size ? 'selected' : ''
-                }`}
-              >
-                <input
-                  type="radio"
-                  name="size"
-                  id={id}
-                  value={size}
-                  checked={selectedSize === size}
-                  onChange={() => {
-                    setSelectedSize(size);
-                  }}
-                />
+    <ul className="size-options">
+      {sizes.map((size) => {
+        const id = `size-${size}`;
+        return (
+          <li key={size} className="size-option-wrapper">
+            <label
+              htmlFor={id}
+              className={`size-label ${
+                selectedSize === size ? 'selected' : ''
+              }`}
+            >
+              <input
+                type="radio"
+                name="size"
+                id={id}
+                value={size}
+                checked={selectedSize === size}
+                onChange={() => {
+                  setSelectedSize(size);
+                }}
+              />
 
-                {size}
-              </label>
-            </li>
-          );
-        })}
-      </ul>
-      {selectedSize && <p>Valgt: {selectedSize}</p>}
-    </fieldset>
+              {size}
+            </label>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
