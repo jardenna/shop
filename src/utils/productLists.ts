99cf5a09a879a4fd426c6d6eb29b8fe6b78de212
jardenna <hbjornum@gmail.com>
2025-07-28 11:34:57 +0200

    productList

--- src/utils/productLists.ts
+++ src/utils/productLists.ts
@@ -2,36 +2,42 @@ import { Roles } from '../app/api/apiTypes/adminApiTypes';
 import { ProductSizes } from '../app/api/apiTypes/sharedApiTypes';
 
 const sizeList: ProductSizes[] = ['S', 'M', 'L', 'XL', 'Onesize'];
-// const sizeListClothing: string[] = ['S', 'M', 'L', 'XL'];
-// const sizeListShoes: string[] = [
-//   '36',
-//   '37',
-//   '38',
-//   '39',
-//   '40',
-//   '41',
-//   '42',
-//   '43',
-//   '44',
-//   '45',
-//   '46',
-//   'Onesize',
-// ];
+const sizeListClothing: string[] = ['S', 'M', 'L', 'XL'];
+const sizeListWomenShoes: string[] = [
+  '36',
+  '37',
+  '38',
+  '39',
+  '40',
+  '41',
+  '42',
+  'Onesize',
+];
+const sizeListMenShoes: string[] = [
+  '40',
+  '41',
+  '42',
+  '43',
+  '44',
+  '45',
+  '46',
+  'Onesize',
+];
 
-// const sizeListShoesKids: string[] = [
-//   '24',
-//   '25',
-//   '26',
-//   '27',
-//   '28',
-//   '29',
-//   '30',
-//   '31',
-//   '32',
-//   '33',
-//   '34',
-//   'Onesize',
-// ];
+const sizeListKidsShoes: string[] = [
+  '24',
+  '25',
+  '26',
+  '27',
+  '28',
+  '29',
+  '30',
+  '31',
+  '32',
+  '33',
+  '34',
+  'Onesize',
+];
 
 const roles: Roles[] = ['Employee', 'User'];
 
@@ -40,4 +46,12 @@ const roleList = roles.map((role) => ({
   label: role.toLowerCase(),
 }));
 
-export { roleList, roles, sizeList };
+export {
+  roleList,
+  roles,
+  sizeList,
+  sizeListClothing,
+  sizeListKidsShoes,
+  sizeListMenShoes,
+  sizeListWomenShoes,
+};
