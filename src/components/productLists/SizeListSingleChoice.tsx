19ae7f5ec9a5cffc33fd3427814ba9d6dc648bf5
jardenna <hbjornum@gmail.com>
2025-07-29 10:25:07 +0200

    modal for single size

--- src/components/productLists/SizeListSingleChoice.tsx
+++ src/components/productLists/SizeListSingleChoice.tsx
@@ -3,10 +3,13 @@ import {
   Size,
   SubCategoryNames,
 } from '../../app/api/apiTypes/sharedApiTypes';
+import useLanguage from '../../features/language/useLanguage';
+import { BtnVariant, SizeVariant } from '../../types/enums';
 import { ProductListChoiceProps } from '../../types/types';
 import { getDisplaySizes } from '../../utils/sizeUtils';
+import { PrimaryActionBtnProps } from '../modal/Modal';
+import ModalContainer from '../modal/ModalContainer';
 import ProductList from './ProductList';
-import ProductListItem from './ProductListItem';
 
 type SizeListSingleChoiceProps = ProductListChoiceProps & {
   availableSizeList: Size[];
@@ -24,35 +27,56 @@ const SizeListSingleChoice = ({
   categoryName,
   subCategoryName,
 }: SizeListSingleChoiceProps) => {
+  const { language } = useLanguage();
   const displaySizeList = getDisplaySizes({
     mainKey: categoryName,
     subKey: subCategoryName,
     availableSizes: availableSizeList,
   });
-
+  const primaryActionBtn: PrimaryActionBtnProps = {
+    onClick: () => {
+      console.log(12);
+    },
+    label: language.delete,
+    variant: BtnVariant.Primary,
+  };
   return (
-    <ProductList groupTitle={groupTitle}>
-      {displaySizeList.map((size) => (
-        <ProductListItem
-          key={size}
-          htmlFor={size}
-          text={size}
-          isUnavailable={!availableSizeList.includes(size)}
-        >
-          <input
-            type="radio"
-            name={name}
-            id={size}
-            value={size}
-            checked={initialChecked === size}
-            onChange={onChange}
-          />
-          {!availableSizeList.includes(size) && initialChecked === size && (
-            <div>Notify me</div>
-          )}
-        </ProductListItem>
-      ))}
-    </ProductList>
+    <>
+      <div> Valgt størrelse: XXL Udsolgt</div>
+      <ProductList groupTitle={groupTitle}>
+        {displaySizeList.map((size) => (
+          <li key={size} className="product-list-item size-list-item">
+            {availableSizeList.includes(size) ? (
+              <>
+                <label htmlFor={size} className="product-label">
+                  {size}
+                </label>
+                <input
+                  type="radio"
+                  name={name}
+                  id={size}
+                  value={size}
+                  checked={initialChecked === size}
+                  onChange={onChange}
+                />
+              </>
+            ) : (
+              <ModalContainer
+                triggerModalBtnContent={size}
+                triggerModalBtnVariant={BtnVariant.Ghost}
+                triggerModalClassName="product-label text-line-through"
+                id={size}
+                primaryActionBtn={primaryActionBtn}
+                modalSize={SizeVariant.Sm}
+                modalHeaderText=" Størrelse XXL er udsolgt"
+              >
+                Vil du have besked, hvis den kommer på lager igen?
+              </ModalContainer>
+            )}
+          </li>
+        ))}
+      </ProductList>
+    </>
   );
 };
 
