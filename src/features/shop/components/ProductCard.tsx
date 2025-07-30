c170e87d51d12e177ae60eb162632b365fccf1f5
jardenna <hbjornum@gmail.com>
2025-07-30 14:35:21 +0200

    fix link

--- src/features/shop/components/ProductCard.tsx
+++ src/features/shop/components/ProductCard.tsx
@@ -1,25 +1,30 @@
 import { Link } from 'react-router';
+import { BaseProduct } from '../../../app/api/apiTypes/sharedApiTypes';
 import Favorites from '../../../components/favorites/Favorites';
 import Img from '../../../components/Img';
 import './ProductCard.styles.scss';
 import ProductCardGridContent from './ProductCardGridContent';
 import ProductCardListContent from './ProductCardListContent';
 import SizeOverlay from './SizeOverlay';
-import { BaseProduct } from '../../../app/api/apiTypes/sharedApiTypes';
 
 type ProductCardProps = {
   displayList: boolean;
   product: BaseProduct;
+  categoryId?: string;
 };
 
-const ProductCard = ({ displayList, product }: ProductCardProps) => (
+const ProductCard = ({
+  displayList,
+  product,
+  categoryId,
+}: ProductCardProps) => (
   <section className="product-card">
     <div className="product-img-container">
       <Favorites id={product.id} />
       {product.discount > 0 && (
         <span className="product-badge">- {product.discount} %</span>
       )}
-      <Link to={product.id}>
+      <Link to={categoryId ? product.id : `allProducts/${product.id}`}>
         <Img alt="" src={product.images[0]} className="product-card-img" />
       </Link>
       {!displayList && <SizeOverlay sizes={product.sizes} />}
