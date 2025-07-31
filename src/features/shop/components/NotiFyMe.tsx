ae533739562eda7f6c477c34326fee18d40fcf71
jardenna <hbjornum@gmail.com>
2025-07-31 13:25:14 +0200

    moved modal

--- src/features/shop/components/NotiFyMe.tsx
+++ src/features/shop/components/NotiFyMe.tsx
@@ -1,14 +1,71 @@
+import { BaseProduct, Size } from '../../../app/api/apiTypes/sharedApiTypes';
+import { PrimaryActionBtnProps } from '../../../components/modal/Modal';
+import ModalContainer from '../../../components/modal/ModalContainer';
+import SizeListMultiChoice from '../../../components/productLists/SizeListMultiChoice';
+import useFormValidation from '../../../hooks/useFormValidation';
+import { BtnVariant, SizeVariant } from '../../../types/enums';
+import { checkBoxSizeList } from '../../../utils/sizeUtils';
 import useLanguage from '../../language/useLanguage';
 
-const NotiFyMe = () => {
+type NotifiMeProps = {
+  displaySizeList: Size[];
+  selectedProduct: BaseProduct;
+};
+const NotiFyMe = ({ selectedProduct, displaySizeList }: NotifiMeProps) => {
   const { language } = useLanguage();
-  return (
-    <section>
-      <p>{language.getNotifiedForProducts}</p>
-      <div>size multiChoise list</div>
 
-      <div>email form</div>
-    </section>
+  const missingSizes = displaySizeList.filter(
+    (size) => !selectedProduct.sizes.includes(size),
+  );
+
+  const initialState = {
+    sizdes: [],
+    email: '',
+  };
+
+  const { onChange, values } = useFormValidation({
+    initialState,
+    callback: () => {
+      console.log(values);
+    },
+  });
+
+  const availableSizeList = checkBoxSizeList(missingSizes);
+  const primaryActionBtn: PrimaryActionBtnProps = {
+    onClick: () => {
+      console.log(12);
+    },
+    label: language.notiftyMe,
+    variant: BtnVariant.Primary,
+  };
+
+  return (
+    <div>
+      <ModalContainer
+        triggerModalBtnContent="Notify me when missing sizes are back in stock"
+        triggerModalBtnVariant={BtnVariant.Ghost}
+        id="hh"
+        primaryActionBtn={primaryActionBtn}
+        modalSize={SizeVariant.Sm}
+        modalHeaderText={`${language.size}   ${language.currentlyUnavailable}`}
+      >
+        <SizeListMultiChoice
+          onChange={onChange}
+          values={values.sizdes}
+          availableSizeList={availableSizeList}
+          name="sizes"
+          required
+          groupTitle={{
+            title: language.sizes,
+            id: 'choose-product-colors',
+          }}
+        />
+        <p>{language.getNotifiedForProducts}</p>
+        <div>size multiChoise list</div>
+
+        <div>email form</div>
+      </ModalContainer>
+    </div>
   );
 };
 
--- src/features/shop/components/ShopProductForm.tsx
+++ src/features/shop/components/ShopProductForm.tsx
@@ -1,25 +1,22 @@
-import { BaseProduct } from '../../../app/api/apiTypes/sharedApiTypes';
+import { BaseProduct, Size } from '../../../app/api/apiTypes/sharedApiTypes';
 import FieldSet from '../../../components/fieldset/FieldSet';
 import Form from '../../../components/form/Form';
-import { PrimaryActionBtnProps } from '../../../components/modal/Modal';
-import ModalContainer from '../../../components/modal/ModalContainer';
 import ColorListSingleChoice from '../../../components/productColorLists/ColorListSingleChoice';
 import SizeListSingleChoice from '../../../components/productLists/SizeListSingleChoice';
 import useFormValidation from '../../../hooks/useFormValidation';
-import { BtnVariant, SizeVariant } from '../../../types/enums';
 import { ColorOption } from '../../../utils/colorUtils';
-import { getDisplaySizes } from '../../../utils/sizeUtils';
 import useLanguage from '../../language/useLanguage';
-import NotiFyMe from './NotiFyMe';
 
 type ShopProductFormProps = {
   colorList: ColorOption[];
+  displaySizeList: Size[];
   selectedProduct: BaseProduct;
 };
 
 const ShopProductForm = ({
   selectedProduct,
   colorList,
+  displaySizeList,
 }: ShopProductFormProps) => {
   const { language } = useLanguage();
 
@@ -39,24 +36,6 @@ const ShopProductForm = ({
     values.size === ''
       ? language.selectSize
       : `${language.selectedSize}: ${values.size}`;
-
-  const primaryActionBtn: PrimaryActionBtnProps = {
-    onClick: () => {
-      console.log(12);
-    },
-    label: language.notiftyMe,
-    variant: BtnVariant.Primary,
-  };
-
-  const displaySizeList = getDisplaySizes({
-    mainKey: selectedProduct.categoryName,
-    subKey: selectedProduct.subCategoryName,
-    availableSizes: selectedProduct.sizes,
-  });
-
-  const missingSizes = displaySizeList.filter(
-    (size) => !selectedProduct.sizes.includes(size),
-  );
 
   return (
     <Form onSubmit={onSubmit} submitBtnLabel={language.create}>
@@ -84,18 +63,6 @@ const ShopProductForm = ({
           }}
         />
       </FieldSet>
-      <div>
-        <ModalContainer
-          triggerModalBtnContent="Notify me when missing sizes are back in stock"
-          triggerModalBtnVariant={BtnVariant.Ghost}
-          id={selectedProduct.productName}
-          primaryActionBtn={primaryActionBtn}
-          modalSize={SizeVariant.Sm}
-          modalHeaderText={`${language.size}   ${language.currentlyUnavailable}`}
-        >
-          <NotiFyMe sizeList={missingSizes} />
-        </ModalContainer>
-      </div>
     </Form>
   );
 };
