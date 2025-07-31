9bdac3da7245c753e13cb2f224d520ecf364b6da
jardenna <hbjornum@gmail.com>
2025-07-31 17:33:10 +0200

    used checkbox list

--- src/features/shop/components/NotiFyMe.tsx
+++ src/features/shop/components/NotiFyMe.tsx
@@ -1,36 +1,23 @@
-import { BaseProduct, Size } from '../../../app/api/apiTypes/sharedApiTypes';
 import { PrimaryActionBtnProps } from '../../../components/modal/Modal';
 import ModalContainer from '../../../components/modal/ModalContainer';
-import SizeListMultiChoice from '../../../components/productLists/SizeListMultiChoice';
-import useFormValidation from '../../../hooks/useFormValidation';
 import { BtnVariant, SizeVariant } from '../../../types/enums';
-import { checkBoxSizeList } from '../../../utils/sizeUtils';
 import useLanguage from '../../language/useLanguage';
 
 type NotifiMeProps = {
-  displaySizeList: Size[];
-  selectedProduct: BaseProduct;
+  name?: string;
 };
-const NotiFyMe = ({ selectedProduct, displaySizeList }: NotifiMeProps) => {
+const NotiFyMe = ({ name }: NotifiMeProps) => {
   const { language } = useLanguage();
 
-  const missingSizes = displaySizeList.filter(
-    (size) => !selectedProduct.sizes.includes(size),
-  );
+  // const missingSizes = displaySizeList.filter(
+  //   (size) => !selectedProduct.sizes.includes(size),
+  // );
 
-  const initialState = {
-    sizes: [],
-    email: '',
-  };
+  // const initialState = {
+  //   sizes: [],
+  //   email: '',
+  // };
 
-  const { onChange, values } = useFormValidation({
-    initialState,
-    callback: () => {
-      console.log(values);
-    },
-  });
-
-  const availableSizeList = checkBoxSizeList(missingSizes);
   const primaryActionBtn: PrimaryActionBtnProps = {
     onClick: () => {
       console.log(12);
@@ -49,20 +36,9 @@ const NotiFyMe = ({ selectedProduct, displaySizeList }: NotifiMeProps) => {
         modalSize={SizeVariant.Sm}
         modalHeaderText={`${language.size}   ${language.currentlyUnavailable}`}
       >
-        <SizeListMultiChoice
-          onChange={onChange}
-          values={values.sizes}
-          availableSizeList={availableSizeList}
-          name="sizes"
-          required
-          groupTitle={{
-            title: language.sizes,
-            id: 'choose-product-colors',
-          }}
-        />
         <p>{language.getNotifiedForProducts}</p>
         <div>size multiChoise list</div>
-
+        {name && name}
         <div>email form</div>
       </ModalContainer>
     </div>
