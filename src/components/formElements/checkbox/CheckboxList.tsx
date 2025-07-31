d759d9aa1ee563bec0b9ea103bf783098a336738
jardenna <hbjornum@gmail.com>
2025-07-31 17:21:34 +0200

    checkboxlist

--- src/components/formElements/checkbox/CheckboxList.tsx
+++ src/components/formElements/checkbox/CheckboxList.tsx
@@ -1,10 +1,16 @@
-import { ChangeInputType } from '../../../types/types';
+import { ChangeInputType, OptionGroupHeading } from '../../../types/types';
+import OptionGroupTitle from '../../productLists/OptionGroupTitle';
+import InputInfo from '../InputInfo';
 import './_checkbox-list.scss';
 
 type CheckboxListProps = {
   name: string;
   options: string[];
   values: string[];
+  disabled?: boolean;
+  groupTitle?: OptionGroupHeading;
+  inputInfo?: string;
+  required?: boolean;
   onChange: (event: ChangeInputType) => void;
 };
 
@@ -13,25 +19,36 @@ const CheckboxList = ({
   values,
   onChange,
   name,
+  disabled,
+  groupTitle,
+  required,
+  inputInfo,
 }: CheckboxListProps) => (
-  <ul className="checkbox-list">
-    {options.map((label, index) => {
-      const id = `${index}`;
-      return (
-        <li key={label} className="checkbox-item">
-          <label htmlFor={id}>{label}</label>
-          <input
-            type="checkbox"
-            id={id}
-            name={name}
-            value={label}
-            checked={values.includes(label)}
-            onChange={onChange}
-          />
-        </li>
-      );
-    })}
-  </ul>
+  <section>
+    {groupTitle && (
+      <OptionGroupTitle groupTitle={groupTitle} required={required} />
+    )}
+    <ul className="checkbox-list">
+      {options.map((label, index) => {
+        const id = `${index}`;
+        return (
+          <li key={label} className="checkbox-item">
+            <label htmlFor={id}>{label}</label>
+            <input
+              type="checkbox"
+              id={id}
+              name={name}
+              value={label}
+              checked={values.includes(label)}
+              onChange={onChange}
+              disabled={disabled}
+            />
+          </li>
+        );
+      })}
+    </ul>
+    {inputInfo && <InputInfo inputInfo={inputInfo} />}
+  </section>
 );
 
 export default CheckboxList;
