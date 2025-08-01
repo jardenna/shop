388ee4ff23ae6959c33a934de088a99bd9b59036
jardenna <hbjornum@gmail.com>
2025-08-01 08:59:44 +0200

    redid profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,40 +1,12 @@
-import ModalContainer from '../components/modal/ModalContainer';
 import useLanguage from '../features/language/useLanguage';
-import { BtnVariant, SizeVariant } from '../types/enums';
 import MainPageContainer from './pageContainer/MainPageContainer';
 
 const ProfilePage = () => {
   const { language } = useLanguage();
 
-  const primaryActionBtn = {
-    onClick: () => {
-      console.log(12);
-    },
-    label: 'Test',
-  };
-
-  // const primaryActionBtn = {
-  //   onclick: () => {
-  //     console.log(12);
-  //   },
-  //   label: 'Test',
-  // };
-
   return (
     <MainPageContainer heading={language.profile}>
-      <section>
-        {language.profile}
-        <ModalContainer
-          triggerModalBtnContent="Notify me when missing sizes are back in stock"
-          triggerModalBtnVariant={BtnVariant.Ghost}
-          id="sizes"
-          primaryActionBtn={primaryActionBtn}
-          modalSize={SizeVariant.Sm}
-          modalHeaderText={`${language.size}  ${language.currentlyUnavailable}`}
-        >
-          Helloo
-        </ModalContainer>
-      </section>
+      <section>{language.profile}</section>
     </MainPageContainer>
   );
 };
