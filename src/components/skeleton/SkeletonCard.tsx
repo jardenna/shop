d9b1e945797b70c0eb2a4fd2d78945ed84cb14ad
jardenna <hbjornum@gmail.com>
2025-08-24 10:08:02 +0200

    card

--- src/components/skeleton/SkeletonCard.tsx
+++ src/components/skeleton/SkeletonCard.tsx
@@ -1,6 +1,15 @@
+import SkeletonBadge from './SkeletonBadge';
+import SkeletonList from './SkeletonList';
+import SkeletonParagraph from './SkeletonParagraph';
+
 const SkeletonCard = () => (
-  <div className="page-card flex-1" style={{ maxWidth: '25rem' }}>
-    <span className="skeleton" style={{ height: '100%' }} />
+  <div className="flex column justify-space-between" style={{ height: '100%' }}>
+    <div className="flex">
+      <SkeletonParagraph height="1.5" count={1} />
+      <SkeletonBadge />
+    </div>
+    <SkeletonParagraph />
+    <SkeletonList />
   </div>
 );
 
