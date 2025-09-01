bab7b0f0c0b2f6e71bf992ea6f0741f49cb1654e
jardenna <hbjornum@gmail.com>
2025-09-01 08:40:14 +0200

    review svg

--- src/features/products/components/reviews/ReviewsDisplay.tsx
+++ src/features/products/components/reviews/ReviewsDisplay.tsx
@@ -1,4 +1,4 @@
-import { DisplyReviews } from '../../../../app/api/apiTypes/shopApiTypes';
+import type { DisplyReviews } from '../../../../app/api/apiTypes/shopApiTypes';
 import Icon from '../../../../components/icons/Icon';
 import { IconName } from '../../../../types/enums';
 import './_reviews.scss';
@@ -29,57 +29,14 @@ const ReviewsDisplay = ({
         <div className="stars">
           {stars.map((type, index) => (
             <span key={index} className="star" aria-hidden="true">
-              {type === 'full' && (
-                <Icon iconName={IconName.Star} title={type} />
-              )}
+              {type === 'full' && <Icon iconName={IconName.Star} title="" />}
               {type === 'half' && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20px"
-                  height="20px"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  {/* Left half */}
-                  <path
-                    d="M12 18.338a2.1 2.1 0 0 0-.987.244L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.12 2.12 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.12 2.12 0 0 0 1.597-1.16l2.309-4.679A.53.53 0 0 1 12 2"
-                    fill="currentColor"
-                  />
-                  {/* Right half (mirrored horizontally) */}
-                  <path
-                    d="M12 18.338a2.1 2.1 0 0 0-.987.244L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.12 2.12 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.12 2.12 0 0 0 1.597-1.16l2.309-4.679A.53.53 0 0 1 12 2"
-                    fill="currentColor"
-                    transform="scale(-1,1) translate(-24,0)"
-                  />
-                </svg>
+                <Icon iconName={IconName.HalfStar} title="" />
               )}
-              {type === 'empty' && (
-                <Icon iconName={IconName.Star} title={type} />
-              )}
+              {type === 'empty' && <Icon iconName={IconName.Star} title="" />}
             </span>
           ))}
         </div>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-            aria-hidden="true"
-          >
-            {/* Left half */}
-            <path
-              d="M12 18.338a2.1 2.1 0 0 0-.987.244L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.12 2.12 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.12 2.12 0 0 0 1.597-1.16l2.309-4.679A.53.53 0 0 1 12 2"
-              fill="currentColor"
-            />
-            {/* Right half (mirrored horizontally) */}
-            <path
-              d="M12 18.338a2.1 2.1 0 0 0-.987.244L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.12 2.12 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.12 2.12 0 0 0 1.597-1.16l2.309-4.679A.53.53 0 0 1 12 2"
-              fill="currentColor"
-              transform="scale(-1,1) translate(-24,0)"
-            />
-          </svg>
-        </span>
 
         <span>{rating}</span>
         <span>{numOfReviews}</span>
