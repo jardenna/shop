a8dd29ab4599fdfdd48bc9b9c751a5701c4c8579
jardenna <hbjornum@gmail.com>
2025-09-05 13:09:13 +0200

    ShopProductsParams

--- src/app/api/apiTypes/shopApiTypes.ts
+++ src/app/api/apiTypes/shopApiTypes.ts
@@ -6,9 +6,12 @@ import type {
 } from './sharedApiTypes';
 
 export type ShopProductsParams = BaseProductParams & {
+  brand?: string;
+  colors?: string;
   mainCategory?: string;
   maxPrice?: string;
   minPrice?: string;
+  sizes?: string;
   subCategoryId?: string;
 };
 
