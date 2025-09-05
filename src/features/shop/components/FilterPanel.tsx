cd6ab36ac0d51d8a7c873d3704585e11aceea8b5
jardenna <hbjornum@gmail.com>
2025-09-05 14:17:16 +0200

    GetColorOptionsParams paramoptional

--- src/features/shop/components/FilterPanel.tsx
+++ src/features/shop/components/FilterPanel.tsx
@@ -1,10 +1,22 @@
+import { Size } from '../../../app/api/apiTypes/sharedApiTypes';
 import Icon from '../../../components/icons/Icon';
 import TogglePanel from '../../../components/togglePanel/TogglePanel';
+import variables from '../../../scss/variables.module.scss';
 import { IconName } from '../../../types/enums';
+import { getColorOptions } from '../../../utils/colorUtils';
 import useLanguage from '../../language/useLanguage';
 
-const FilterPanel = () => {
+type FilterPanelProps = {
+  availableBrands: string[];
+  availableSizes: Size[];
+};
+const FilterPanel = ({ availableBrands, availableSizes }: FilterPanelProps) => {
   const { language } = useLanguage();
+  const sortedColorList = getColorOptions({
+    language,
+    borderColor: variables.colorIconBorder,
+  });
+  console.log(sortedColorList, availableBrands, availableSizes);
 
   return (
     <TogglePanel
@@ -18,7 +30,7 @@ const FilterPanel = () => {
         </>
       }
     >
-      Filter
+      Filterff
     </TogglePanel>
   );
 };
