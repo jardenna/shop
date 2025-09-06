2710e29edefb367ecfc67d81635fed3e5ccb0192
jardenna <hbjornum@gmail.com>
2025-09-06 09:39:29 +0200

    nuberstep

---
+++ src/components/numberStep/NumberStep.tsx
@@ -0,0 +1,71 @@
+import { FC } from 'react';
+import { IconName } from '../../types/enums';
+import Button from '../Button';
+import Input from '../formElements/Input';
+import Icon from '../icons/Icon';
+import './_number-step.scss';
+
+interface NumberStepProps {
+  id: string;
+  labelText: string;
+  name: string;
+  value: number;
+  initCount?: number;
+  max?: number;
+  min?: number;
+  onChange: (event?: any) => void;
+  onNumberStepChange: (event: any, count: number) => void;
+}
+
+const NumberStep: FC<NumberStepProps> = ({
+  onChange,
+  // onNumberStepChange,
+  value,
+  initCount = 1,
+  min = 0,
+  max,
+  labelText,
+  id,
+  name,
+}) => (
+  <article className="number-step">
+    <label htmlFor={id}>{labelText}</label>
+    <div className="number-step-container">
+      <Button
+        id="add"
+        name={id}
+        ariaLabel={`Subtract ${initCount} `}
+        disabled={value === min}
+        // onClick={(event?: ButtonEventType) => {
+        //   onNumberStepChange(event, value !== Number(min) ? -initCount : 0);
+        // }}
+      >
+        <Icon iconName={IconName.Subtract} title="Subtract" />
+      </Button>
+      <Input
+        type="number"
+        value={value}
+        onChange={onChange}
+        id={id}
+        labelText={labelText}
+        inputHasNoLabel
+        name={name}
+        min={min}
+        max={max}
+      />
+      <Button
+        // onClick={(event?: ButtonEventType) => {
+        //   onNumberStepChange(event, value !== Number(max) ? initCount : 0);
+        // }}
+        ariaLabel={`Add ${initCount} `}
+        disabled={value === Number(max)}
+        id="subtract"
+        name={id}
+      >
+        <Icon iconName={IconName.Add} title="Add" />
+      </Button>
+    </div>
+  </article>
+);
+
+export default NumberStep;
