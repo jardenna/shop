2cc950a4093b6713ed450fec8b61a03700c189ef
jardenna <hbjornum@gmail.com>
2025-09-08 13:35:09 +0200

    fix lighthouse problem

--- src/features/shop/components/ProductCard.tsx
+++ src/features/shop/components/ProductCard.tsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router';
 import type { ProductPreview } from '../../../app/api/apiTypes/shopApiTypes';
 import FavoriteHeart from '../../../components/favorites/FavoriteHeart';
 import Img from '../../../components/Img';
+import VisuallyHidden from '../../../components/VisuallyHidden';
+import useLanguage from '../../language/useLanguage';
 import './ProductCard.styles.scss';
 import SizeOverlay from './SizeOverlay';
 
@@ -18,23 +20,29 @@ const ProductCard = ({
   product,
   linkTo,
   children,
-}: ProductCardProps) => (
-  <section className="product-card">
-    <div className="product-img-container">
-      <FavoriteHeart id={product.id} />
-      {product.discount > 0 && (
-        <span className="product-badge">- {product.discount} %</span>
-      )}
-      <Link to={linkTo}>
-        <Img alt="" src={product.images[0]} className="product-card-img" />
-      </Link>
-      {showSizeOverlay && <SizeOverlay sizes={product.sizes} count={5} />}
-    </div>
-    <div className="product-card-content">
-      <h2 className="product-card-title">{product.productName}</h2>
-      {children}
-    </div>
-  </section>
-);
+}: ProductCardProps) => {
+  const { language } = useLanguage();
+  return (
+    <section className="product-card">
+      <div className="product-img-container">
+        <FavoriteHeart id={product.id} />
+        {product.discount > 0 && (
+          <span className="product-badge">- {product.discount} %</span>
+        )}
+        <Link to={linkTo}>
+          <VisuallyHidden>
+            {language.view} {product.productName}
+          </VisuallyHidden>
+          <Img alt="" src={product.images[0]} className="product-card-img" />
+        </Link>
+        {showSizeOverlay && <SizeOverlay sizes={product.sizes} count={5} />}
+      </div>
+      <div className="product-card-content">
+        <h2 className="product-card-title">{product.productName}</h2>
+        {children}
+      </div>
+    </section>
+  );
+};
 
 export default ProductCard;
