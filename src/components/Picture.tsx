ca018223878b7e66bb6b3600e78ba6765b70775f
jardenna <hbjornum@gmail.com>
2025-09-19 16:46:46 +0200

    alt

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -5,11 +5,10 @@ type PictureProps = ImgProps & {
   srcSet: `${string}.avif`;
 };
 
-const Picture = ({ alt, srcSet, src, className }: PictureProps) => (
+const Picture = ({ alt = '', srcSet, src, className }: PictureProps) => (
   <picture className={className}>
     <source srcSet={srcSet} type="image/avif" />
     <Img className="img-fallback" src={src} alt={alt} />
   </picture>
 );
-
 export default Picture;
