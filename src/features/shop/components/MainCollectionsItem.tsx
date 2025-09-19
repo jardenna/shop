609ef530e6f13667104b48a46fd50158d7805d5e
jardenna <hbjornum@gmail.com>
2025-09-19 17:06:51 +0200

    type

--- src/features/shop/components/MainCollectionsItem.tsx
+++ src/features/shop/components/MainCollectionsItem.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router';
 import Picture from '../../../components/Picture';
 import { ShopPath } from '../../../layout/nav/enums';
-import { ImgExtention } from '../../../types/types';
+import type { ImgExtention } from '../../../types/types';
 import useLanguage from '../../language/useLanguage';
 
 export type MainCollectionsBaseProps = {
