a31abd27385544ddd96355745bb6b35a9623417c
jardenna <hbjornum@gmail.com>
2025-09-19 15:55:43 +0200

    moved logic

--- src/features/shop/components/MainCollections.tsx
+++ src/features/shop/components/MainCollections.tsx
@@ -1,50 +1,23 @@
-import { ShopPath } from '../../../layout/nav/enums';
-import { getlowerCaseFirstLetter } from '../../../utils/utils';
-import { useGetPublishedCategoriesQuery } from '../../categories/categoriyApiSlice';
-import useLanguage from '../../language/useLanguage';
 import MainCollectionsItem, {
   MainCollectionsBaseProps,
 } from './MainCollectionsItem';
 
 type MainCollectionsProps = {
-  categoryOrder: string[];
-  collectionImages: Record<string, string[]>;
+  mainCollectionList: MainCollectionsBaseProps[];
 };
-const MainCollections = ({
-  collectionImages,
-  categoryOrder,
-}: MainCollectionsProps) => {
-  const { language } = useLanguage();
-
-  const { data: publishedCategories } = useGetPublishedCategoriesQuery();
-
-  const sortedCategories = (
-    publishedCategories?.length ? [...publishedCategories] : ['Women']
-  ).sort((a, b) => categoryOrder.indexOf(a) - categoryOrder.indexOf(b));
-
-  const mainCollectionsList: MainCollectionsBaseProps[] = sortedCategories.map(
-    (item) => ({
-      title: getlowerCaseFirstLetter(item, language),
-      imgList: collectionImages[item],
-      linkTo: ShopPath[item as keyof typeof ShopPath],
-      linkText: language.discoverCollection,
-    }),
-  );
-
-  return (
-    <div className="main-collection-container">
-      {mainCollectionsList.map(({ title, imgList, linkTo, linkText }) => (
-        <MainCollectionsItem
-          key={title}
-          imgPath="/images/adImages"
-          title={title}
-          imgList={imgList}
-          linkTo={linkTo}
-          linkText={linkText}
-        />
-      ))}
-    </div>
-  );
-};
+const MainCollections = ({ mainCollectionList }: MainCollectionsProps) => (
+  <div className="main-collection-container">
+    {mainCollectionList.map(({ title, imgList, linkTo, linkText }) => (
+      <MainCollectionsItem
+        key={title}
+        imgPath="/images/adImages"
+        title={title}
+        imgList={imgList}
+        linkTo={linkTo}
+        linkText={linkText}
+      />
+    ))}
+  </div>
+);
 
 export default MainCollections;
--- src/pages/MainCollectionsPage.tsx
+++ src/pages/MainCollectionsPage.tsx
@@ -1,11 +1,21 @@
+import { MainCategoryNames } from '../app/api/apiTypes/sharedApiTypes';
+import { useGetPublishedCategoriesQuery } from '../features/categories/categoriyApiSlice';
 import useLanguage from '../features/language/useLanguage';
 import MainCollections from '../features/shop/components/MainCollections';
+import { MainCollectionsBaseProps } from '../features/shop/components/MainCollectionsItem';
+import { ShopPath } from '../layout/nav/enums';
+import { getlowerCaseFirstLetter } from '../utils/utils';
 import './MainCollectionPage.styles.scss';
 import MainPageContainer from './pageContainer/MainPageContainer';
 
+type AdImages = Partial<Record<MainCategoryNames, string[]>> & {
+  [key: string]: string[];
+};
+
 const CollectionLandingPage = () => {
   const { language } = useLanguage();
-  const collectionImages: Record<string, string[]> = {
+
+  const collectionImages: AdImages = {
     Women: ['women', 'women_2', 'women_3'],
     Men: ['men'],
     Kids: ['kids'],
@@ -13,12 +23,24 @@ const CollectionLandingPage = () => {
 
   const categoryOrder = ['Kids', 'Men', 'Women'];
 
+  const { data: publishedCategories } = useGetPublishedCategoriesQuery();
+
+  const sortedCategories = (
+    publishedCategories?.length ? [...publishedCategories] : ['Women']
+  ).sort((a, b) => categoryOrder.indexOf(a) - categoryOrder.indexOf(b));
+
+  const mainCollectionsList: MainCollectionsBaseProps[] = sortedCategories.map(
+    (item) => ({
+      title: getlowerCaseFirstLetter(item, language),
+      imgList: collectionImages[item],
+      linkTo: ShopPath[item as keyof typeof ShopPath],
+      linkText: language.discoverCollection,
+    }),
+  );
+
   return (
     <MainPageContainer heading={language.collection}>
-      <MainCollections
-        collectionImages={collectionImages}
-        categoryOrder={categoryOrder}
-      />
+      <MainCollections mainCollectionList={mainCollectionsList} />
     </MainPageContainer>
   );
 };
