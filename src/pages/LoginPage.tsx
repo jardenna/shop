523958ea91c64400a47d64e0884cfb217a677ab5
jardenna <hbjornum@gmail.com>
2025-09-20 13:50:52 +0200

    loged in

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,12 @@
 import { useLocation, useNavigate } from 'react-router';
 import AuthForm from '../components/authForm/AuthForm';
+import Button from '../components/Button';
 import validateLogin from '../components/formElements/validation/validateLogin';
 import useMessagePopup from '../components/messagePopup/useMessagePopup';
-import { useLoginMutation } from '../features/auth/authApiSlice';
+import {
+  useCheckAuthQuery,
+  useLoginMutation,
+} from '../features/auth/authApiSlice';
 import useLanguage from '../features/language/useLanguage';
 import useFormValidation from '../hooks/useFormValidation';
 import { ShopPath } from '../layout/nav/enums';
@@ -12,13 +16,11 @@ const LoginPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { language } = useLanguage();
-
-  const initialState = {
-    email: '',
-    password: '',
-  };
-
+  const { data: userProfile, isLoading: userIsLoading } = useCheckAuthQuery();
+  const [loginUser, { isLoading }] = useLoginMutation();
   const { onAddMessagePopup } = useMessagePopup();
+
+  const initialState = { email: '', password: '' };
   const from = location.state?.from?.pathname || ShopPath.Root;
 
   const { values, errors, onChange, onBlur, onSubmit } = useFormValidation({
@@ -26,7 +28,6 @@ const LoginPage = () => {
     callback: handleLoginUser,
     validate: validateLogin,
   });
-  const [loginUser, { isLoading }] = useLoginMutation();
 
   async function handleLoginUser() {
     try {
@@ -43,20 +44,50 @@ const LoginPage = () => {
     }
   }
 
+  // Show nothing (or a spinner) while we are checking auth from cookie
+  if (userIsLoading) {
+    return null;
+  }
+
   return (
     <MainPageContainer heading={language.login} className="page-small">
-      <AuthForm
-        values={values}
-        submitBtnLabel={language.login}
-        onSubmit={onSubmit}
-        isLoading={isLoading}
-        legendText={language.userInfo}
-        onChange={onChange}
-        errors={errors}
-        onBlur={onBlur}
-        navigateTo={ShopPath.CreateAccount}
-        navigateToText={language.createAccount}
-      />
+      {userProfile?.user ? (
+        <div>
+          You are already logged in as {userProfile.user.username}
+          <div>
+            <Button
+              onClick={() => {
+                /* logout logic */
+              }}
+            >
+              Log out
+            </Button>
+            <Button
+              onClick={() => {
+                /* switch account logic */
+              }}
+            >
+              Switch account
+            </Button>
+            <Button onClick={() => navigate(ShopPath.CreateAccount)}>
+              {language.createAccount}
+            </Button>
+          </div>
+        </div>
+      ) : (
+        <AuthForm
+          values={values}
+          submitBtnLabel={language.login}
+          onSubmit={onSubmit}
+          isLoading={isLoading}
+          legendText={language.userInfo}
+          onChange={onChange}
+          errors={errors}
+          onBlur={onBlur}
+          navigateTo={ShopPath.CreateAccount}
+          navigateToText={language.createAccount}
+        />
+      )}
     </MainPageContainer>
   );
 };
