64837a0e5ab06137f3e5fee805dd6e0dfe3220a2
jardenna <hbjornum@gmail.com>
2025-09-25 16:34:29 +0200

    account layout

--- src/layout/AccountLayout.tsx
+++ src/layout/AccountLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet, useLocation } from 'react-router';
 import Img from '../components/Img';
 import useLanguage from '../features/language/useLanguage';
-import FullSizePageContainer from '../pages/pageContainer/FullSizePageContainer';
 import { getPathInfo } from '../utils/utils';
+import './accountLayout.styles.scss';
 import LayoutElement from './LayoutElement';
+import MetaTags from './nav/MetaTags';
 
 const AccountLayout = () => {
   const { language } = useLanguage();
@@ -13,26 +14,29 @@ const AccountLayout = () => {
   const title = language[pathInfo];
 
   return (
-    <FullSizePageContainer metaTitle={title}>
-      <section className="account">
-        <div className="test">
-          <LayoutElement as="nav" ariaLabel="brugerkonto">
-            <ul className="nav">
-              <li>Personlige oplysninger</li>
-              <li>Adresser</li>
-              <li>Ordrer</li>
-            </ul>
-          </LayoutElement>
-          <div className="content-test">
-            <h1>{title}</h1>
-            <Outlet />
+    <>
+      <MetaTags metaTitle={title} />
+      <article>
+        <section className="main-page account">
+          <div className="test">
+            <LayoutElement as="nav" ariaLabel="brugerkonto">
+              <ul className="nav">
+                <li>Personlige oplysninger</li>
+                <li>Adresser</li>
+                <li>Ordrer</li>
+              </ul>
+            </LayoutElement>
+            <div className="content-test">
+              <h1>{title}</h1>
+              <Outlet />
+            </div>
           </div>
-        </div>
-        <div className="img-wrapper">
-          <Img src="/images/about/woman.jpg" alt="" />
-        </div>
-      </section>
-    </FullSizePageContainer>
+          <div className="img-wrapper">
+            <Img src="/images/about/woman.jpg" alt="" />
+          </div>
+        </section>
+      </article>
+    </>
   );
 };
 
