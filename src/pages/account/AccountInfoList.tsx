dd3b11d62863417e064223cb713c7f8727d710d5
jardenna <hbjornum@gmail.com>
2025-09-29 18:21:37 +0200

    AccountInfoList

---
+++ src/pages/account/AccountInfoList.tsx
@@ -0,0 +1,50 @@
+import { UserProfileResponse } from '../../app/api/apiTypes/sharedApiTypes';
+import DateDisplay from '../../components/datePicker/DateDisplay';
+import Icon from '../../components/icons/Icon';
+import LabelValueGrid from '../../components/LabelValueGrid';
+import Tooltip from '../../components/tooltip/Tooltip';
+import useLanguage from '../../features/language/useLanguage';
+import { IconName } from '../../types/enums';
+
+type AccountInfoListProps = {
+  profile: UserProfileResponse;
+};
+
+const AccountInfoList = ({ profile }: AccountInfoListProps) => {
+  const { language } = useLanguage();
+  const notProvided = language.notProvided;
+  return (
+    <div>
+      <LabelValueGrid text={language.name}>{profile.username}</LabelValueGrid>
+
+      <LabelValueGrid
+        text={language.phone}
+        tooltip={
+          <Tooltip
+            ariaControls="phone"
+            ariaLabel={language.viewInfo}
+            tooltip={language.phoneInfo}
+          >
+            <Icon iconName={IconName.Info} title="" size="1em" />
+          </Tooltip>
+        }
+      >
+        {profile.phoneNo || notProvided}
+      </LabelValueGrid>
+
+      <LabelValueGrid text={language.fashionPreference}>
+        {language[profile.preferredFashion]}
+      </LabelValueGrid>
+      <LabelValueGrid text={language.dateOfBirth}>
+        {profile.dateOfBirth ? (
+          <DateDisplay date={profile.dateOfBirth} />
+        ) : (
+          notProvided
+        )}
+      </LabelValueGrid>
+      <LabelValueGrid text={language.email}>{profile.email}</LabelValueGrid>
+    </div>
+  );
+};
+
+export default AccountInfoList;
--- src/pages/account/MyAccountPage.tsx
+++ src/pages/account/MyAccountPage.tsx
@@ -1,19 +1,14 @@
-import DateDisplay from '../../components/datePicker/DateDisplay';
-import Icon from '../../components/icons/Icon';
-import LabelValueGrid from '../../components/LabelValueGrid';
 import { PrimaryActionBtnProps } from '../../components/modal/Modal';
 import ModalContainer from '../../components/modal/ModalContainer';
 import SkeletonParagraph from '../../components/skeleton/SkeletonParagraph';
-import Tooltip from '../../components/tooltip/Tooltip';
 import useLanguage from '../../features/language/useLanguage';
 import { useGetUserProfileQuery } from '../../features/profile/profileApiSlice';
-import { IconName } from '../../types/enums';
 import './_my-account.scss';
+import AccountInfoList from './AccountInfoList';
 
 const MyAccountPage = () => {
   const { language } = useLanguage();
   const { data: profile, isLoading } = useGetUserProfileQuery();
-  const notProvided = language.notProvided;
 
   const primaryActionBtn: PrimaryActionBtnProps = {
     label: 'ok',
@@ -26,36 +21,8 @@ const MyAccountPage = () => {
 
       {profile && (
         <div className="my-account">
-          <LabelValueGrid text={language.name}>
-            {profile.username}
-          </LabelValueGrid>
+          <AccountInfoList profile={profile} />
 
-          <LabelValueGrid
-            text={language.phone}
-            tooltip={
-              <Tooltip
-                ariaControls="phone"
-                ariaLabel={language.viewInfo}
-                tooltip={language.phoneInfo}
-              >
-                <Icon iconName={IconName.Info} title="" size="1em" />
-              </Tooltip>
-            }
-          >
-            {profile.phoneNo || notProvided}
-          </LabelValueGrid>
-
-          <LabelValueGrid text={language.fashionPreference}>
-            {language[profile.preferredFashion]}
-          </LabelValueGrid>
-          <LabelValueGrid text={language.dateOfBirth}>
-            {profile.dateOfBirth ? (
-              <DateDisplay date={profile.dateOfBirth} />
-            ) : (
-              notProvided
-            )}
-          </LabelValueGrid>
-          <LabelValueGrid text={language.email}>{profile.email}</LabelValueGrid>
           <ModalContainer
             triggerModalBtnContent={language.update}
             id="id"
