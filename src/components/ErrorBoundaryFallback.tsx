615fba731ee56ba27e6075d98d24b9e87d576fda
jardenna <hbjornum@gmail.com>
2025-09-30 17:36:33 +0200

    error fallback

--- src/components/ErrorBoundaryFallback.tsx
+++ src/components/ErrorBoundaryFallback.tsx
@@ -2,16 +2,24 @@ import useLanguage from '../features/language/useLanguage';
 import ErrorContent from './ErrorContent';
 
 type FallbackProps = {
+  error?: any; // <- her modtager vi fejlen
   resetErrorBoundary: () => void;
 };
 
-const ErrorBoundaryFallback = ({ resetErrorBoundary }: FallbackProps) => {
+const ErrorBoundaryFallback = ({
+  resetErrorBoundary,
+  error,
+}: FallbackProps) => {
   const { language } = useLanguage();
+
+  console.error('Caught error in ErrorBoundary:', error); // optional logging
+
+  const errorText = error?.data?.message ?? language.somethingWentWrong; // brug RTKQ message hvis den findes
 
   return (
     <ErrorContent
       onClick={resetErrorBoundary}
-      errorText={language.somethingWentWrong}
+      errorText={errorText}
       btnLabel={language.retry}
     />
   );
