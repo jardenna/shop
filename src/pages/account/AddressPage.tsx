7ab62dd62e86dd07f4e00b701c9e071fda4970b1
jardenna <hbjornum@gmail.com>
2025-10-02 11:02:51 +0200

    translation

--- src/pages/account/AddressPage.tsx
+++ src/pages/account/AddressPage.tsx
@@ -1,3 +1,26 @@
-const AddressPage = () => <div>Mine adresser psedo text will be replaced</div>;
+import SkeletonParagraph from '../../components/skeleton/SkeletonParagraph';
+import useLanguage from '../../features/language/useLanguage';
+import { useGetUserProfileQuery } from '../../features/profile/profileApiSlice';
+
+const AddressPage = () => {
+  const { language } = useLanguage();
+  const { data: profile, isLoading } = useGetUserProfileQuery();
+  console.log(profile?.addresses);
+
+  return (
+    <>
+      {isLoading && <SkeletonParagraph />}
+      <p>{language.addOrManageAddress}</p>
+
+      {profile && (
+        <div>
+          {profile.addresses.map((address) => (
+            <div key={address.id}>{address.city}</div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default AddressPage;
