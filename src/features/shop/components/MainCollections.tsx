87f851038a2e7ef836318d9550464dec8e456e33
jardenna <hbjornum@gmail.com>
2025-10-18 12:07:24 +0200

    maincoll

--- src/features/shop/components/MainCollections.tsx
+++ src/features/shop/components/MainCollections.tsx
@@ -1,26 +1,44 @@
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorBoundaryFallback from '../../../components/ErrorBoundaryFallback';
+import Skeleton from '../../../components/skeleton/Skeleton';
 import MainCollectionsItem, {
   MainCollectionsBaseProps,
 } from './MainCollectionsItem';
 
 type MainCollectionsProps = {
+  isLoading: boolean;
   mainCollectionList: MainCollectionsBaseProps[];
+  onReset: () => void;
 };
 
-const MainCollections = ({ mainCollectionList }: MainCollectionsProps) => (
+const MainCollections = ({
+  mainCollectionList,
+  onReset,
+  isLoading,
+}: MainCollectionsProps) => (
   <section className="main-collection-container">
-    {mainCollectionList.map(
-      ({ title, imgList, linkTo, linkText, ariaLabelledby }) => (
-        <MainCollectionsItem
-          key={title}
-          ariaLabelledby={ariaLabelledby}
-          imgPath="/images"
-          title={title}
-          imgList={imgList}
-          linkTo={linkTo}
-          linkText={linkText}
-        />
-      ),
+    {isLoading && (
+      <Skeleton
+        count={mainCollectionList.length}
+        className="collections-item"
+        height="28"
+      />
     )}
+    <ErrorBoundary FallbackComponent={ErrorBoundaryFallback} onReset={onReset}>
+      {mainCollectionList.map(
+        ({ title, imgList, linkTo, linkText, ariaLabelledby }) => (
+          <MainCollectionsItem
+            key={title}
+            ariaLabelledby={ariaLabelledby}
+            imgPath="/images"
+            title={title}
+            imgList={imgList}
+            linkTo={linkTo}
+            linkText={linkText}
+          />
+        ),
+      )}
+    </ErrorBoundary>
   </section>
 );
 
