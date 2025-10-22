c57b6cb21461c1e1327bf31b3eeb9e1d4f05b6fa
jardenna <hbjornum@gmail.com>
2025-10-22 14:56:49 +0200

    ErrorBoundary

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,3 +1,5 @@
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorBoundaryFallback from '../components/ErrorBoundaryFallback';
 import useFavorites from '../components/favorites/useFavorites';
 import SkeletonCardList from '../components/skeleton/SkeletonCardList';
 import useLanguage from '../features/language/useLanguage';
@@ -7,29 +9,33 @@ import MainPageContainer from './pageContainer/MainPageContainer';
 
 const FavoritesPage = () => {
   const { language } = useLanguage();
-  const { favorites, isLoading } = useFavorites({});
+  const { favorites, isLoading, onReset } = useFavorites({});
 
   return (
     <MainPageContainer heading="favorites">
       {isLoading && <SkeletonCardList count={4} />}
-
       <ul className="product-card-list">
-        {favorites && favorites.length > 0 ? (
-          favorites.map((product) => (
-            <li key={product.id}>
-              <ProductCard
-                product={product}
-                linkText=""
-                linkTo={`${ShopPath.FavoritesProduct}/${product.id}`}
-              />
-            </li>
-          ))
-        ) : (
-          <div>
-            <h2>{language.noFavoritesYet}</h2>
-            <p>{language.noFavorites}</p>
-          </div>
-        )}
+        <ErrorBoundary
+          FallbackComponent={ErrorBoundaryFallback}
+          onReset={onReset}
+        >
+          {favorites && favorites.length > 0 ? (
+            favorites.map((product) => (
+              <li key={product.id}>
+                <ProductCard
+                  product={product}
+                  linkText=""
+                  linkTo={`${ShopPath.FavoritesProduct}/${product.id}`}
+                />
+              </li>
+            ))
+          ) : (
+            <div>
+              <h2>{language.noFavoritesYet}</h2>
+              <p>{language.noFavorites}</p>
+            </div>
+          )}
+        </ErrorBoundary>
       </ul>
     </MainPageContainer>
   );
