283b1be1a1fadceaa0cbe8535bbe1f140d7b49f2
jardenna <hbjornum@gmail.com>
2025-10-29 13:17:27 +0100

    range

--- src/pages/rangeSlider/RangeSlider.tsx
+++ src/pages/rangeSlider/RangeSlider.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import VisuallyHidden from '../../components/VisuallyHidden';
 import { ChangeInputType } from '../../types/types';
 import './_range-slider.scss';
 
@@ -9,6 +10,8 @@ const RangeSlider = () => {
 
   const [minValue, setMinValue] = useState(min);
   const [maxValue, setMaxValue] = useState(max);
+  const minPos = ((minValue - min) / (max - min)) * 100;
+  const maxPos = ((maxValue - min) / (max - min)) * 100;
 
   const handleRangeChange = (event: ChangeInputType) => {
     const { name, value } = event.target;
@@ -24,34 +27,47 @@ const RangeSlider = () => {
   };
 
   return (
-    <section>
-      <div>
-        <label htmlFor="min">Min</label>
-        <input
-          type="range"
-          value={minValue}
-          onChange={handleRangeChange}
-          min={min}
-          max={max}
-          step={step}
-          name="min"
-          id="min"
+    <div className="range-slider">
+      <div className="input-wrapper">
+        <div>
+          <VisuallyHidden>
+            <label htmlFor="min">Min</label>
+          </VisuallyHidden>
+          <input
+            type="range"
+            value={minValue}
+            onChange={handleRangeChange}
+            min={min}
+            max={max}
+            step={step}
+            name="min"
+            id="min"
+          />
+        </div>
+        <div>
+          <VisuallyHidden>
+            <label htmlFor="max">Max</label>
+          </VisuallyHidden>
+          <input
+            type="range"
+            value={maxValue}
+            onChange={handleRangeChange}
+            min={min}
+            max={max}
+            step={step}
+            name="max"
+            id="max"
+          />
+        </div>
+      </div>
+
+      <div className="rail">
+        <div
+          className="inner-rail"
+          style={{ left: `${minPos}%`, right: `${100 - maxPos}%` }}
         />
       </div>
-      <div>
-        <label htmlFor="max">Max</label>
-        <input
-          type="range"
-          value={maxValue}
-          onChange={handleRangeChange}
-          min={min}
-          max={max}
-          step={step}
-          name="max"
-          id="max"
-        />
-      </div>
-    </section>
+    </div>
   );
 };
 
