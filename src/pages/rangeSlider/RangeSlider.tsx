443bbb92951272df8d463495d44d9e3aceee732e
jardenna <hbjornum@gmail.com>
2025-10-29 19:19:42 +0100

    style

--- src/pages/rangeSlider/RangeSlider.tsx
+++ src/pages/rangeSlider/RangeSlider.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import VisuallyHidden from '../../components/VisuallyHidden';
+import ProductPrice from '../../features/currency/components/ProductPrice';
 import { ChangeInputType } from '../../types/types';
 import './_range-slider.scss';
 
 const RangeSlider = () => {
-  const min = 0;
-  const max = 100;
-  const step = 1;
+  const min = 50;
+  const max = 9000;
+  const step = 50;
 
   const [minValue, setMinValue] = useState(min);
   const [maxValue, setMaxValue] = useState(max);
@@ -27,50 +28,59 @@ const RangeSlider = () => {
   };
 
   return (
-    <div className="range-slider" aria-labelledby="price-range-label">
-      <VisuallyHidden id="price-range-label">Price range</VisuallyHidden>
-      <div className="range-input-container">
-        <VisuallyHidden as="label" htmlFor="minPriceSliderInput">
-          Minimum price kr
-        </VisuallyHidden>
-        <input
-          className="input range-start"
-          type="range"
-          value={minValue}
-          min={min}
-          max={max}
-          name="min"
-          id="minPriceSliderInput"
-          step={step}
-          onChange={handleRangeChange}
-        />
-        <VisuallyHidden as="label" htmlFor="maxPriceSliderInput">
-          Maximum price kr
-        </VisuallyHidden>
-        <input
-          className="input range-end"
-          type="range"
-          value={maxValue}
-          min={min}
-          name="max"
-          id="maxPriceSliderInput"
-          max={max}
-          step={step}
-          onChange={handleRangeChange}
-        />
+    <div className="range-slider-container">
+      <div className="range-slider-info">
+        <ProductPrice price={minValue} />
+        <ProductPrice price={maxValue} />
       </div>
-      <div className="control-container">
-        <span
-          className="control control-start"
-          style={{ left: `${minPos}%` }}
-        />
-        <span className="rail">
-          <span
-            className="inner-rail"
-            style={{ left: `${minPos}%`, right: `${100 - maxPos}%` }}
+      <div className="range-slider" aria-labelledby="price-range-label">
+        <VisuallyHidden id="price-range-label">Price range</VisuallyHidden>
+        <div className="range-input-container">
+          <VisuallyHidden as="label" htmlFor="minPriceSliderInput">
+            Minimum price kr
+          </VisuallyHidden>
+          <input
+            className="input range-start"
+            type="range"
+            value={minValue}
+            min={min}
+            max={max}
+            name="min"
+            id="minPriceSliderInput"
+            step={step}
+            onChange={handleRangeChange}
           />
-        </span>
-        <span className="control control-end" style={{ left: `${maxPos}%` }} />
+          <VisuallyHidden as="label" htmlFor="maxPriceSliderInput">
+            Maximum price kr
+          </VisuallyHidden>
+          <input
+            className="input range-end"
+            type="range"
+            value={maxValue}
+            min={min}
+            name="max"
+            id="maxPriceSliderInput"
+            max={max}
+            step={step}
+            onChange={handleRangeChange}
+          />
+        </div>
+        <div className="control-container">
+          <span
+            className="control control-start"
+            style={{ left: `${minPos}%` }}
+          />
+          <span className="rail">
+            <span
+              className="inner-rail"
+              style={{ left: `${minPos}%`, right: `${100 - maxPos}%` }}
+            />
+          </span>
+          <span
+            className="control control-end"
+            style={{ left: `${maxPos}%` }}
+          />
+        </div>
       </div>
     </div>
   );
