39947189ec9b3ed32b2055645ada14180f4497a5
jardenna <hbjornum@gmail.com>
2025-10-30 09:54:15 +0100

    deleted rangeslider

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,11 +1,9 @@
 import Img from '../components/Img';
 import MainPageContainer from './pageContainer/MainPageContainer';
-import RangeSlider from './rangeSlider/RangeSlider';
 
 const AboutUsPage = () => (
   <MainPageContainer heading="about">
     <section>
-      <RangeSlider />
       <Img src="/images/about/about.jpg" alt="" />
     </section>
   </MainPageContainer>
