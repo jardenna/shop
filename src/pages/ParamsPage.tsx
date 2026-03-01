22212ecff18bb9f1a29127d8b68d86ab0ef4fd20
jardenna <hbjornum@gmail.com>
2026-03-01 09:58:52 +0100

    params from BE

--- src/pages/ParamsPage.tsx
+++ src/pages/ParamsPage.tsx
@@ -3,9 +3,10 @@ import ControlInput from '../components/formElements/ControlInput';
 import DualRange from '../components/formElements/dualRangeSlider/DualRange';
 import useCurrency from '../features/currency/useCurrency';
 import useLanguage from '../features/language/useLanguage';
+import { useGetProductsQuery } from '../features/shop/shopApiSlice';
 import useSearchParamsState from '../hooks/useSearchParamsState';
-import { sortColorsByTranslation } from '../utils/colorUtils';
-import { availableBrands, availableSizes, colors } from '../utils/filters';
+import { colorList, sortColorsByTranslation } from '../utils/colorUtils';
+import { sortSizesDynamic } from '../utils/sizeUtils';
 import { translateKey } from '../utils/utils';
 
 const ParamsPage = () => {
@@ -18,81 +19,93 @@ const ParamsPage = () => {
     minPrice: '',
     maxPrice: '',
   };
-
   const { values, toggleValue, setValue } =
     useSearchParamsState(initialFilters);
-  const sortedTranslatedColors = sortColorsByTranslation(colors, language);
+
+  const { data: products } = useGetProductsQuery({
+    productsPerPage: 10,
+    page: '1',
+    colors: values.colors,
+    brand: values.brand,
+    sizes: values.sizes,
+    mainCategory: 'men',
+    subCategoryId: '',
+  });
+
+  const sortedTranslatedColors = sortColorsByTranslation(colorList, language);
 
   return (
-    <Form
-      submitBtnLabel="Search"
-      onSubmit={() => {
-        console.log(values);
-      }}
-    >
-      <DualRange
-        minValue={values.minPrice}
-        maxValue={values.maxPrice}
-        rangeLabel={language.priceRange}
-        inputNames={{
-          min: 'minPrice',
-          max: 'maxPrice',
+    products && (
+      <Form
+        submitBtnLabel="Search"
+        onSubmit={() => {
+          console.log(values);
         }}
-        inputLabels={{
-          min: 'Pris fra',
-          max: 'Pris til',
-        }}
-        onChange={setValue}
-        unitLabel={currencyText}
-      />
+      >
+        <DualRange
+          minValue={values.minPrice}
+          maxValue={values.maxPrice}
+          rangeLabel={language.priceRange}
+          inputNames={{
+            min: 'minPrice',
+            max: 'maxPrice',
+          }}
+          inputLabels={{
+            min: 'Pris fra',
+            max: 'Pris til',
+          }}
+          onChange={setValue}
+          unitLabel={currencyText}
+        />
 
-      <ul className="checkbox-list">
-        {availableBrands.map((value) => (
-          <li key={value} className="checkbox-item">
-            <input
-              id={value}
-              type="checkbox"
-              name="brand"
-              value={value}
-              checked={values.brand.includes(value)}
-              onChange={toggleValue}
-            />
-            <label htmlFor={value}>{value}</label>
-          </li>
-        ))}
-      </ul>
+        <ul className="checkbox-list">
+          {products.availableBrands.map((value) => (
+            <li key={value} className="checkbox-item">
+              <input
+                id={value}
+                type="checkbox"
+                name="brand"
+                value={value}
+                checked={values.brand.includes(value)}
+                onChange={toggleValue}
+              />
+              <label htmlFor={value}>{value}</label>
+            </li>
+          ))}
+        </ul>
 
-      <ul className="checkbox-list">
-        {availableSizes.map((value) => (
-          <li key={value} className="checkbox-item">
-            <input
-              id={value}
-              name="sizes"
-              value={value}
-              type="checkbox"
-              checked={values.sizes.includes(value)}
-              onChange={toggleValue}
-            />
-            <label htmlFor={value}>{value}</label>
-          </li>
-        ))}
-      </ul>
-      <ul className="checkbox-list">
-        {sortedTranslatedColors.map((value) => (
-          <li key={value} className="checkbox-item">
-            <ControlInput
-              id={value}
-              type="checkbox"
-              name="colors"
-              value={value}
-              checked={values.colors.includes(value)}
-              onChange={toggleValue}
-              label={translateKey(value, language)}
-            />
-          </li>
-        ))}
-      </ul>
-    </Form>
+        <ul className="checkbox-list">
+          {sortSizesDynamic(products.availableSizes).map((value) => (
+            <li key={value} className="checkbox-item">
+              <input
+                id={value}
+                name="sizes"
+                value={value}
+                type="checkbox"
+                checked={values.sizes.includes(value)}
+                onChange={toggleValue}
+              />
+              <label htmlFor={value}>{value}</label>
+            </li>
+          ))}
+        </ul>
+        <ul className="checkbox-list">
+          {sortedTranslatedColors.map((value) => (
+            <li key={value} className="checkbox-item">
+              <ControlInput
+                id={value}
+                type="checkbox"
+                name="colors"
+                value={value}
+                checked={values.colors.includes(value)}
+                onChange={toggleValue}
+                label={translateKey(value, language)}
+              />
+            </li>
+          ))}
+        </ul>
+      </Form>
+    )
   );
 };
 
