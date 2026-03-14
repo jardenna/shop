2c1839542452b0d34f372648095a9e9787ec91f0
jardenna <hbjornum@gmail.com>
2026-03-14 11:39:31 +0100

    togglepanel

---
+++ src/components/togglePanel/TogglePanel.tsx
@@ -0,0 +1,61 @@
+import { ReactNode, RefObject, useId } from 'react';
+import { BtnVariant } from '../../types/enums';
+import type { AriaHasPopup } from '../../types/types';
+import BtnClose from '../BtnClose';
+import Button from '../Button';
+
+type TogglePanelProps = {
+  ariaHasPopup: AriaHasPopup;
+  children: ReactNode;
+  isPanelShown: boolean;
+  panelRef: RefObject<HTMLDivElement | null>;
+  triggerBtnContent: string;
+  ariaLabel?: string;
+  btnVariant?: BtnVariant;
+  className?: string;
+  hideCloseBtn?: boolean;
+  onHidePanel: () => void;
+  onTogglePanel: () => void;
+};
+
+const TogglePanel = ({
+  children,
+  ariaLabel,
+  onTogglePanel,
+  isPanelShown,
+  btnVariant = BtnVariant.Ghost,
+  className = '',
+  triggerBtnContent,
+  ariaHasPopup,
+  panelRef,
+  onHidePanel,
+  hideCloseBtn,
+}: TogglePanelProps) => {
+  const togglePanelId = useId();
+  return (
+    <>
+      <Button
+        className={className}
+        variant={btnVariant}
+        ariaExpanded={isPanelShown}
+        onClick={onTogglePanel}
+        ariaLabel={ariaLabel}
+        ariaHasPopup={ariaHasPopup}
+        ariaControls={togglePanelId}
+      >
+        {triggerBtnContent}
+      </Button>
+      <div
+        ref={panelRef}
+        role="dialog"
+        className={`toggle-panel filter-panel ${isPanelShown ? 'shown' : ''}`}
+        id={togglePanelId}
+        aria-hidden={isPanelShown ? undefined : true}
+      >
+        {!hideCloseBtn && <BtnClose onClick={onHidePanel} />}
+        {children}
+      </div>
+    </>
+  );
+};
+export default TogglePanel;
