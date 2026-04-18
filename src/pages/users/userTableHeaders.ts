4c82128a957cf837d492b053ecf517fdba0e1431
jardenna <hbjornum@gmail.com>
2026-04-18 12:47:15 +0200

    fix sorticon for userpage

--- src/pages/users/userTableHeaders.ts
+++ src/pages/users/userTableHeaders.ts
@@ -2,7 +2,12 @@ import { UserResponse } from '../../app/api/apiTypes/adminApiTypes';
 import { Column } from '../../components/sortTable/Table';
 
 export const tableHeaders: Column<UserResponse>[] = [
-  { key: 'username', label: 'username', name: 'name', tableFilterType: 'text' },
+  {
+    key: 'username',
+    label: 'username',
+    name: 'username',
+    tableFilterType: 'text',
+  },
   { key: 'email', label: 'email', name: 'email', tableFilterType: 'text' },
   { key: 'role', label: 'role', name: 'role', tableFilterType: 'text' },
   { key: 'id', label: '', name: '' },
