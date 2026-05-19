50cf9e48a181b898eeefc5dc94fd2715c0f967a5
jardenna <hbjornum@gmail.com>
2026-05-19 19:06:15 +0200

    send new order

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,17 +1,9 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import Order from '../models/ordersModel.js';
 import Product from '../models/productModel.js';
-
 const createOrder = asyncHandler(async (req, res) => {
-  const {
-    orderItems,
-    shippingAddress,
-    paymentMethod,
-    itemPrice,
-    taxPrice,
-    shippingPrice,
-    totalPrice,
-  } = req.body;
+  const { orderItems, paymentMethod, shippingAddress } = req.body;
+
   if (!orderItems || orderItems.length === 0) {
     return res.status(400).json({
       success: false,
@@ -26,29 +18,48 @@ const createOrder = asyncHandler(async (req, res) => {
       $in: productIds,
     },
   });
-  console.log({ databaseProducts });
 
   const productsWithDiscountedPrices = databaseProducts.map(
     (databaseProduct) => {
+      const matchingOrderItem = orderItems.find(
+        (orderItem) => orderItem.product === databaseProduct._id.toString(),
+      );
+
       const discountedPrice =
         databaseProduct.price -
         (databaseProduct.price * databaseProduct.discount) / 100;
-      console.log({ databaseProduct });
+
+      const subtotal = discountedPrice * matchingOrderItem.qty;
+      const taxPrice = subtotal * 0.2;
+      const noTax = subtotal - taxPrice;
+      console.log(req.body);
 
       return {
         productId: databaseProduct._id,
         productName: databaseProduct.productName,
         image: databaseProduct.images[0],
-        discountedPrice,
+        price: discountedPrice,
+        taxPrice,
+        subtotal,
+        noTax,
+        qty: matchingOrderItem.qty,
       };
     },
   );
+  const itemPrice = productsWithDiscountedPrices.reduce((total, product) => {
+    return total + product.subtotal;
+  }, 0);
 
-  res.status(200).json(productsWithDiscountedPrices);
+  const taxPrice = productsWithDiscountedPrices.reduce((total, product) => {
+    return total + product.taxPrice;
+  }, 0);
+
+  const shippingPrice = itemPrice >= 1500 ? 0 : 49;
+  const totalPrice = itemPrice + shippingPrice;
 
   const order = new Order({
     user: req.user._id,
-    orderItems,
+    orderItems: productsWithDiscountedPrices,
     shippingAddress,
     paymentMethod,
     itemPrice,
@@ -57,9 +68,9 @@ const createOrder = asyncHandler(async (req, res) => {
     totalPrice,
   });
 
-  // const createdOrder = await order.save();
+  const createdOrder = await order.save();
 
-  // res.status(201).json(createdOrder);
+  res.status(201).json(createdOrder);
 });
 
 export { createOrder };
