13e74921653620b32adb9694b00f957959651d44
jardenna <hbjornum@gmail.com>
2026-05-30 16:39:15 +0200

    fix cartUtils

--- src/features/shop/cartUtils.ts
+++ src/features/shop/cartUtils.ts
@@ -4,44 +4,39 @@ interface CartUtilsParams {
   cartItem: CartItem;
   cartList: CartItem[];
 }
-
 export const cartUtils = ({ cartList, cartItem }: CartUtilsParams) => {
-  const identicalVariant = cartList.find(
-    (item) =>
-      item.productId === cartItem.productId &&
-      item.size === cartItem.size &&
-      item.color === cartItem.color,
-  );
-
-  const sameProduct = cartList.find(
+  const existingVariants = cartList.filter(
     (item) => item.productId === cartItem.productId,
   );
 
-  const sameProductDifferentVariant = !!sameProduct && !identicalVariant;
-
-  const existingProduct = cartList.find(
-    (item) => item.productId === cartItem.productId,
+  const identicalVariant = existingVariants.find(
+    (item) => item.size === cartItem.size && item.color === cartItem.color,
   );
-
-  const sizeChanged = existingProduct?.size !== cartItem.size;
-  const colorChanged = existingProduct?.color !== cartItem.color;
 
   if (identicalVariant) {
     return {
       action: 'addToQty',
+      existingVariant: identicalVariant,
     };
   }
 
-  if (
-    sameProductDifferentVariant &&
-    ((sizeChanged && !colorChanged) || (!sizeChanged && colorChanged))
-  ) {
+  const popupVariant = existingVariants.find((item) => {
+    const sameSize = item.size === cartItem.size;
+    const sameColor = item.color === cartItem.color;
+
+    return (sameSize && !sameColor) || (!sameSize && sameColor);
+  });
+
+  if (popupVariant) {
     return {
       action: 'showPopup',
+      existingVariant: popupVariant,
+      incomingVariant: cartItem,
     };
   }
 
   return {
     action: 'addToCartList',
+    incomingVariant: cartItem,
   };
 };
