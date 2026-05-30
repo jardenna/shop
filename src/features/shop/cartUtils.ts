dc03db219ec6eee95fb885f49769d49e9c54ba29
jardenna <hbjornum@gmail.com>
2026-05-30 16:07:18 +0200

    cartUtils

--- src/features/shop/cartUtils.ts
+++ src/features/shop/cartUtils.ts
@@ -1,10 +1,41 @@
 import { CartItem } from '../../app/api/apiTypes/sharedApiTypes';
 
 interface CartUtilsParams {
-  cart: CartItem[];
   cartItem: CartItem;
+  cartList: CartItem[];
 }
 
-export const cartUtils = ({ cart, cartItem }: CartUtilsParams) => {
-  console.log(cart, cartItem);
+export const cartUtils = ({ cartList, cartItem }: CartUtilsParams) => {
+  const identicalVariant = cartList.find(
+    (item) =>
+      item.productId === cartItem.productId &&
+      item.size === cartItem.size &&
+      item.color === cartItem.color,
+  );
+
+  const sameProduct = cartList.find(
+    (item) => item.productId === cartItem.productId,
+  );
+
+  const sameProductDifferentVariant = !!sameProduct && !identicalVariant;
+
+  const existingProduct = cartList.find(
+    (item) => item.productId === cartItem.productId,
+  );
+
+  const sizeChanged = existingProduct?.size !== cartItem.size;
+  const colorChanged = existingProduct?.color !== cartItem.color;
+
+  if (identicalVariant) {
+    return 'add to qty';
+  }
+
+  if (
+    sameProductDifferentVariant &&
+    ((sizeChanged && !colorChanged) || (!sizeChanged && colorChanged))
+  ) {
+    return 'popup';
+  }
+
+  return 'add to cartlist';
 };
