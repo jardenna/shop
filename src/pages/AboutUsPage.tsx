a99c2df5da8eca3db6343588a3a04f87c7483a0b
jardenna <hbjornum@gmail.com>
2026-05-31 17:16:16 +0200

    sizeguide

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,7 +1,9 @@
+import SizeGuide from '../components/SizeGuide';
 import MainPageContainer from './pageContainer/MainPageContainer';
 
 const AboutUsPage = () => (
   <MainPageContainer heading="about">
+    <SizeGuide />
     <section>about</section>
   </MainPageContainer>
 );
