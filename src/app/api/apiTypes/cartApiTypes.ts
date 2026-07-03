892f14fd158c412a54ebd16c8cbd9b9b2d46399f
jardenna <hbjornum@gmail.com>
2026-07-03 09:38:30 +0200

    type

--- src/app/api/apiTypes/cartApiTypes.ts
+++ src/app/api/apiTypes/cartApiTypes.ts
@@ -2,6 +2,7 @@ import type { DefaultResponseType, Size } from './sharedApiTypes';
 
 export interface CartItem {
   color: string;
+  id: string;
   productId: string;
   qty: number;
   size: Size;
