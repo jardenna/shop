3f3cb111b1a827f7dcd72301a384d8cadc78f74a
jardenna <hbjornum@gmail.com>
2026-07-13 16:17:08 +0200

    style

--- src/features/cart/components/CartSummary.tsx
+++ src/features/cart/components/CartSummary.tsx
@@ -1,5 +1,7 @@
 import { CartListSummary } from '../../../app/api/apiTypes/cartApiTypes';
+import Button from '../../../components/Button';
 import ProductPrice from '../../shop/components/productPrice/ProductPrice';
+import './cartSummary.styles.scss';
 
 interface CartSummaryProps {
   language: Record<string, string>;
@@ -7,34 +9,41 @@ interface CartSummaryProps {
 }
 
 const CartSummary = ({ summary, language }: CartSummaryProps) => {
-  console.log(summary);
+  const summaryItems = [
+    {
+      label: language.subtotal,
+      price: summary.subTotal,
+    },
+    summary.discountPrice > 0
+      ? {
+          label: language.discount,
+          price: summary.discountPrice,
+          className: 'summary-discount',
+        }
+      : null,
+    {
+      label: language.estimatedShipping,
+      price: summary.shippingPrice,
+    },
+    {
+      label: language.totalPrice,
+      price: summary.totalPrice,
+      className: 'summary-total',
+    },
+  ].filter((item) => item !== null);
 
   return (
-    <section>
-      <div>
-        <span>{language.subtotal}</span>
-        <span>
-          <ProductPrice price={summary.subTotal} />
-        </span>
-      </div>
-      <div>
-        <span>{language.discount}</span>
-        <span>
-          <ProductPrice price={summary.discountPrice} />
-        </span>
-      </div>
-      <div>
-        <span>{language.estimatedShipping}</span>
-        <span>
-          <ProductPrice price={summary.shippingPrice} />
-        </span>
-      </div>
-      <div>
-        <span>{language.totalPrice}</span>
-        <span>
-          <ProductPrice price={summary.totalPrice} />
-        </span>
-      </div>
+    <section className="summary-list">
+      <h2>Summary</h2>
+      {summaryItems.map(({ label, price, className }) => (
+        <div key={label} className={`summary-item ${className ?? ''}`}>
+          <span>{label}</span>
+          <span className="summary-info">
+            <ProductPrice price={price} />
+          </span>
+        </div>
+      ))}
+      <Button>Fortsæt til kassen</Button>
     </section>
   );
 };
