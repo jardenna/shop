3def013d28812c2a84e754ff8dc173f2d2171ee7
jardenna <hbjornum@gmail.com>
2026-07-15 14:17:34 +0200

    total qty

--- src/features/orders/components/OrderSummaryList.tsx
+++ src/features/orders/components/OrderSummaryList.tsx
@@ -2,6 +2,7 @@ import { CartListResponse } from '../../../app/api/apiTypes/cartApiTypes';
 import DeleteItem from '../../../components/deleteItem/DeleteItem';
 import Img from '../../../components/Img';
 import SkeletonList from '../../../components/skeleton/SkeletonList';
+import { getCartQuantity } from '../../../utils/reduceQty';
 import './_order.scss';
 import SummaryInfo from './SummaryInfo';
 
@@ -24,38 +25,40 @@ const OrderSummaryList = ({
 
   return (
     <aside className="order-summary">
-      <h2 className="summary-title">
-        {language.orderSummary}[add reduced qty]
-      </h2>
       {orderItems && (
-        <ul className="order-summary-list">
-          {orderItems.cartItems.map(
-            ({ id, image, productName, color, size, qty }) => (
-              <li key={id}>
-                <article className="order-summary-item">
-                  <Img src={image} alt="" className="summary-img" />
-                  <div>
-                    <h3 className="summary-item-title">{productName}</h3>
-                    <SummaryInfo
-                      qty={qty}
-                      color={color}
-                      size={size}
-                      language={language}
-                    />
+        <>
+          <h2 className="summary-title">
+            {language.orderSummary} [ {getCartQuantity(orderItems.cartItems)} ]
+          </h2>
+          <ul className="order-summary-list">
+            {orderItems.cartItems.map(
+              ({ id, image, productName, color, size, qty }) => (
+                <li key={id}>
+                  <article className="order-summary-item">
+                    <Img src={image} alt="" className="summary-img" />
+                    <div>
+                      <h3 className="summary-item-title">{productName}</h3>
+                      <SummaryInfo
+                        qty={qty}
+                        color={color}
+                        size={size}
+                        language={language}
+                      />
 
-                    <DeleteItem
-                      ariaLabel={`${language.delete} ${productName}`}
-                      onDeleteItem={() => {
-                        deleteCartItem(id);
-                      }}
-                      itemName={productName}
-                    />
-                  </div>
-                </article>
-              </li>
-            ),
-          )}
-        </ul>
+                      <DeleteItem
+                        ariaLabel={`${language.delete} ${productName}`}
+                        onDeleteItem={() => {
+                          deleteCartItem(id);
+                        }}
+                        itemName={productName}
+                      />
+                    </div>
+                  </article>
+                </li>
+              ),
+            )}
+          </ul>
+        </>
       )}
     </aside>
   );
