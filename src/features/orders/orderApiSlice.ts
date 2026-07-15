ad5c004697ed9dc43fef3afdb7bdec9f7f95fb49
jardenna <hbjornum@gmail.com>
2026-07-15 11:05:28 +0200

    types for createorder

--- src/features/orders/orderApiSlice.ts
+++ src/features/orders/orderApiSlice.ts
@@ -1,9 +1,13 @@
 import apiSlice from '../../app/api/apiSlice';
+import {
+  CreateOrderResponse,
+  CreateOrderRequest,
+} from '../../app/api/apiTypes/orderApiTypes';
 import { ordersUrl } from '../../app/endpoints';
 
 export const orderApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    createOrder: builder.mutation<any, any>({
+    createOrder: builder.mutation<CreateOrderResponse, CreateOrderRequest>({
       query: (body) => ({
         url: ordersUrl,
         method: 'POST',
