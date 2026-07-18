f15fe961666b5a8ba3a619493f7fd6de08e74e19
jardenna <hbjornum@gmail.com>
2026-07-18 17:30:10 +0200

    payment with fields

--- server/config/constants.js
+++ server/config/constants.js
@@ -67,21 +67,84 @@ export const PAYMENT_METHODS = [
     id: 'visa',
     label: 'Visa',
     icon: 'visa',
+    fields: [
+      {
+        name: 'cardNumber',
+        label: 'Card number',
+        type: 'text',
+      },
+      {
+        name: 'expiryDate',
+        label: 'Expiry date',
+        type: 'text',
+      },
+      {
+        name: 'cvvCode',
+        label: 'Security code',
+        type: 'password',
+      },
+      {
+        name: 'cardholderName',
+        label: 'Cardholder name',
+        type: 'text',
+      },
+    ],
   },
   {
     id: 'mastercard',
     label: 'Mastercard',
     icon: 'mastercard',
+    fields: [
+      {
+        name: 'cardNumber',
+        label: 'Card number',
+        type: 'text',
+      },
+      {
+        name: 'expiryDate',
+        label: 'Expiry date',
+        type: 'text',
+      },
+      {
+        name: 'cvvCode',
+        label: 'Security code',
+        type: 'password',
+      },
+      {
+        name: 'cardholderName',
+        label: 'Cardholder name',
+        type: 'text',
+      },
+    ],
   },
   {
     id: 'paypal',
     label: 'PayPal',
     icon: 'paypal',
+    fields: [
+      {
+        name: 'paypalEmail',
+        label: 'Email',
+        type: 'email',
+      },
+      {
+        name: 'paypalPassword',
+        label: 'Password',
+        type: 'password',
+      },
+    ],
   },
   {
     id: 'mobilepay',
     label: 'MobilePay',
     icon: 'mobilepay',
+    fields: [
+      {
+        name: 'phoneNumber',
+        label: 'Phone number',
+        type: 'tel',
+      },
+    ],
   },
 ];
 
