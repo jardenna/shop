78327dcb5f04b728208ab5f587f0cc0d77986e48
jardenna <hbjornum@gmail.com>
2026-07-18 15:54:01 +0200

    rename class

--- src/features/cart/components/CartInfo.tsx
+++ src/features/cart/components/CartInfo.tsx
@@ -5,14 +5,14 @@ interface CartInfoProps {
 }
 
 const CartInfo = ({ language }: CartInfoProps) => (
-  <div className="cart-info">
+  <div className="cart-page-info">
     <span>{language.pricesConfirmed}</span>
     <span>
-      <span className="cart-info-text">{language.returnPeriod}</span>
+      <span className="cart-page-info-text">{language.returnPeriod}</span>
       <Link to="/">{language.returnPolicyLink}</Link>
     </span>
     <span>
-      <span className="cart-info-text">{language.needHelp}?</span>
+      <span className="cart-page-info-text">{language.needHelp}?</span>
       <Link to="/">{language.contactCustomerService}</Link>
     </span>
   </div>
