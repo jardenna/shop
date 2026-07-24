beeeae6c646263d813291098251f75314a5545a6
jardenna <hbjornum@gmail.com>
2026-07-24 09:17:37 +0200

    be regex

--- server/utils/regex.js
+++ server/utils/regex.js
@@ -1,3 +1,10 @@
-const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+export const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+// const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
-export { emailRegex };
+export const cardNumberRegex = /^\d{16}$/;
+
+export const expiryDateRegex = /^(0[1-9]|1[0-2])\/\d{2}$/;
+
+export const securityCodeRegex = /^\d{3,4}$/;
+
+export const mobilePhoneNumberRegex = /^\d{8}$/;
