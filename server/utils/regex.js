e810bb3d650658ecff94b2d23afe469997b3c8ca
jardenna <hbjornum@gmail.com>
2026-07-24 09:19:06 +0200

    removed comment

--- server/utils/regex.js
+++ server/utils/regex.js
@@ -1,5 +1,4 @@
 export const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-// const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
 export const cardNumberRegex = /^\d{16}$/;
 
