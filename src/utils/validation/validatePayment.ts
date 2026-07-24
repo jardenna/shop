7fbeded252e0ab72f51b0629053cca06fb50b2fe
jardenna <hbjornum@gmail.com>
2026-07-24 09:48:19 +0200

    validate payment fe

--- src/utils/validation/validatePayment.ts
+++ src/utils/validation/validatePayment.ts
@@ -1,27 +1,104 @@
+import {
+  PaymentMethods,
+  paymentMethods,
+} from '../../app/api/apiTypes/paymentApiTypes';
 import type { ValidationErrors } from '../../hooks/useFormValidation';
 import { ValidationMessage } from '../../types/enums';
-import type { OmitChecked } from '../../types/types';
-import { validateEmail } from './CommonFieldValidation';
-import type { AuthFormValues } from './validateCreateAccount';
+import {
+  cardNumberRegex,
+  emailRegex,
+  expiryDateRegex,
+  mobilePhoneNumberRegex,
+  securityCodeRegex,
+} from '../regex';
 
-type validatePayment = OmitChecked<
-  AuthFormValues,
-  'username' | 'confirmPassword'
->;
+export interface PaymentFormValues {
+  cardholderName: string;
+  cardNumber: string;
+  cvvCode: string;
+  expiryDate: string;
+  mobilePhoneNumber: string;
+  paymentMethod: PaymentMethods;
+  paypalEmail: string;
+  paypalPassword: string;
+}
 
-export function validatePayment(values: validatePayment) {
-  const errors: ValidationErrors<validatePayment> = {};
-  const { email, password } = values;
+type ValidatePayment = PaymentFormValues;
 
-  // Email Errors
-  const emailError = validateEmail(email);
-  if (emailError) {
-    errors.email = emailError;
+export function validatePayment(values: ValidatePayment) {
+  const errors: ValidationErrors<ValidatePayment> = {};
+
+  const {
+    paymentMethod,
+    cardNumber,
+    expiryDate,
+    cvvCode,
+    cardholderName,
+    paypalEmail,
+    paypalPassword,
+    mobilePhoneNumber,
+  } = values;
+
+  if (
+    paymentMethod === paymentMethods.visa ||
+    paymentMethod === paymentMethods.mastercard
+  ) {
+    const sanitizedCardNumber = cardNumber?.replace(/\s/g, '');
+
+    if (!sanitizedCardNumber || !cardNumberRegex.test(sanitizedCardNumber)) {
+      errors.cardNumber =
+        ValidationMessage.CardNumberMustContainExactly16Digits;
+    }
+
+    if (!expiryDate || !expiryDateRegex.test(expiryDate)) {
+      errors.expiryDate = ValidationMessage.InvalidExpiryDateFormat;
+    } else {
+      const [month, year] = expiryDate.split('/').map(Number);
+
+      const today = new Date();
+      const currentMonth = today.getMonth() + 1;
+      const currentYear = today.getFullYear() % 100;
+
+      if (
+        year < currentYear ||
+        (year === currentYear && month < currentMonth)
+      ) {
+        errors.expiryDate = ValidationMessage.CardHasExpired;
+      }
+    }
+
+    if (!cvvCode || !securityCodeRegex.test(cvvCode)) {
+      errors.cvvCode = ValidationMessage.SecurityCodeMustContain3Or4Digits;
+    }
+
+    if (!cardholderName.trim()) {
+      errors.cardholderName = ValidationMessage.PleaseEnterCardholderName;
+    }
   }
 
-  // Password Errors
-  if (!password) {
-    errors.password = ValidationMessage.PleaseEnterPassword;
+  if (paymentMethod === paymentMethods.paypal) {
+    if (!paypalEmail.trim()) {
+      errors.paypalEmail = ValidationMessage.PleaseEnterEmail;
+    } else if (!emailRegex.test(paypalEmail)) {
+      errors.paypalEmail = ValidationMessage.PleaseEnterValidEmail;
+    }
+
+    if (!paypalPassword || paypalPassword.length < 6) {
+      errors.paypalPassword =
+        ValidationMessage.PayPalPasswordMustContainAtLeast6Characters;
+    }
+  }
+
+  if (paymentMethod === paymentMethods.mobilepay) {
+    const sanitizedPhoneNumber = mobilePhoneNumber.replace(/\s/g, '');
+
+    if (
+      !sanitizedPhoneNumber ||
+      !mobilePhoneNumberRegex.test(sanitizedPhoneNumber)
+    ) {
+      errors.mobilePhoneNumber =
+        ValidationMessage.PhoneNumberMustContainExactly8Digits;
+    }
   }
 
   return errors;
