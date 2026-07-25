979ac01def482062e8df39b471a62d32e2143b39
jardenna <hbjornum@gmail.com>
2026-07-25 09:26:11 +0200

    skeleton input style

--- src/components/skeleton/SkeletonInput.tsx
+++ src/components/skeleton/SkeletonInput.tsx
@@ -2,7 +2,7 @@ import Skeleton from './Skeleton';
 
 const SkeletonInput = () => (
   <div className="skeleton-input skeleton-column">
-    <Skeleton height="1.3" width="14" />
+    <Skeleton height="0.875" width="14" />
     <Skeleton height="4" />
   </div>
 );
