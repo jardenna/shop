7e29bb09bcea1d8157d71441a1410b9cd1b663c1
jardenna <hbjornum@gmail.com>
2026-07-26 16:41:45 +0200

    skeleton nav item

--- src/components/skeleton/SkeletonNavItem.tsx
+++ src/components/skeleton/SkeletonNavItem.tsx
@@ -1,7 +1,7 @@
 import Skeleton, { SkeletonProps } from './Skeleton';
 
 const SkeletonNavItem = ({ width, className = '' }: SkeletonProps) => (
-  <div className={`nav-item ${className}`}>
+  <div className={`skeleton-nav-item ${className}`}>
     <Skeleton className="nav-icon" />
     <Skeleton className="nav-label" width={width} />
   </div>
