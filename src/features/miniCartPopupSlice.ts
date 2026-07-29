2d2aa47fe04a01409777d120e330bb800f107bee
jardenna <hbjornum@gmail.com>
2026-07-29 14:22:57 +0200

    minicart slice

---
+++ src/features/miniCartPopupSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../app/store';
+
+interface MiniCartState {
+  isOpen: boolean;
+}
+
+const initialState: MiniCartState = {
+  isOpen: false,
+};
+
+const miniCartPopupSlice = createSlice({
+  name: 'miniCartPopupSlice',
+  initialState,
+  reducers: {
+    openMiniCart: (state) => {
+      state.isOpen = true;
+    },
+    closeMiniCart: (state) => {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const selectIsMiniCardOpen = (state: RootState) =>
+  state.miniCartIsOpen.isOpen;
+export const { openMiniCart, closeMiniCart } = miniCartPopupSlice.actions;
+
+export default miniCartPopupSlice.reducer;
