555e89a916fa80f908aa8400b99aad54442680f7
jardenna <hbjornum@gmail.com>
2026-07-29 15:47:15 +0200

    close minicart

--- src/features/cart/components/miniCartPopup/MiniCartPopup.tsx
+++ src/features/cart/components/miniCartPopup/MiniCartPopup.tsx
@@ -1,20 +1,40 @@
-import { useAppSelector } from '../../../../app/hooks';
+import { useRef } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../../app/hooks';
 import LabelValueGrid from '../../../../components/labelValueGrid/LabelValueGrid';
 import Portal from '../../../../components/Portal';
+import { useClickOutside } from '../../../../hooks/useClickOutside';
+import { useKeyPress } from '../../../../hooks/useKeyPress';
+import { KeyCode } from '../../../../types/enums';
 import { selectUser } from '../../../auth/authSlice';
 import { useLanguage } from '../../../language/useLanguage';
-import { selectIsMiniCartOpen } from '../../../miniCartPopupSlice';
+import {
+  closeMiniCart,
+  selectIsMiniCartOpen,
+} from '../../../miniCartPopupSlice';
 import ProductPrice from '../../../shop/components/productPrice/ProductPrice';
 import { useActiveCart } from '../../useActiveCart';
 import OrderItemCard from '../orderItemCard/OrderItemCard';
 import './_mini-cart-popup.scss';
 
 const MiniCartPopup = () => {
-  const { language } = useLanguage();
+  const dispatch = useAppDispatch();
   const loggedInUser = useAppSelector(selectUser);
+  const { language } = useLanguage();
   const currentUser = loggedInUser?.user ?? null;
   const { apiCartList } = useActiveCart({ currentUser });
   const isMiniCartOpen = useAppSelector(selectIsMiniCartOpen);
+
+  const miniCartRef = useRef<HTMLUListElement>(null);
+
+  const handleCloseMiniCart = () => {
+    dispatch(closeMiniCart());
+  };
+
+  useKeyPress(handleCloseMiniCart, [KeyCode.Esc]);
+
+  useClickOutside(miniCartRef, () => {
+    handleCloseMiniCart();
+  }, [miniCartRef]);
 
   if (!apiCartList || !isMiniCartOpen) {
     return null;
@@ -22,7 +42,7 @@ const MiniCartPopup = () => {
 
   return (
     <Portal portalId="miniCard">
-      <ul className="mini-cart">
+      <ul className="mini-cart" ref={miniCartRef}>
         {apiCartList.cartItems.map((order) => (
           <li key={order.id} className="mini-cart-item">
             <OrderItemCard order={order} language={language} />
