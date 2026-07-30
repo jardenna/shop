4a58103d2edb792cbf02a27b7f21f8102f36af61
jardenna <hbjornum@gmail.com>
2026-07-30 20:00:41 +0200

    translation

--- src/pages/OrderConfirmation.tsx
+++ src/pages/OrderConfirmation.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router';
+import Button from '../components/Button';
 import { useLanguage } from '../features/language/useLanguage';
 import { useGetOrderByIdQuery } from '../features/orders/orderApiSlice';
 import MainPageContainer from './pageContainer/MainPageContainer';
@@ -11,8 +12,15 @@ const OrderConfirmation = () => {
   console.log(order);
 
   return (
-    <MainPageContainer heading={language.checkout}>
-      {language.order}
+    <MainPageContainer heading={language.order}>
+      <h2>{language.orderConfirmationTitle}</h2>
+      <p>{language.orderConfirmationDescription}</p>
+      <p>{language.orderConfirmationProcessing}</p>
+
+      <Button>{language.trackYourOrder}</Button>
+      <p>{language.orderNumber}</p>
+      <p>{language.orderSummary}</p>
+      <p>{language.paymentMethod}</p>
     </MainPageContainer>
   );
 };
