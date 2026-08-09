a8019f4b67ee2f92a36fa6da3a10334cdba04de5
jardenna <hbjornum@gmail.com>
2026-08-09 18:44:56 +0200

    new endpoint for admin

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -230,6 +230,12 @@ const getOrderById = asyncHandler(async (req, res) => {
     select: '_id username',
   });
 
+  if (!order) {
+    return res
+      .status(404)
+      .json({ success: false, message: t('orderNotFound', req.lang) });
+  }
+
   const orderBelongsToUser =
     order.user._id.toString() === req.user._id.toString();
 
@@ -239,6 +245,19 @@ const getOrderById = asyncHandler(async (req, res) => {
       message: t('orderNotFound', req.lang),
     });
   }
+
+  res.status(200).json(order);
+});
+
+// @desc    Get admin order by ID
+// @route   GET /api/admin/orders/:id
+// @method  GET
+// @access  Private for admin and employees
+const getAdminOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate({
+    path: 'user',
+    select: '_id username',
+  });
 
   if (!order) {
     return res
@@ -405,6 +424,7 @@ const deliverOrder = asyncHandler(async (req, res) => {
 export {
   createOrder,
   deliverOrder,
+  getAdminOrderById,
   getAllOrders,
   getOrderById,
   getUserOrders,
