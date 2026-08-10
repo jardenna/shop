2f91c6e7045ac5861cd06e88a8ec87cd485ac9af
jardenna <hbjornum@gmail.com>
2026-08-10 09:24:11 +0200

    details

--- src/pages/adminOrderPage/AdminOrderDetailsPage.tsx
+++ src/pages/adminOrderPage/AdminOrderDetailsPage.tsx
@@ -1,14 +1,30 @@
 import { useParams } from 'react-router';
+import StatusTracker from '../../components/statusTracker/StatusTracker';
+import SummaryList from '../../features/cart/components/SummaryList';
 import { useLanguage } from '../../features/language/useLanguage';
+import OrderAddressList from '../../features/orders/components/OrderAddressList';
 import OrderList from '../../features/orders/components/orders/OrderList';
 import { useGetAdminOrderByIdQuery } from '../../features/orders/orderApiSlice';
+import { createOrderAddressList } from '../../features/orders/utils/createOrderAddressList';
+import { orderTrackingList } from '../../features/orders/utils/createTrackingList';
 import { AdminPath } from '../../layout/nav/enums';
 import AdminPageContainer from '../pageContainer/AdminPageContainer';
 
 const AdminOrderDetailsPage = () => {
   const { id } = useParams();
   const { language } = useLanguage();
-  const { data: order } = useGetAdminOrderByIdQuery(id ?? '');
+  const { data: order, refetch } = useGetAdminOrderByIdQuery(id ?? '');
+
+  const status = {
+    status: order?.delivery.status ?? 'created',
+  };
+
+  const addressList = order
+    ? createOrderAddressList({
+        billingAddress: order.billingAddress,
+        shippingAddress: order.shippingAddress,
+      })
+    : [];
 
   return (
     <AdminPageContainer
@@ -16,9 +32,17 @@ const AdminOrderDetailsPage = () => {
       linkText={language.createNewCategory}
       linkTo={AdminPath.AdminSubCategoryCreate}
     >
+      <StatusTracker steps={orderTrackingList} status={status} />
       {order && (
         <article className="order-cart">
           <OrderList orders={order.orderItems} language={language} />
+          <SummaryList
+            language={language}
+            summary={order.summary}
+            promoDiscount={order.discount}
+          />
+
+          <OrderAddressList addresses={addressList} refetch={refetch} />
         </article>
       )}
     </AdminPageContainer>
