b58c40e512d4ec920c56e24fdda9d3204af64ae1
jardenna <hbjornum@gmail.com>
2026-08-10 13:33:29 +0200

    style

--- src/features/orders/components/orders/OrderList.tsx
+++ src/features/orders/components/orders/OrderList.tsx
@@ -2,20 +2,29 @@ import { BaseOrder } from '../../../../app/api/apiTypes/cartApiTypes';
 import Img from '../../../../components/Img';
 import LabelValue from '../../../../components/LabelValue';
 import { translateKey } from '../../../../utils/utils';
+import ProductPrice from '../../../shop/components/productPrice/ProductPrice';
 
 export interface OrderItems extends BaseOrder {
+  discount: number;
   id: string;
   image: string;
+  price: number;
   productName: string;
 }
 
 interface OrderListProps {
   language: Record<string, string>;
   orders: OrderItems[];
+  showPrice?: boolean;
   variant?: string;
 }
 
-const OrderList = ({ orders, language, variant = '' }: OrderListProps) => (
+const OrderList = ({
+  orders,
+  language,
+  showPrice,
+  variant = '',
+}: OrderListProps) => (
   <ul className={`order-list ${variant}`}>
     {orders.map((order) => (
       <li className="order-list-item" key={order.id}>
@@ -23,6 +32,9 @@ const OrderList = ({ orders, language, variant = '' }: OrderListProps) => (
           <Img src={order.image} alt="" className="order-img" />
           <div>
             <h2 className="order-item-title">{order.productName}</h2>
+            {showPrice && (
+              <ProductPrice price={order.price} discount={order.discount} />
+            )}
             <div className="new-order-meta">
               <LabelValue
                 label={language.color}
