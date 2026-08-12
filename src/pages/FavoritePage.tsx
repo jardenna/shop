cc64d96779a9c5010eb246d4221dcf0018509171
jardenna <hbjornum@gmail.com>
2026-08-12 17:43:42 +0200

    rename cartform

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -22,7 +22,7 @@ import MainPageContainer from './pageContainer/MainPageContainer';
 import FavoritesPanelCart from '../features/favorites/components/FavoritesPanelCart';
 import CartForm, {
   InitialShopValues,
-} from '../features/shop/components/singleProduct/CartForm';
+} from '../features/shop/components/singleProduct/CartFormNew';
 
 const FavoritePage = () => {
   const { language } = useLanguage();
