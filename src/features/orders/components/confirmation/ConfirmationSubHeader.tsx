4a9e626a1e518cd3d44731e503593e4b3c36cf5e
jardenna <hbjornum@gmail.com>
2026-08-14 17:46:04 +0200

    translate

--- src/features/orders/components/confirmation/ConfirmationSubHeader.tsx
+++ src/features/orders/components/confirmation/ConfirmationSubHeader.tsx
@@ -5,8 +5,8 @@ const ConfirmationSubHeader = () => {
 
   return (
     <section className="confirmation-sub-header">
-      <h2>{language.orderConfirmationDescription}</h2>
-      <p>{language.orderConfirmationProcessing}</p>
+      <h2>{language.orderStatusMessage}</h2>
+      <p>{language.orderCancelled}</p>
     </section>
   );
 };
