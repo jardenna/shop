655f201ef04615c3e3bcb6b6a3d384e57ec7f01c
jardenna <hbjornum@gmail.com>
2026-08-16 10:51:46 +0200

    SimpleTable comp

---
+++ src/components/simpleTable/SimpleTable.tsx
@@ -0,0 +1,49 @@
+import type { ReactNode } from 'react';
+import { useLanguage } from '../../features/language/useLanguage';
+import VisuallyHidden from '../VisuallyHidden';
+
+interface TableHeaderItem {
+  label: string;
+}
+
+interface SimpleTableProps<RowData> {
+  tableCaption: string;
+  tableDataList: RowData[];
+  tableHeaderList: TableHeaderItem[];
+  getRowKey: (rowData: RowData) => string;
+  renderCells: (rowData: RowData) => ReactNode;
+}
+
+const SimpleTable = <RowData,>({
+  tableHeaderList,
+  tableDataList,
+  renderCells,
+  getRowKey,
+  tableCaption,
+}: SimpleTableProps<RowData>) => {
+  const { language } = useLanguage();
+
+  return (
+    <table className="simple-table">
+      <VisuallyHidden as="caption">{tableCaption}</VisuallyHidden>
+
+      <thead>
+        <tr>
+          {tableHeaderList.map(({ label }) => (
+            <th key={label} scope="col">
+              {language[label]}
+            </th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {tableDataList.map((rowData) => (
+          <tr key={getRowKey(rowData)}>{renderCells(rowData)}</tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default SimpleTable;
