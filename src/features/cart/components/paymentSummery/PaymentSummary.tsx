aa699b83348f585cb665654ad09421f08126f47d
jardenna <hbjornum@gmail.com>
2026-08-28 08:51:41 +0200

    order-summary

--- src/features/cart/components/paymentSummery/PaymentSummary.tsx
+++ src/features/cart/components/paymentSummery/PaymentSummary.tsx
@@ -2,6 +2,7 @@ import { OrderResponse } from '../../../../app/api/apiTypes/orderApiTypes';
 import OrderHeading from '../../../orders/components/orderHeading/OrderHeading';
 import OrderList from '../../../orders/components/orders/OrderList';
 import SummaryList from './SummaryList';
+import './paymentSummary.styles.scss';
 
 interface PaymentSummaryProps {
   language: Record<string, string>;
@@ -9,12 +10,11 @@ interface PaymentSummaryProps {
 }
 
 const PaymentSummary = ({ language, order }: PaymentSummaryProps) => (
-  <section className="confirmation-summary">
+  <section className="order-summary">
     <article className="summary-items">
       <OrderHeading heading={language.orderedItems} />
       <OrderList orders={order.orderItems} language={language} />
     </article>
-
     <article>
       <OrderHeading heading={language.priceOverview} />
       <div className="summary-payment">
