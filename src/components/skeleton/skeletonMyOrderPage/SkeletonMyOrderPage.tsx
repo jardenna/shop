bb6b4ceb38980d0e97b8b7e6b3a82db58559b59e
jardenna <hbjornum@gmail.com>
2026-08-29 10:44:50 +0200

    new comp

--- src/components/skeleton/skeletonMyOrderPage/SkeletonMyOrderPage.tsx
+++ src/components/skeleton/skeletonMyOrderPage/SkeletonMyOrderPage.tsx
@@ -1,8 +1,6 @@
 import Skeleton from '../Skeleton';
-import SkeletonBadge from '../SkeletonBadge';
-import SkeletonButton from '../SkeletonButton';
 import SkeletonParagraph from '../SkeletonParagraph';
-import SkeletonMyOrderSummary from './SkeletonMyOrderSummary';
+import SkeletonOrderCart from './SkeletonOrderCart';
 import './_skeleton-my-order-page.scss';
 
 const SkeletonMyOrderPage = () => (
@@ -11,25 +9,7 @@ const SkeletonMyOrderPage = () => (
       <Skeleton className="skeleton-sub-header-title" />
       <SkeletonParagraph count={2} />
     </span>
-    <div className="my-orders-page-cart skeleton-order-cart">
-      <div className="order-header">
-        <div>
-          <div className="skeleton order-id" />
-          <SkeletonBadge width="8" />
-        </div>
-        <div className="skeleton order-price" />
-      </div>
-
-      <div className="skeleton-content-row">
-        <SkeletonMyOrderSummary />
-      </div>
-      <Skeleton className="skeleton-divider" />
-
-      <div className="footer-row">
-        <div className="skeleton delivery-date" />
-        <SkeletonButton width="10" />
-      </div>
-    </div>
+    <SkeletonOrderCart />
   </div>
 );
 
--- src/components/skeleton/skeletonMyOrderPage/SkeletonOrderCart.tsx
+++ src/components/skeleton/skeletonMyOrderPage/SkeletonOrderCart.tsx
@@ -4,8 +4,15 @@ import SkeletonButton from '../SkeletonButton';
 import SkeletonMyOrderSummary from './SkeletonMyOrderSummary';
 
 const SkeletonOrderCart = () => (
-  <div className="skeleton-order-cart">
-    <SkeletonBadge width="8" />
+  <div className="my-orders-page-cart skeleton-order-cart">
+    <div className="order-header">
+      <div>
+        <div className="skeleton order-id" />
+        <SkeletonBadge width="8" />
+      </div>
+      <div className="skeleton order-price" />
+    </div>
+
     <div className="skeleton-content-row">
       <SkeletonMyOrderSummary />
     </div>
