75fdd7cdf0f0e5956e4123088b999e56617b521e
jardenna <hbjornum@gmail.com>
2026-08-29 09:56:33 +0200

    style

--- src/components/skeleton/skeletonMyOrderPage/SkeletonMyOrderPage.tsx
+++ src/components/skeleton/skeletonMyOrderPage/SkeletonMyOrderPage.tsx
@@ -1,6 +1,8 @@
 import Skeleton from '../Skeleton';
+import SkeletonBadge from '../SkeletonBadge';
 import SkeletonParagraph from '../SkeletonParagraph';
 import SkeletonMyOrderSummary from './SkeletonMyOrderSummary';
+import './_skeleton-my-order-page.scss';
 
 const SkeletonMyOrderPage = () => (
   <div className="skeleton-order-page">
@@ -8,9 +10,21 @@ const SkeletonMyOrderPage = () => (
       <Skeleton className="skeleton-sub-header-title" />
       <SkeletonParagraph count={2} />
     </span>
+    <div className="skeleton-order-cart">
+      <div className="order-header">
+        <div className="skeleton order-id" />
+        <div className="skeleton order-price" />
+      </div>
+      <SkeletonBadge width="8" />
+      <div className="skeleton-content-row">
+        <SkeletonMyOrderSummary />
+      </div>
+      <div className="divider" />
 
-    <div className="skeleton-content-row">
-      <SkeletonMyOrderSummary />
+      <div className="footer-row">
+        <div className="skeleton delivery-date" />
+        <div className="skeleton details-button" />
+      </div>
     </div>
   </div>
 );
