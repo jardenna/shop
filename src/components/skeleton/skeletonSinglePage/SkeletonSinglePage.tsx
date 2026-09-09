c532aab653ed6b9d96b9c83538720fcfb7e58b39
jardenna <hbjornum@gmail.com>
2026-09-09 12:01:27 +0200

    style

--- src/components/skeleton/skeletonSinglePage/SkeletonSinglePage.tsx
+++ src/components/skeleton/skeletonSinglePage/SkeletonSinglePage.tsx
@@ -1,7 +1,5 @@
 import Skeleton from '../Skeleton';
 import SkeletonAccordion from '../skeletonAccordion/SkeletonAccordion';
-import SkeletonHeader from '../SkeletonHeader';
-import SkeletonParagraph from '../SkeletonParagraph';
 import './_skeleton-single-page.scss';
 import SkeletonSingleProductInfo from './skeletonSingleProductInfo/SkeletonSingleProductInfo';
 
@@ -10,9 +8,8 @@ const SkeletonSinglePage = () => (
     <Skeleton height="44" className="skeleton-single-product-img" />
 
     <div className="flex flex-column">
-      <SkeletonParagraph width="4" count={1} />
-      <SkeletonHeader hideLink />
       <SkeletonSingleProductInfo />
+      Skeleton Accordion
       <SkeletonAccordion />
     </div>
   </>
--- src/components/skeleton/skeletonSinglePage/skeletonSingleProductInfo/SkeletonSingleProductInfo.tsx
+++ src/components/skeleton/skeletonSinglePage/skeletonSingleProductInfo/SkeletonSingleProductInfo.tsx
@@ -1,36 +1,39 @@
+import Skeleton from '../../Skeleton';
+import SkeletonBreadCrumbs from '../../SkeletonBreadCrumbs';
 import SkeletonRatingList from '../../SkeletonRatingList';
 import './_skeleton-single-product-info.scss';
 
 const SkeletonSingleProductInfo = () => (
-  <div className="product-panel">
-    <div className="title-row">
-      <div className="title-skeleton skeleton" />
-      <div className="heart-skeleton skeleton" />
+  <div className="skeleton-single-product-container">
+    <SkeletonBreadCrumbs width="12" />
+    <div className="skeleton-title-row">
+      <Skeleton className="skeleton-title" />
+      <Skeleton className="skeleton-heart" />
     </div>
     <SkeletonRatingList />
-
+    Stock row
     <div className="stock-row">
       <div className="stock-icon skeleton" />
       <div className="stock-label skeleton" />
     </div>
-
+    PriceRow
     <div className="price-row">
       <div className="price-skeleton skeleton" />
     </div>
-
+    ???
     <div className="section-label skeleton" />
-    <hr className="divider" />
+    Divider
+    <Skeleton className="skeleton-divider" />
+    Rating list medium
     <SkeletonRatingList variant="medium" />
-
+    ???
     <div className="swatch-label skeleton" />
     <div className="swatches">
       <div className="swatch skeleton" />
       <div className="swatch skeleton" />
     </div>
-
     <div className="size-label skeleton" />
     <div className="size-option skeleton" />
-
     <div className="qty-row">
       <div className="qty-title skeleton" />
       <div className="qty-selector">
@@ -39,7 +42,6 @@ const SkeletonSingleProductInfo = () => (
         <div className="skeleton" />
       </div>
     </div>
-
     <div className="add-to-cart skeleton" />
   </div>
 );
