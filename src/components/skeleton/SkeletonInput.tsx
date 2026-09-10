b6db18cd4cd5aed42f305439925cd33eedb82157
jardenna <hbjornum@gmail.com>
2026-09-10 19:47:32 +0200

    heights

--- src/components/skeleton/SkeletonInput.tsx
+++ src/components/skeleton/SkeletonInput.tsx
@@ -2,7 +2,7 @@ import Skeleton, { SkeletonProps } from './Skeleton';
 
 const SkeletonInput = ({ className }: SkeletonProps) => (
   <div className={`skeleton-input skeleton-column ${className}`}>
-    <Skeleton height="0.875" width="14" />
+    <Skeleton height="1" width="14" />
     <Skeleton height="4" />
   </div>
 );
