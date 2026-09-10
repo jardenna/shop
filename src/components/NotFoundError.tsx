5b7f53af740f547422a798e51be1c49fa5d53d98
jardenna <hbjornum@gmail.com>
2026-09-10 16:14:20 +0200

    error meta title

--- src/components/NotFoundError.tsx
+++ src/components/NotFoundError.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router';
 import { useLanguage } from '../features/language/useLanguage';
+import MetaTags from '../layout/MetaTags';
 import Button from './Button';
 import Picture from './Picture';
 
@@ -19,38 +20,42 @@ const NotFoundError = ({
   const navigate = useNavigate();
   const { language } = useLanguage();
   const errorText = error?.data?.message ?? language.somethingWentWrong;
+
   const src = '/images/icons/sad_smiley';
+  const hasStatusCode = error?.status && error.status !== 'FETCH_ERROR';
+  const metaErrorText = `${language.error} ${hasStatusCode ? error.status : undefined}`;
 
   return (
-    <section className={`error-content ${className}`}>
-      <Picture
-        className="emoji"
-        src={`${src}.png`}
-        srcSet={`${src}.avif`}
-        alt={language.errorAltText}
-      />
+    <>
+      <MetaTags metaTitle={metaErrorText} />
+      <section className={`error-content ${className}`}>
+        <Picture
+          className="emoji"
+          src={`${src}.png`}
+          srcSet={`${src}.avif`}
+          alt={language.errorAltText}
+        />
 
-      <div className="flex">
-        {error?.status && error.status !== 'FETCH_ERROR' && (
-          <span className="error-code">{error.status}</span>
-        )}
-        <h1 className="error-info">{errorText}</h1>
-      </div>
+        <div className="flex">
+          {hasStatusCode && <span className="error-code">{error.status}</span>}
+          <h1 className="error-info">{errorText}</h1>
+        </div>
 
-      <Button
-        onClick={() => {
-          if (path) {
-            navigate(path);
-          } else {
-            navigate(-1);
-          }
-        }}
-      >
-        {btnLabel
-          ? `${language.goBackTo} ${language[btnLabel]}`
-          : language.goBack}
-      </Button>
-    </section>
+        <Button
+          onClick={() => {
+            if (path) {
+              navigate(path);
+            } else {
+              navigate(-1);
+            }
+          }}
+        >
+          {btnLabel
+            ? `${language.goBackTo} ${language[btnLabel]}`
+            : language.goBack}
+        </Button>
+      </section>
+    </>
   );
 };
 
