ecd4b36c84969b65f295b07ea24c0c633bf568bf
jardenna <hbjornum@gmail.com>
2026-09-10 16:32:35 +0200

    status code show in meta title

--- src/components/ErrorBoundaryFallback.tsx
+++ src/components/ErrorBoundaryFallback.tsx
@@ -1,4 +1,5 @@
 import { useLanguage } from '../features/language/useLanguage';
+import MetaTags from '../layout/MetaTags';
 import ErrorContent from './ErrorContent';
 
 // Props are automatically injected by react-error-boundary
@@ -16,14 +17,19 @@ const ErrorBoundaryFallback = ({
   console.error('Caught error in ErrorBoundary:', error);
 
   const errorText = error?.data?.message ?? language.somethingWentWrong;
+  const hasStatusCode = error?.status && error.status !== 'FETCH_ERROR';
+  const metaErrorText =
+    `${language.error} ${hasStatusCode ? error.status : ''}`.trim();
 
   return (
-    <ErrorContent
-      onClick={resetErrorBoundary}
-      errorText={errorText}
-
-      btnLabel={language.retry}
-    />
+    <>
+      <MetaTags metaTitle={metaErrorText} />
+      <ErrorContent
+        onClick={resetErrorBoundary}
+        errorText={errorText}
+        btnLabel={language.retry}
+      />
+    </>
   );
 };
 
