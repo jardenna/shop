526a39318ae2655812dc9a5f5a345ae9ec456420
jardenna <hbjornum@gmail.com>
2026-09-11 14:12:18 +0200

    SkeletonUpdateProductRight

--- src/components/skeleton/skeletonUpdateProduct/SkeletonUpdateProductRight.tsx
+++ src/components/skeleton/skeletonUpdateProduct/SkeletonUpdateProductRight.tsx
@@ -1,19 +1,31 @@
+import SkeletonControlList from '../SkeletonControlList';
 import SkeletonFlexInputList from '../SkeletonFlexInputList';
 import SkeletonFormCart from '../SkeletonFormCart';
 import SkeletonInputList from '../SkeletonInputList';
+import SkeletonPriceItem from '../skeletonPriceList/SkeletonPriceItem';
+import SkeletonToggleButton from '../SkeletonToggleButton';
 
 const SkeletonUpdateProductRight = () => (
   <div className="product-form-right-column">
     <SkeletonFormCart>
-      <SkeletonInputList count={1} />
+      <div className="skeleton-column">
+        <SkeletonInputList count={1} />
+        <SkeletonControlList variant="medium" count={4} />
+      </div>
     </SkeletonFormCart>
 
     <SkeletonFormCart>
-      <SkeletonFlexInputList />
+      <div className="skeleton-column">
+        <SkeletonFlexInputList />
+        <SkeletonToggleButton />
+      </div>
     </SkeletonFormCart>
 
     <SkeletonFormCart>
-      <SkeletonInputList count={2} />
+      <div className="skeleton-column">
+        <SkeletonInputList count={2} />
+        <SkeletonPriceItem count={1} />
+      </div>
     </SkeletonFormCart>
   </div>
 );
