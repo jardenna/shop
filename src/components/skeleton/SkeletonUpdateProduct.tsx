9cc438aeb762292cb01a0ece1264b7a3fd56a2a4
jardenna <hbjornum@gmail.com>
2026-09-11 12:15:05 +0200

    skeleton

--- src/components/skeleton/SkeletonUpdateProduct.tsx
+++ src/components/skeleton/SkeletonUpdateProduct.tsx
@@ -1,11 +1,21 @@
+import Skeleton from './Skeleton';
 import SkeletonAdminPage from './SkeletonAdminPage';
 import SkeletonFormCart from './SkeletonFormCart';
+import SkeletonInputList from './SkeletonInputList';
 
 const SkeletonUpdateProduct = () => (
   <SkeletonAdminPage variant="large">
     <div className="product-form-container">
       <div className="product-form-left-column">
-        <SkeletonFormCart>update</SkeletonFormCart>
+        <SkeletonFormCart>
+          <SkeletonInputList count={1} />
+        </SkeletonFormCart>
+        <SkeletonFormCart>
+          <Skeleton height="2" />
+        </SkeletonFormCart>
+        <SkeletonFormCart>
+          <SkeletonInputList count={3} />
+        </SkeletonFormCart>
       </div>
       <div className="product-form-right-column">ss</div>
     </div>
