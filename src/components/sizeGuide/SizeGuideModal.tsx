fe4a533afe1e6a7dffd42fb362f0a1b24ac4f6d2
jardenna <hbjornum@gmail.com>
2026-09-13 17:51:24 +0200

    label

--- src/components/sizeGuide/SizeGuideModal.tsx
+++ src/components/sizeGuide/SizeGuideModal.tsx
@@ -1,25 +1,21 @@
-import Modal, { PrimaryActionBtnProps } from '../modal/Modal';
+import Modal from '../modal/Modal';
 
 interface SizeGuideModalProps {
   id: string;
   language: Record<string, string>;
 }
 
-const SizeGuideModal = ({ language, id }: SizeGuideModalProps) => {
-  const primaryActionBtn: PrimaryActionBtnProps = {
-    label: language.update,
-  };
-
-  return (
-    <Modal
-      id={id}
-      modalSize="medium"
-      primaryActionBtn={primaryActionBtn}
-      modalHeaderText={language.sizeGuide}
-    >
-      hello
-    </Modal>
-  );
-};
+const SizeGuideModal = ({ language, id }: SizeGuideModalProps) => (
+  <Modal
+    id={id}
+    modalSize="medium"
+    primaryActionBtn={{
+      label: 'OK',
+    }}
+    modalHeaderText={language.sizeGuide}
+  >
+    hello
+  </Modal>
+);
 
 export default SizeGuideModal;
