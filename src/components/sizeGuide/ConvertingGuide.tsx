0555e6519eb3b01eb600a1b70958aceaa774d94d
jardenna <hbjornum@gmail.com>
2026-09-14 09:45:46 +0200

    size guides

--- src/components/sizeGuide/ConvertingGuide.tsx
+++ src/components/sizeGuide/ConvertingGuide.tsx
@@ -3,9 +3,7 @@ import { useLanguage } from '../../features/language/useLanguage';
 const ConvertingGuide = () => {
   const { language } = useLanguage();
   return (
-    <section>
-      <h2>{language.sizeGuide}</h2>
-      <p>{language.sizeGuideInfo}</p>
+    <article>
       <table>
         <caption>
           <span>{language.convertingSize}</span>
@@ -183,111 +181,9 @@ const ConvertingGuide = () => {
               <span>20</span>
             </td>
           </tr>
-          <tr>
-            <td>
-              <span>50</span>
-            </td>
-            <td>
-              <span>5XL</span>
-            </td>
-            <td>
-              <span>52</span>
-            </td>
-            <td>
-              <span>56</span>
-            </td>
-            <td>
-              <span>22</span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <span>52</span>
-            </td>
-            <td>
-              <span>6XL</span>
-            </td>
-            <td>
-              <span>54</span>
-            </td>
-            <td>
-              <span>58</span>
-            </td>
-            <td>
-              <span>24</span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <span>54</span>
-            </td>
-            <td>
-              <span>7XL</span>
-            </td>
-            <td>
-              <span>56</span>
-            </td>
-            <td>
-              <span>60</span>
-            </td>
-            <td>
-              <span>26</span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <span>56</span>
-            </td>
-            <td>
-              <span>8XL</span>
-            </td>
-            <td>
-              <span>58</span>
-            </td>
-            <td>
-              <span>62</span>
-            </td>
-            <td>
-              <span>28</span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <span>58</span>
-            </td>
-            <td>
-              <span>9XL</span>
-            </td>
-            <td>
-              <span>60</span>
-            </td>
-            <td>
-              <span>64</span>
-            </td>
-            <td>
-              <span>30</span>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <span>60</span>
-            </td>
-            <td>
-              <span>10XL</span>
-            </td>
-            <td>
-              <span>62</span>
-            </td>
-            <td>
-              <span>66</span>
-            </td>
-            <td>
-              <span>32</span>
-            </td>
-          </tr>
         </tbody>
       </table>
-    </section>
+    </article>
   );
 };
 
