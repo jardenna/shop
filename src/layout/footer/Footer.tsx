9c4e18ec7e29de5b3dcead4b87e6630e5c251534
jardenna <hbjornum@gmail.com>
2026-09-15 09:46:50 +0200

    footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,5 +1,17 @@
+import IconContent from '../../components/IconContent';
+import { IconName } from '../../types/enums';
 import LayoutElement from '../LayoutElement';
 
-const Footer = () => <LayoutElement ariaLabel="main">footer</LayoutElement>;
+const Footer = () => (
+  <LayoutElement ariaLabel="main" as="footer">
+    <section>
+      <article>Vores</article>
+      <article>Vores</article>
+      <article>
+        <IconContent iconName={IconName.Facebook} ariaLabel="Facebook" />
+      </article>
+    </section>
+  </LayoutElement>
+);
 
 export default Footer;
