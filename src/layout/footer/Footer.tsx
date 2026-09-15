91c0b70c60539081f06da1bdec6be8d12d23f832
jardenna <hbjornum@gmail.com>
2026-09-15 11:19:07 +0200

    footer style

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,19 +1,52 @@
+import { PaymentMethods } from '../../app/api/apiTypes/paymentApiTypes';
 import IconContent from '../../components/IconContent';
+import PaymentMethodsList from '../../features/cart/components/PaymentMethodsList';
 import { IconName } from '../../types/enums';
 import LayoutElement from '../LayoutElement';
+import NavContainer from '../nav/NavContainer';
+import { subNavList } from '../nav/navLists';
+import './_footer.scss';
 
-const Footer = () => (
-  <LayoutElement ariaLabel="main" as="footer">
-    <section>
-      <article>Vores</article>
-      <article>Vores</article>
-      <article>
-        <IconContent iconName={IconName.Facebook} ariaLabel="Facbook" />
-        <IconContent iconName={IconName.Instagram} ariaLabel="Instagram" />
-        <IconContent iconName={IconName.Tiktok} ariaLabel="Tiktok" />
-      </article>
-    </section>
-  </LayoutElement>
-);
+const Footer = () => {
+  // Make endpoint
+  const paymentMethods: PaymentMethods[] = [
+    'visa',
+    'mastercard',
+    'paypal',
+    'mobilepay',
+  ];
 
+  return (
+    <LayoutElement ariaLabel="main" as="footer">
+      <section>
+        <div>
+          <PaymentMethodsList paymentMethods={paymentMethods} />
+        </div>
+        <article>
+          Vores Kollection Tilbud home
+          <NavContainer navList={subNavList} />
+        </article>
+        <article>
+          Kunde service
+          <div>Kontakt os Faq Levering Returnering</div>
+        </article>
+        <article>
+          Virksomhed
+          <div>
+            om os handelsbetingelsr privatliv Cookies accessibility statement
+          </div>
+        </article>
+        <article>
+          Account
+          <div>Mine ordrer login</div>
+        </article>
+        <article>
+          <IconContent iconName={IconName.Facebook} ariaLabel="Facbook" />
+          <IconContent iconName={IconName.Instagram} ariaLabel="Instagram" />
+          <IconContent iconName={IconName.Tiktok} ariaLabel="Tiktok" />
+        </article>
+      </section>
+    </LayoutElement>
+  );
+};
 export default Footer;
