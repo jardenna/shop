4215504f21241fbc7a323d50f91b2c98932b9ab3
jardenna <hbjornum@gmail.com>
2026-09-16 15:46:57 +0200

    grid

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,29 +1,47 @@
 import { useId } from 'react';
+import ImageTextGrid from '../components/imageTextGrid/ImageTextGrid';
 import PageHeader from '../components/PageHeader';
-import Picture from '../components/Picture';
 import { useLanguage } from '../features/language/useLanguage';
-import { useMediaQuery } from '../hooks/useMediaQuery';
 import MetaTags from '../layout/MetaTags';
+import './AboutUsPage.styles.scss';
 
 const AboutUsPage = () => {
   const { language } = useLanguage();
-  const { isMobileSize } = useMediaQuery();
   const aboutId = useId();
-  const src = `/images/about/about_us`;
 
   const heading = language.about;
   return (
     <>
       <MetaTags metaTitle={heading} />
-      <div
-        className="about-us-page container page-large"
-        aria-labelledby={aboutId}
-      >
-        <div className="about-us-container">
-          <PageHeader heading={heading} ariaLabelledby={aboutId} />
-
-          <div className="about-us-content">
-            <section>
+      <div aria-labelledby={aboutId}>
+        <PageHeader heading={heading} ariaLabelledby={aboutId} />
+        <div className="about-us-page">
+          <ImageTextGrid
+            heading="Who we are"
+            src="/images/about/about_us"
+            alt=""
+          >
+            <h3>We believe great style should feel effortless</h3>
+            <p>
+              We are an independent online fashion store offering carefully
+              selected clothing and accessories for modern wardrobes. We believe
+              great style should feel effortless, accessible and easy to shop.
+            </p>
+          </ImageTextGrid>
+          <ImageTextGrid
+            heading="Who we are"
+            src="/images/about/about_us"
+            alt=""
+          >
+            <h3>We believe great style should feel effortless</h3>
+            <p>
+              We are an independent online fashion store offering carefully
+              selected clothing and accessories for modern wardrobes. We believe
+              great style should feel effortless, accessible and easy to shop.
+            </p>
+          </ImageTextGrid>
+          {/* <section>
+            <div>
               <h2>Who we are</h2>
               <h3>We believe great style should feel effortless</h3>
               <p>
@@ -32,9 +50,14 @@ const AboutUsPage = () => {
                 believe great style should feel effortless, accessible and easy
                 to shop.
               </p>
-            </section>
+            </div>
+            <div>
+              <Img src={src} alt="" />
+            </div>
+          </section>
 
-            <section>
+          <section>
+            <div>
               <h2>Our collection</h2>
               <p>
                 Our collection is built around versatile pieces that can be
@@ -42,32 +65,41 @@ const AboutUsPage = () => {
                 quality, thoughtful design and styles that fit naturally into
                 everyday life.
               </p>
-            </section>
 
-            <section>
+              <div>
+                <Img src={src} alt="" />
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <div>
               <h2>Our approach</h2>
               <p>
                 We want shopping online to be simple and enjoyable. From
                 discovering new styles to receiving your order, we aim to make
                 every part of the experience clear, convenient and accessible.
               </p>
-            </section>
+            </div>
+            <div>
+              <Img src={src} alt="" />
+            </div>
+          </section>
 
-            <section>
+          <section>
+            <div>
               <h2>Made for everyday</h2>
               <p>
                 Fashion should work for real life. That is why we choose pieces
                 that are easy to wear, easy to combine and designed to become
                 part of your wardrobe.
               </p>
-            </section>
-          </div>
+            </div>
+            <div>
+              <Img src={src} alt="" />
+            </div>
+          </section> */}
         </div>
-        {!isMobileSize && (
-          <div className="about-img-container">
-            <Picture src={`${src}.jpg`} srcSet={`${src}.avif`} alt="alttext" />
-          </div>
-        )}
       </div>
     </>
   );
