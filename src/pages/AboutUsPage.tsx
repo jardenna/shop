e8d866bc18c791145050ea383a4b8df57d2eaa23
jardenna <hbjornum@gmail.com>
2026-09-16 16:05:48 +0200

    cleanup

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -29,76 +29,42 @@ const AboutUsPage = () => {
             </p>
           </ImageTextGrid>
           <ImageTextGrid
-            heading="Who we are"
+            heading="Our collection"
             src="/images/about/about_us"
             alt=""
           >
-            <h3>We believe great style should feel effortless</h3>
+            <h3>We focus on quality</h3>
             <p>
-              We are an independent online fashion store offering carefully
-              selected clothing and accessories for modern wardrobes. We believe
-              great style should feel effortless, accessible and easy to shop.
+              Our collection is built around versatile pieces that can be worn,
+              combined and enjoyed season after season. We focus on quality,
+              thoughtful design and styles that fit naturally into everyday
+              life.
             </p>
           </ImageTextGrid>
-          {/* <section>
-            <div>
-              <h2>Who we are</h2>
-              <h3>We believe great style should feel effortless</h3>
-              <p>
-                We are an independent online fashion store offering carefully
-                selected clothing and accessories for modern wardrobes. We
-                believe great style should feel effortless, accessible and easy
-                to shop.
-              </p>
-            </div>
-            <div>
-              <Img src={src} alt="" />
-            </div>
-          </section>
-
-          <section>
-            <div>
-              <h2>Our collection</h2>
-              <p>
-                Our collection is built around versatile pieces that can be
-                worn, combined and enjoyed season after season. We focus on
-                quality, thoughtful design and styles that fit naturally into
-                everyday life.
-              </p>
-
-              <div>
-                <Img src={src} alt="" />
-              </div>
-            </div>
-          </section>
-
-          <section>
-            <div>
-              <h2>Our approach</h2>
-              <p>
-                We want shopping online to be simple and enjoyable. From
-                discovering new styles to receiving your order, we aim to make
-                every part of the experience clear, convenient and accessible.
-              </p>
-            </div>
-            <div>
-              <Img src={src} alt="" />
-            </div>
-          </section>
-
-          <section>
-            <div>
-              <h2>Made for everyday</h2>
-              <p>
-                Fashion should work for real life. That is why we choose pieces
-                that are easy to wear, easy to combine and designed to become
-                part of your wardrobe.
-              </p>
-            </div>
-            <div>
-              <Img src={src} alt="" />
-            </div>
-          </section> */}
+          <ImageTextGrid
+            heading="Our approach"
+            src="/images/about/about_us"
+            alt=""
+          >
+            <h3>We aim to make every part of the experience clear</h3>
+            <p>
+              We want shopping online to be simple and enjoyable. From
+              discovering new styles to receiving your order, we aim to make
+              every part of the experience clear, convenient and accessible.
+            </p>
+          </ImageTextGrid>
+          <ImageTextGrid
+            heading="Made for everyday"
+            src="/images/about/about_us"
+            alt=""
+          >
+            <h3>We choose pieces that are designed for real life</h3>
+            <p>
+              Fashion should work for real life. That is why we choose pieces
+              that are easy to wear, easy to combine and designed to become part
+              of your wardrobe.
+            </p>
+          </ImageTextGrid>
         </div>
       </div>
     </>
