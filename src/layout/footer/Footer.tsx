e9b78175674bf0b92fbc16850b205849334910ed
jardenna <hbjornum@gmail.com>
2026-09-17 10:30:13 +0200

    footer style

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -20,12 +20,23 @@ const Footer = () => {
     <LayoutElement ariaLabel="main" as="footer" className="main-footer">
       <section className="container">
         <article className="footer-container">
-          <Icon
-            iconName={IconName.Logo}
-            title="Fashion Fusion Logo"
-            ariaHidden={false}
-            desc="Logo"
-          />
+          <div className="footer-shop-info">
+            <Icon
+              iconName={IconName.Logo}
+              title="Fashion Fusion Logo"
+              ariaHidden={false}
+              desc="Logo"
+            />
+
+            <div className="footer-social-container">
+              <IconContent iconName={IconName.Facebook} ariaLabel="Facbook" />
+              <IconContent
+                iconName={IconName.Instagram}
+                ariaLabel="Instagram"
+              />
+              <IconContent iconName={IconName.Tiktok} ariaLabel="Tiktok" />
+            </div>
+          </div>
           <div className="footer-nav-container">
             <FooterNav navList={footerCompanyNav} title={language.company} />
             <FooterNav
@@ -36,11 +47,6 @@ const Footer = () => {
             <FooterNav navList={footerAccountNav} title={language.account} />
           </div>
         </article>
-        <div>
-          <IconContent iconName={IconName.Facebook} ariaLabel="Facbook" />
-          <IconContent iconName={IconName.Instagram} ariaLabel="Instagram" />
-          <IconContent iconName={IconName.Tiktok} ariaLabel="Tiktok" />
-        </div>
       </section>
     </LayoutElement>
   );
