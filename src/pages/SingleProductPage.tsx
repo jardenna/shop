a12c4b0d417dff2835045f4830723b7421258c58
jardenna <hbjornum@gmail.com>
2026-09-18 09:46:25 +0200

    removed console.log

--- src/pages/SingleProductPage.tsx
+++ src/pages/SingleProductPage.tsx
@@ -103,7 +103,6 @@ const SingleProductPage = () => {
   const missingSizes = displaySizeList.filter(
     (size) => !product.sizes.includes(size),
   );
-  console.log(product);
 
   return (
     <>
