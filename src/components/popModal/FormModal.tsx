e9a23c1b3f902be873cae5c46ba0f8838799b0c8
jardenna <hbjornum@gmail.com>
2026-09-22 19:35:01 +0200

    account modal

--- src/components/popModal/FormModal.tsx
+++ src/components/popModal/FormModal.tsx
@@ -5,14 +5,13 @@ import PopModal, { PopModalProps } from './PopModal';
 import { usePopModal } from './usePopModal';
 
 interface FormModalProps extends PopModalProps {
-  id: string;
   isLoading: boolean;
+  disabled?: boolean;
   submitLabel?: string;
   onSubmit: () => void;
 }
 
 const FormModal = ({
-  id,
   ariaControls,
   children,
   headerText,
@@ -22,11 +21,10 @@ const FormModal = ({
   modalSize = 'medium',
   className,
   submitLabel,
+  disabled,
 }: FormModalProps) => {
   const { language } = useLanguage();
   const { closeModal } = usePopModal();
-
-  console.log(id);
 
   return (
     <PopModal
@@ -36,14 +34,20 @@ const FormModal = ({
       ariaControls={ariaControls}
       className={className}
     >
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          onSubmit();
+          closeModal();
+        }}
+      >
         {children}
 
         <footer className="footer">
           <Button onClick={closeModal} variant={BtnVariant.Secondary}>
             {language.cancel}
           </Button>
-          <Button showBtnLoader={isLoading} type="submit">
+          <Button showBtnLoader={isLoading} type="submit" disabled={disabled}>
             {submitLabel ?? language.update}
           </Button>
         </footer>
