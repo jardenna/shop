42dba4503a1b5c2f7a6cb20cf453588425f50237
jardenna <hbjornum@gmail.com>
2026-09-26 12:56:10 +0200

    moved getCartQuantity

--- src/utils/reduceQty.ts
+++
@@ -1,4 +0,0 @@
-import { CartItem } from '../app/api/apiTypes/cartApiTypes';
-
-export const getCartQuantity = (cartItems: CartItem[]): number =>
-  cartItems.reduce((totalQty, cartItem) => totalQty + cartItem.qty, 0);
