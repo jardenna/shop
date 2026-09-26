d20689f03e876c5643d850d9af92819c705a68f8
jardenna <hbjornum@gmail.com>
2026-09-26 13:54:12 +0200

    panelpopup

---
+++ src/features/cart/components/miniCartPopup/PanelPopup.tsx
@@ -0,0 +1,47 @@
+import { ReactNode, useRef } from 'react';
+import BtnClose from '../../../../components/BtnClose';
+import Portal from '../../../../components/Portal';
+import { useAnimate } from '../../../../hooks/useAnimate';
+import { useClickOutside } from '../../../../hooks/useClickOutside';
+import { useKeyPress } from '../../../../hooks/useKeyPress';
+import { useScrollLock } from '../../../../hooks/useScrollLock';
+import { KeyCode } from '../../../../types/enums';
+import './_mini-cart-popup.scss';
+
+interface PanelPopupProps {
+  children: ReactNode;
+  isOpen: boolean;
+  onClosePanel: () => void;
+}
+
+const PanelPopup = ({ children, onClosePanel, isOpen }: PanelPopupProps) => {
+  const panelRef = useRef<HTMLElement>(null);
+
+  const { shouldRender, transitionState } = useAnimate({
+    isOpen,
+  });
+
+  useKeyPress(onClosePanel, [KeyCode.Esc]);
+  useScrollLock(shouldRender);
+
+  useClickOutside(panelRef, onClosePanel, [panelRef]);
+
+  if (!shouldRender) {
+    return null;
+  }
+
+  return (
+    <Portal portalId="panel">
+      <section
+        className={`mini-cart transition from-right ${transitionState}`}
+        ref={panelRef}
+      >
+        {children}
+
+        <BtnClose onClick={onClosePanel} />
+      </section>
+    </Portal>
+  );
+};
+
+export default PanelPopup;
